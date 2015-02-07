974b703fa6492d701156a65c6b8222c615c923a8
B3h3m0th <behemoth@autistici.org>
2015-02-07 17:56:02 +0100

    splice() adding comments

--- func_splice.pl
+++ func_splice.pl
@@ -5,13 +5,15 @@ use strict;
 #splice ARRAY, OFFSET, LENGTH, LIST
 #rimuove LENGTH elementi a partire da OFFSET nell'ARRAY, rimpiazza con LIST se
 #e' stato inserito
-
+#L'OFFSET inizia da zero
 my @skills = qw(C C++ win$oz Perl Awk Unix);
 my $new_skill = 'System Programming';
 
 foreach my $name (@skills) {
     print "skill: $name\n";
 }
+
+#elimino il terzo elemento e aggiungo uno nuovo
 splice @skills, 2, 1, $new_skill;
 
 print "\n\nNew skills: \n";
