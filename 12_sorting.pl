03d1f25756637a24c6f8383bf87e890e648d0814
B3h3m0th <behemoth@autistici.org>
2015-02-15 11:26:58 +0100

    sorting strings and numbers

--- 12_sorting.pl
+++ 12_sorting.pl
@@ -2,3 +2,23 @@
 use warnings;
 use strict;
 
+my @data = qw(31 BSD linux 3  OpenBSD 1973 0 unix freeBSD kernel 69 48 25);
+print "before sorting: @data\n";
+
+#basic alphanumerical sort
+@data = sort @data;
+print " after sorting: @data\n";
+
+#compare alphabetic
+@data = sort {$a cmp $b} @data;
+print "     after cmp: @data\n";
+
+#compare strings (warnings)
+@data = sort {$a <=> $b} @data;
+print "     after <=>: @data\n";
+
+no warnings;
+#compare mix strings and numbers
+@data = sort {$a <=> $b or $a cmp $b} @data;
+print "after <=>orcmp: @data\n";
+use warnings;
