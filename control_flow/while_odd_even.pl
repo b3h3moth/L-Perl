dc41b4721cc539f26ae322a96002b2c260577a38
B3h3m0th <behemoth@autistici.org>
2015-02-17 12:02:36 +0100

    program to verify is number is odd or even

--- control_flow/while_odd_even.pl
+++ control_flow/while_odd_even.pl
@@ -6,4 +6,12 @@ die "Specify al least five numbers\n" if (@ARGV < 5);
 
 my @numbers = @ARGV;
 
-print "@numbers";
+while (@numbers) {
+    my $number = shift(@numbers);
+
+    if ($number % 2 == 0) {
+        print "Number: $number is even\n";
+    } else {
+        print "Number: $number is odd\n";
+    }
+}
