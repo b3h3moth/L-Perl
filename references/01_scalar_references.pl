c0619d4b61f73324b10406de4b131cc2089acb04
B3h3m0th <behemoth@autistici.org>
2015-02-18 14:32:50 +0100

    scalar references

---
+++ references/01_scalar_references.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+#L'operatore per i riferimenti è il backslash \
+my $os_name = 'OpenBSD';
+my $ref = \$os_name;
+
+say  "$ref"; #indirizzo di memoria
+say "$$ref";  #dereferenziazione, accede al valore a cui si riferisce
+              #concettualmente come i puntatori del C, fatte ovviamente le
+              #dovute eccezioni.
+
+#si modifica il valore
+sub invert {
+     my $ref = shift;
+     $$ref = reverse($$ref);
+}
+
+invert(\$os_name);
+
+say "$os_name";
