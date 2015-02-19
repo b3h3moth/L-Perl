6178914a7a8f62f9440623929bb8e5af3d556722
B3h3m0th <behemoth@autistici.org>
2015-02-19 18:32:27 +0100

    function sigil '&'

--- references/05_function_references.pl
+++ references/05_function_references.pl
@@ -3,15 +3,10 @@ use warnings;
 use strict;
 use v5.14;
 
-my @cards = qw(A 1 2 3 4 5 6 7 8 9 10);
-my $rif = \@cards;
+sub which_unix {
+    say 'I\'m on OpenBSD Unix system 5.7';
+}
 
-#dereferenzia automaticamente i riferimenti
-push $rif, qw(J Q K);
-#La deferenziazione vuol dire accedere al valore del riferimento
+#reference operator '\' e function sigil '&'
+my $rif = \&which_unix;
 
-say @$rif;
-
-pop $rif;
-
-say @cards;
