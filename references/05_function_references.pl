89d4e7cea42d241d0bdaa265d1790e942b10c2c6
B3h3m0th <behemoth@autistici.org>
2015-02-19 18:37:29 +0100

    & ... works great with function

--- references/05_function_references.pl
+++ references/05_function_references.pl
@@ -4,9 +4,12 @@ use strict;
 use v5.14;
 
 sub which_unix {
-    say 'I\'m on OpenBSD Unix system 5.7';
+    say 'I\'m on OpenBSD 5.7, real UNIX OS';
 }
 
 #reference operator '\' e function sigil '&'
 my $rif = \&which_unix;
 
+$rif->();
+
+#&$rif; this works :)
