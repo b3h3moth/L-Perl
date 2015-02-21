0b8d8a2c1ee1eaea01f2b996bdf58b4d29f825b2
B3h3m0th <behemoth@autistici.org>
2015-02-21 22:12:48 +0100

    count characters and lines with readline

--- input_output/01_reading_file.pl
+++ input_output/01_reading_file.pl
@@ -4,3 +4,15 @@ use strict;
 use v5.14;
 
 #count characters and lines in a specified file on command line.
+die "Usage: ./a.out <filename>\n" if (@ARGV != 1);
+print "Getting file: $ARGV[0]\n";
+
+my $lines = 0;
+my $characters = 0;
+
+while (my $line = <>) {
+    $lines++;
+    $characters += length($line);
+}
+
+print "Lines: $lines\nCharacters: $characters\n";
