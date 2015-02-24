9ac7300c1d4093e9f60fdcad7680f8c5abe2e515
B3h3m0th <behemoth@autistici.org>
2015-02-24 19:19:44 +0100

    changed number of arguments, added loop for getting filename

--- input_output/02_reading_multiple.pl
+++ input_output/02_reading_multiple.pl
@@ -3,8 +3,13 @@ use warnings;
 use strict;
 
 #count characters and lines pf multiple file at once
-die "Usage: ./a.out <filename>\n" if (@ARGV != 2);
-print "Getting file: $ARGV[0]\n";
+die "Usage: ./a.out <filename1><filename2>\n" if (@ARGV != 2);
+
+my $ARGC = @ARGV;
+
+for (my $i=0; $i<$ARGC; $i++) {
+    print "Getting file: $ARGV[$i]\n";
+}
 
 my $lines = 0;
 my $characters = 0;
