af5ebe2c0eae524f76cfd8e9a4910470545fdf34
B3h3m0th <behemoth@autistici.org>
2015-02-24 19:13:04 +0100

    fixed prev file to reading multiple files and lines

---
+++ input_output/02_reading_multiple.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+#count characters and lines in a specified file on command line.
+die "Usage: ./a.out <filename>\n" if (@ARGV != 2);
+print "Getting file: $ARGV[0]\n";
+
+my $lines = 0;
+my $characters = 0;
+
+while (my $line = <>) {
+    $lines++;
+    $characters += length($line);
+    #print "$line";
+}
+
+print "Lines: $lines\nCharacters: $characters\n";
