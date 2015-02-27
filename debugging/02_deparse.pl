62eff678b7730e4ccb030cb5e108f2609266365c
B3h3m0th <behemoth@autistici.org>
2015-02-27 12:30:10 +0100

    add comment on B::Deparse

--- debugging/02_deparse.pl
+++ debugging/02_deparse.pl
@@ -4,4 +4,10 @@ use strict;
 use v5.14;
 use B::Deparse;
 
+#Il modulo B::Deparse esegue la verifica del codice, in particolare il
+#controllo della precedenza e dell'associativita' degli operatori, ma non solo,
+#e' in generale un'ottima risorsa per il debugging.
+#
+#Uso:
+# perl -MO=Deparse, -p 02_deparse.pl
 my $num = 12 + 9 / 2 + 4 * 7 - 2*+2*+3**2+4/34**2-100;
