e62904761e616a141d89533f6e21337ef49262db
B3h3m0th <behemoth@autistici.org>
2015-03-03 20:16:07 +0100

    added comments

--- input_output/05_open_01_reading_file.pl
+++ input_output/05_open_01_reading_file.pl
@@ -11,9 +11,12 @@ my ($file) = @ARGV;
 #$file is the filename.
 open(INPUT_FILE, "< $file");
 
+#read the file from filehandle and store in $line
 while (my $line = <INPUT_FILE>) {
+    #uc function convert each line in upper-case
     $line = uc($line);
     print "$line";
 }
 
+#filehandle is closed
 close(INPUT_FILE);
