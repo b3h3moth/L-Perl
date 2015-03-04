bf87fce612f00c195a8fadb09940425e00b95575
B3h3m0th <behemoth@autistici.org>
2015-03-04 18:26:25 +0100

    renamed INPUT_FILE

--- input_output/05_open_1_reading_file.pl
+++ input_output/05_open_1_reading_file.pl
@@ -6,26 +6,26 @@ use strict;
 my ($file) = @ARGV;
 
 #open the file for reading.
-#INPUT_FILE is the name of filehandle
+#INPUT is the name of filehandle
 #< is the mode, in this case reading from the file
 #$file is the filename.
-open(INPUT_FILE, "< $file");
+open(INPUT, "< $file");
 
 #read the file from filehandle and store in $line
-while (my $line = <INPUT_FILE>) {
+while (my $line = <INPUT>) {
     #uc() function convert each line in upper-case
     $line = uc($line);
     print "$line";
 }
 
 #filehandle is closed
-close(INPUT_FILE);
+close(INPUT);
 
 #If I have a file called 'pass.txt' and his name is stored in a $file
 #variable, then all of the following do the same thing:
-#open(INPUT_FILE, "< $file");
-#open(INPUT_FILE, "<$file");
-#open(INPUT_FILE, "<pass.txt");
-#open(INPUT_FILE, "pass.txt");
-#open(INPUT_FILE, "<", "$file");
-#open(INPUT_FILE, "<", "pass.txt");
+#open(INPUT, "< $file");
+#open(INPUT, "<$file");
+#open(INPUT, "<pass.txt");
+#open(INPUT, "pass.txt");
+#open(INPUT, "<", "$file");
+#open(INPUT, "<", "pass.txt");
