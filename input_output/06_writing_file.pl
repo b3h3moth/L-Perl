d5295a75aba2d8729871602e906f5c64a240b679
B3h3m0th <behemoth@autistici.org>
2015-03-05 20:04:51 +0100

    check errors info

--- input_output/06_writing_file.pl
+++ input_output/06_writing_file.pl
@@ -9,8 +9,12 @@ die "Usage: ./prg.pl <input_filename>\n" if (@ARGV != 1);
 my ($input_file) = @ARGV;
 my $output_file = "reverse_$input_file";
 
+#reading
 open(my $in, "< $input_file") or die "Err. reading $input_file. $!";
+#writing
 open(my $out, "> $output_file") or die "Err. reading $output_file. $!";
+
+#always check that files have been opened correctly
 
 while (<$in>) {
     chomp;
@@ -20,3 +24,4 @@ while (<$in>) {
 
 close($input_file);
 close($output_file);
+
