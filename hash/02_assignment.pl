3d48b4a3da4b1f027000f9ccabfcce6a07e71437
B3h3m0th <behemoth@autistici.org>
2015-03-07 19:14:52 +0100

    fixed repo, hash assignment

---
+++ hash/02_assignment.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use Data::Dumper;
+use v5.14;
+
+my %job;
+
+#assignment
+$job{luca} = 'Programmer';
+$job{mamy} = 'Student';
+$job{albi} = 'Professor';
+$job{papi} = 'Doctor';
+
+#multiple assignment, method 1
+%job = ('carla', 'actress', 'francesco', 'actor', 'maria', 'barman');
+#multiple assignment, method 2
+%job = (
+    'papi', 'Software developer',
+    'albi', 'driver'
+);
+#in both cases the keys of hash are immediately followed by their respective
+#values
+
+#multiple assignment, method 3
+%job = (
+    franco => 'student',
+    gina   => 'professor'
+
+);
+
+print Dumper \%job;
