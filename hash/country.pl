68730e02a6538c928c4bcab5a48274fbb5a871ab
B3h3m0th <behemoth@autistici.org>
2015-03-07 17:08:07 +0100

    search key/value in hash

---
+++ hash/country.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+#Usage: ./prog.pl Italy Fr
+#   Code: It
+#Country: France
+die "Usage ./prog.pl <country><country two-letter code>\n" if @ARGV != 2;
+
+my ($state, $code) = @ARGV;
+
+my %country = (
+    "Italy"     => "It",
+    "France"    => "Fr",
+    "England"   => "Uk",
+    "Germany"   => "Ge"
+);
+
+my %country_code = (
+    "It"        => "Italy",
+    "Fr"        => "France",
+    "Uk"        => "England",
+    "Ge"        => "Germany"
+);
+
+say "   Code: $country{$state}" if exists $country{$state};
+say "Country: $country_code{$code}" if exists $country_code{$code};
