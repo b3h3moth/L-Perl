74684584388402ae84b820fdfaf6c7bc8dc98987
B3h3m0th <behemoth@autistici.org>
2015-03-07 19:03:19 +0100

    fixed dir, new hash

---
+++ hash/01_declare_hash.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use Data::Dumper;
+use v5.14;
+
+#in older version of Perl these were also know as associative arrays
+my %my_hash; #empty
+
+#$ 'dollar signs'   = scalar variable
+#@ 'at' symbol      = array
+#% 'percent' sign   = hash
+#@hash              = entire hash
+#$hash{'elem'}      = hash element
