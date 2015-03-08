9cfcbdfbaa841adcad2f773c4bc5b30e5707289d
B3h3m0th <behemoth@autistici.org>
2015-03-08 16:43:26 +0100

    sorting hash

---
+++ hash/10_sorting.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.10;
+
+my %count;
+
+die "Usage ./prog.pl <str 1><str 2><str 3>...<str N>\n" if (@ARGV < 4);
+
+#count each word
+foreach my $word (@ARGV) {
+    $count{$word}++;
+}
+
+#print sort results (keys)
+foreach my $word (sort keys %count) {
+    print "$word\t$count{$word}\n";
+}
