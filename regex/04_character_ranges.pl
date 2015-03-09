a1638f8660552a86e3fd5e5d9c6b2baf6b18e170
B3h3m0th <behemoth@autistici.org>
2015-03-09 20:04:06 +0100

    character ranges, first example

--- regex/04_character_ranges.pl
+++ regex/04_character_ranges.pl
@@ -3,3 +3,7 @@ use warnings;
 use strict;
 use v5.14;
 
+my $password = "9n0A";
+
+#using dash to specify range of numbers or characters
+say "numeric psss is: $password" if ($password =~ m/[0-9][a-z][0-9][A-Z]/)
