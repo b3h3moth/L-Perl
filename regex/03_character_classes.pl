49c3c157449c7051a6d89fea5d9569024989835b
B3h3m0th <behemoth@autistici.org>
2015-03-09 18:04:53 +0100

    added few options to character-classes

--- regex/03_character_classes.pl
+++ regex/03_character_classes.pl
@@ -5,12 +5,15 @@ use v5.14;
 
 my $lang = "perl";
 my $superhero = "super-man";
-my $year = "1925";
+my $year = "2015";
+my $name = "michele";
 
 #character classes is a set of allowed characters,
 #specifying one character to match into square brackets.
 
 if ($lang =~ m/[Pp]erl/) { say "I use $lang every day"; }
 if ($superhero =~ m/super[ -]man/) { say "but I'm not $superhero :)"; }
-if ($year =~ m/192[058]/) { say "$year"; }
+if ($year =~ m/201[0123456789]/) { say "I hope $year will be good"; }
 
+#match michele, michela and michelle
+if ($name =~ m/michel([ae]|le)/) { say "Italian name \'$name\' is very old"; }
