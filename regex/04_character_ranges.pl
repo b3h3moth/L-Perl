4d597c9c41bc36f223648a72c824151ec56ecea2
B3h3m0th <behemoth@autistici.org>
2015-03-09 20:19:03 +0100

    added simple code with tr//

--- regex/04_character_ranges.pl
+++ regex/04_character_ranges.pl
@@ -4,8 +4,16 @@ use strict;
 use v5.14;
 
 my $password = "9n0A";
+my $PIN = "3d47";
+my $string = "aaaaaBBBBBcccccDDDDDeeeee";
 
 #using dash to specify range of numbers or characters
 say "$password" if ($password =~ m/[0-9][a-z][0-9][A-Z]/);
 #both work fine. Final 'i' stands for ignore case
 say "(with ignore case) $password" if ($password =~ m/[0-9][a-z][0-9][a-z]/i);
+
+say "It's not a valid PIN" if ($PIN =~ m/[^0-9]/);
+
+say "before: $string";
+$string =~ tr/[a-z]/[A-Z]/;
+say " after: $string";
