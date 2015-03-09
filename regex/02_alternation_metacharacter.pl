a5c1d22e7d16a79c7e40c6e2337e22eb21bc3789
B3h3m0th <behemoth@autistici.org>
2015-03-09 12:39:30 +0100

    nested alternation metacharacters, and fixed scalars

--- regex/02_alternation_metacharacter.pl
+++ regex/02_alternation_metacharacter.pl
@@ -3,16 +3,18 @@ use warnings;
 use strict;
 use v5.14;
 
+my $lang = "perl";
+my $sport = "football";
+my $name = "behemoth";
+my $osname = "OpenBSD";
+
 #how to create a pattern to match more than one string with '|', it's a pipe
 #or alternation metacharacter
-my $lang = "perl";
-
 say "$lang is awesome" if ($lang =~ m/C|C++|AWK|Perl|Lua|Python|Lisp|Ruby/i);
-
-my $sport = "football";
 
 #use parentheses for grouping metacharacters
 say "I like $sport" if ($sport =~ m/(foot|basket|roller|speed)ball/);
-
-my $name = "behemoth";
 say "My name is $name" if ($name =~ m/be(he|h3|re|ge)mo(a|h|y|o|t|z|)h/);
+
+#nested
+say "$osname rulez" if ($osname =~ m/(o|O)pen(b|B)(s|S|(|d|D))/);
