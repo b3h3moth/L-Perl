053e495831ef86943012da937f0556fa631d62d1
B3h3m0th <behemoth@autistici.org>
2015-03-10 15:06:35 +0100

    caret ^, anchors

--- regex/05_anchors.pl
+++ regex/05_anchors.pl
@@ -3,3 +3,9 @@ use warnings;
 use strict;
 use v5.14;
 
+my $game = "football";
+
+#anchors
+#pattern matches from the beginning of he string '^'
+say "I like to play $game one" if ($game =~ m/^foot/);  #true
+say "I like to play $game two" if ($game =~ m/^sfoot/); #false
