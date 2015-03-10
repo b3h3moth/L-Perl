e2dcd53c52edb9e50614e362df8718651b3cb806
B3h3m0th <behemoth@autistici.org>
2015-03-10 15:07:18 +0100

    new example, ^ caret, anchors

--- regex/05_anchors.pl
+++ regex/05_anchors.pl
@@ -4,8 +4,11 @@ use strict;
 use v5.14;
 
 my $game = "football";
+my $login = "s89287j";
 
 #anchors
 #pattern matches from the beginning of he string '^'
 say "I like to play $game one" if ($game =~ m/^foot/);  #true
 say "I like to play $game two" if ($game =~ m/^sfoot/); #false
+say "text starts with non-digit one" if($login =~ m/^[^0-9]/);  #true
+say "text starts with non-digit two" if($login =~ m/^[0-9]/);   #false
