653ebba174ce6ba2f673c5e0f45a00c4dd9b71ab
B3h3m0th <behemoth@autistici.org>
2015-03-10 17:36:36 +0100

    added updir()

--- filesystem/03_cur_root_dir.pl
+++ filesystem/03_cur_root_dir.pl
@@ -4,8 +4,11 @@ use strict;
 use v5.14;
 use File::Spec;
 
-#How to get current and root directories
+#How to get current, root and up directories
 my $cur_directory = File::Spec->curdir();
 my $root_directory = File::Spec->rootdir();
+my $up_directory = File::Spec->updir();
 
-print "current dir: $cur_directory\nroot dir: $root_directory";
+say "current dir: $cur_directory";
+say "   root dir: $root_directory";
+say "     up dir: $up_directory";
