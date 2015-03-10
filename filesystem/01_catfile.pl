4fdb9f50dd1ceaf49841fc7687c2333cf066e8bc
B3h3m0th <behemoth@autistici.org>
2015-03-10 17:10:14 +0100

    Module File::Spec, catfile() method

---
+++ filesystem/01_catfile.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use File::Spec;
+
+#How to concatenate one or more directory names and a filename to form an
+#absolute path ending with a filename.
+my $path = File::Spec->catfile(qw{home behemoth docs books perl.pdf});
+
+print "$path\n";
