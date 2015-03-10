ec08bee76032c5b03b3f48c7d8927fcb079ba77f
B3h3m0th <behemoth@autistici.org>
2015-03-10 17:25:43 +0100

    File::Spec module, catdir method

--- filesystem/02_catdir.pl
+++ filesystem/02_catdir.pl
@@ -4,13 +4,11 @@ use strict;
 use v5.14;
 use File::Spec;
 
-#How to concatenate one or more directory names and a filename to form an
-#absolute path ending with a filename.
-my $path = File::Spec->catfile(qw{home behemoth docs books perl.pdf});
-
-print "My pdf books path: $path\n";
-
+#How to concatenate two or more directory names to form an absolute path
+#ending with a directory
 my @docs = qw(home behemoth docs books);
-my @books = qw(infosec programming networking gamedevel);
+my @books = qw(programming perl);
+
 my $dpath = File::Spec->catdir(@docs, @books);
-print "$dpath\n";
+
+print "My computer science books path:\n\/$dpath\n";
