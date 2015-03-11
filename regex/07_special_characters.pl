2b5463af014f4d5c6bbf023acf05feb7dacc4cc5
B3h3m0th <behemoth@autistici.org>
2015-03-11 12:14:45 +0100

    added a lot of tests on special chars

--- regex/07_special_characters.pl
+++ regex/07_special_characters.pl
@@ -6,10 +6,15 @@ use v5.14;
 #to match special characters use backslah before the special character.
 my $eng_actor = "Mr. Bean";
 my $or = "a|b";
+my $home = "/home/behemoth";
 
 say "$eng_actor is an English actor" if ($eng_actor =~ m/Mr\./);    #true
 say "$eng_actor is funny :)" if ($eng_actor =~ m/M\./);             #false
 say "special characters" if ($or=~ m/\|/);                          #true
 say "regex" if ($or=~ m/a\|/);                                      #true
 say "works?" if ($or=~ m/b\|/);                                     #false
-
+print "My home dir is: " if ($home =~ m/\//);                       #true
+print "$home 1\n" if ($home =~ m/\/home/);                          #true
+print "$home 2\n" if ($home =~ m/\/[a-z]/);                         #true
+print "$home 3\n" if ($home =~ m/\/[a-z]\/[a-z]/);                  #false
+print "$home 4\n" if ($home =~ m/\/[a-z]...\/[a-z]/);               #true
