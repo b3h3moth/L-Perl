d6e9895e8b1864dc8f05b771d620c0cfa85e1f46
B3h3m0th <behemoth@autistici.org>
2015-03-11 16:19:17 +0100

    reverse input file

---
+++ input_output/reverse.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+die "Usage: prog.pl <filename>\n" if (@ARGV != 1);
+
+#get input file
+open FILE, "<".$ARGV[0];
+my @lines = <FILE>;
+
+#print in reverse order
+for(my $i = scalar(@lines) - 1; $i >= 0; $i--) {
+    print $lines[$i];
+}
+
+close(FILE);
