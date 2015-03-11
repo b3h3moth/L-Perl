1396763f25cfb9d2e673ef169ac8e34d072e6556
B3h3m0th <behemoth@autistici.org>
2015-03-11 12:24:15 +0100

    fixed regex metacharacters, special characters

--- regex/07_special_characters.pl
+++ regex/07_special_characters.pl
@@ -7,6 +7,7 @@ use v5.14;
 my $eng_actor = "Mr. Bean";
 my $or = "a|b";
 my $home = "/home/behemoth";
+my $windir = "\\home\\docs";
 
 say "$eng_actor is an English actor" if ($eng_actor =~ m/Mr\./);    #true
 say "$eng_actor is funny :)" if ($eng_actor =~ m/M\./);             #false
@@ -15,6 +16,8 @@ say "regex" if ($or=~ m/a\|/);                                      #true
 say "works?" if ($or=~ m/b\|/);                                     #false
 print "My home dir is: " if ($home =~ m/\//);                       #true
 print "$home 1\n" if ($home =~ m/\/home/);                          #true
-print "$home 2\n" if ($home =~ m/\/[a-z]/);                         #true
-print "$home 3\n" if ($home =~ m/\/[a-z]\/[a-z]/);                  #false
-print "$home 4\n" if ($home =~ m/\/[a-z]...\/[a-z]/);               #true
+say "$home 2" if ($home =~ m/\/[a-z]/);                             #true
+say "$home 3" if ($home =~ m/\/[a-z]\/[a-z]/);                      #false
+say "$home 4" if ($home =~ m/\/[a-z]...\/[a-z]/);                   #true
+say "I prefer Unix system:" if ($windir =~ m/\\/);                  #true
+say "I can't see $windir, please!" if ($windir =~ m/\\home\\/);     #true
