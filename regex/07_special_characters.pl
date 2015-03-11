155f57ee873b8474ad3ce3e2f9e12d5a93581e0f
B3h3m0th <behemoth@autistici.org>
2015-03-11 12:06:40 +0100

    added few test on special char

--- regex/07_special_characters.pl
+++ regex/07_special_characters.pl
@@ -4,6 +4,12 @@ use strict;
 use v5.14;
 
 #to match special characters use backslah before the special character.
-my $actor = "Mr. Bean";
+my $eng_actor = "Mr. Bean";
+my $or = "a|b";
 
-say "$actor is fine :)" if ($actor =~ m/Mr\./);
+say "$eng_actor is an English actor" if ($eng_actor =~ m/Mr\./);    #true
+say "$eng_actor is funny :)" if ($eng_actor =~ m/M\./);             #false
+say "special characters" if ($or=~ m/\|/);                          #true
+say "regex" if ($or=~ m/a\|/);                                      #true
+say "works?" if ($or=~ m/b\|/);                                     #false
+
