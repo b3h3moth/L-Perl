cdf54148c6bfe2b9f65e0095d98430a5c5eaa8d5
B3h3m0th <behemoth@autistici.org>
2015-03-12 19:18:22 +0100

    fixed output

--- regex/01_transliteration_operator_3.pl
+++ regex/01_transliteration_operator_3.pl
@@ -3,13 +3,16 @@ use warnings;
 use strict;
 use v5.14;
 
-my $sequence = "The perl prograxxing language is awesoxe";
+my $sequence = "Th1 p1rl prograHHing languag1 is aw1soH1";
 
 #'tr///' substitute individual characters in the referenced string
 #'tr/X/Y/'
-$sequence =~ tr/x/m/;
-say "$sequence";
+say "Before: $sequence";
+$sequence =~ tr/H/m/;
+$sequence =~ tr/1/e/;
+say " After: $sequence";
 
 my $url_one = "h00p://iloveperl.com";
+say "Before: $url_one";
 $url_one =~ tr/0/t/;
-say "$url_one";
+say " After: $url_one";
