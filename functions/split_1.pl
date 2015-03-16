725ce71e30cb77d7fc753da0c6864f95edd4f78a
B3h3m0th <behemoth@autistici.org>
2015-03-16 17:22:05 +0100

    added comment about \s and \W

--- functions/split_1.pl
+++ functions/split_1.pl
@@ -4,8 +4,13 @@ use strict;
 
 #split(); splits a string into a list of strings and returns that list.
 
-my $string = "take a walk on the wild side";
-my @new_string = split(/\W/, $string);
+my $string = "take   a    walk  on    the    wild    side";
+
+#there are few whitespace between words.
+my @new_string = split(/\s+/, $string);
+
+#\W+ is the same thing for this kind of $string
+#my @new_string = split(/\W+/, $string);
 
 foreach my $i (@new_string) {
     print "$i\n";
