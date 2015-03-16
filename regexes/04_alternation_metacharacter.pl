532a5e76423d3e9c74d81915a05c57e752230b92
B3h3m0th <behemoth@autistici.org>
2015-03-16 10:56:34 +0100

    deleted few lines of code

--- regexes/04_alternation_metacharacter.pl
+++ regexes/04_alternation_metacharacter.pl
@@ -4,17 +4,7 @@ use strict;
 use v5.14;
 
 my $lang = "perl";
-my $sport = "football";
-my $name = "behemoth";
-my $osname = "OpenBSD";
 
 #how to create a pattern to match more than one string with '|', it's a pipe
 #or alternation metacharacter
 say "$lang is awesome" if ($lang =~ m/C|C++|AWK|Perl|Lua|Python|Lisp|Ruby/i);
-
-#use parentheses for grouping metacharacters
-say "I like $sport" if ($sport =~ m/(foot|basket|roller|speed)ball/);
-say "My name is $name" if ($name =~ m/be(he|h3|re|ge)mo(a|h|y|o|t|z|)h/);
-
-#nested
-say "$osname rulez" if ($osname =~ m/(o|O)pen(b|B)(s|S|(|d|D))/);
