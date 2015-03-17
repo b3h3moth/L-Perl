571d08e2f514e18f873138362ab53fa89de432e4
B3h3m0th <behemoth@autistici.org>
2015-03-17 19:09:08 +0100

    added comment about modifiers

--- regexes/07_modifiers.pl
+++ regexes/07_modifiers.pl
@@ -3,7 +3,8 @@ use warnings;
 use strict;
 use v5.14;
 
-#essi alterano il modo in cui una regex e' utilizzata da perl
+#a regular expression is one or more characters appended to the end of the regex
+#that modifies it
 #/i
 #/s
 #/x
