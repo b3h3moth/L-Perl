763f807009370a731526560c65590dabe9cfeae2
B3h3m0th <behemoth@autistici.org>
2015-03-17 19:58:39 +0100

    fixed comment about \x modifier

--- regexes/07_modifiers_1.pl
+++ regexes/07_modifiers_1.pl
@@ -9,7 +9,7 @@ use v5.14;
 #/i do case-insensitive pattern matching;
 #/m treat a string as a multiple line;
 #/s treat string as a single line, the dot '.' metacharacter matches \n.
-#/x ignore unescaped whitespace.
+#/x ignore unescaped whitespace and permit comments in pattern.
 
 my $book = "The master and Margarita";
 my $author = "Author of:\n"." $book\n"."is Bulgakov";
@@ -18,4 +18,4 @@ say "A: One of my favorite book is \'$book\'" if ($book =~ /^[a-z]/i); #true
 say "B: " if ($book =~ /^[a-z]/); #false
 
 say "C: $author" if ($author =~ /master.*bulgakov/si); #true
-say "D: " if ($author =~ /master.*bulgakov/s); #true
+say "D: " if ($author =~ /master.*bulgakov/s); #false
