727e7f4319c9cc097421e18ab3b55253b197037b
B3h3m0th <behemoth@autistici.org>
2015-03-17 19:17:51 +0100

    added comment about 'g' modifier

--- regexes/07_modifiers.pl
+++ regexes/07_modifiers.pl
@@ -5,7 +5,8 @@ use v5.14;
 
 #A regular expression is one or more characters appended to the end of the regex
 #that modifies it:
-#/i do case insensitive pattern matching;
-#/s treat string as a single line;
-#/x ignore unescaped whitespace;
+#/g global matching;
+#/i do case-insensitive pattern matching;
 #/m treat a string as a multiple line;
+#/s treat string as a single line;
+#/x ignore unescaped whitespace.
