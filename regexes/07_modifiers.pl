732121402488386f6ae80fbfd6c0691881952fc6
B3h3m0th <behemoth@autistici.org>
2015-03-17 19:10:50 +0100

    added comment about 'i' modifier

--- regexes/07_modifiers.pl
+++ regexes/07_modifiers.pl
@@ -3,9 +3,9 @@ use warnings;
 use strict;
 use v5.14;
 
-#a regular expression is one or more characters appended to the end of the regex
+#A regular expression is one or more characters appended to the end of the regex
 #that modifies it
-#/i
+#/i do case insensitive pattern matching
 #/s
 #/x
 #/m
