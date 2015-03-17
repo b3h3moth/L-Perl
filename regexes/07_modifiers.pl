197a149cee171c1adb73569b62f4174815bebe9b
B3h3m0th <behemoth@autistici.org>
2015-03-17 19:13:29 +0100

    added comment about 'x' modifier

--- regexes/07_modifiers.pl
+++ regexes/07_modifiers.pl
@@ -5,7 +5,7 @@ use v5.14;
 
 #A regular expression is one or more characters appended to the end of the regex
 #that modifies it
-#/i do case insensitive pattern matching
-#/s treat string as a single line
-#/x
+#/i do case insensitive pattern matching;
+#/s treat string as a single line;
+#/x ignore unescaped whitespace;
 #/m
