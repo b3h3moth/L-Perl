8923e2010b4f6572809c72c874b1a8826217b7ed
B3h3m0th <behemoth@autistici.org>
2015-03-18 19:35:21 +0100

    added example and comment about capture variables

--- regexes/10_capture_buffers.pl
+++ regexes/10_capture_buffers.pl
@@ -6,15 +6,20 @@ use v5.14;
 #Capture variables are associated with parentheses inside regex, thery are also
 #know as capture buffers; each pair of parentheses in a regex captures what its
 #contents match and memorizes it in a capture variable.
+#these special variables will be only populated if the match succeeds
 
 my $cellphone = "123-456-7890";
-say "$1 $2 $3" if ($cellphone =~ /([0-9]{3})-([0-9]{3})-([0-9]{4})/);
 
-#these special variables will be only populated if the match succeeds
+say "$1 $2 $3" if ($cellphone =~ /([0-9]{3})-([0-9]{3})-([0-9]{4})/);
+#true, matches 123 456 7890
 
 $_ = 'http://www.perlmonks.org/index.html';
 
 if (m#^http://([^/]+)(.*)#) {
-    say "host: $1";
-    say "path: $2";
+    say "host: $1"; #true, matches www.perlmonks.orf
+    say "path: $2"; #true, matches /index.html
 }
+
+my $movie = "Natural born killers, by Oliver Stone";
+
+say "$1" if ($movie =~ /\s([a-zA-Z]+),/); #true, matches killers
