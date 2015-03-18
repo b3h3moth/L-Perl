5410159841b20a4afb583cd742f45dd6e81796f0
B3h3m0th <behemoth@autistici.org>
2015-03-18 12:28:40 +0100

    added example about capture variables

--- regexes/10_capture_buffers.pl
+++ regexes/10_capture_buffers.pl
@@ -6,3 +6,10 @@ use v5.14;
 #Capture variables are associated with parentheses inside regex, thery are also
 #know as capture buffers; each pair of parentheses in a regex captures what its
 #contents match and memorizes it in a capture variable.
+
+$_ = 'http://www.perlmonks.org/index.html';
+
+if (m#^http://([^/]+)(.*)#) {
+    say "host: $1";
+    say "path: $2";
+}
