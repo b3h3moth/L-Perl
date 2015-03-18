a21a1cba6628deefe0543ae52c414153b49fc67b
B3h3m0th <behemoth@autistici.org>
2015-03-18 19:25:13 +0100

    added example about capture variables

--- regexes/10_capture_buffers.pl
+++ regexes/10_capture_buffers.pl
@@ -7,11 +7,14 @@ use v5.14;
 #know as capture buffers; each pair of parentheses in a regex captures what its
 #contents match and memorizes it in a capture variable.
 
+my $cellphone = "123-456-7890";
+say "$1 $2 $3" if ($cellphone =~ /([0-9]{3})-([0-9]{3})-([0-9]{4})/);
+
+#these special variables will be only populated if the match succeeds
+
 $_ = 'http://www.perlmonks.org/index.html';
 
 if (m#^http://([^/]+)(.*)#) {
     say "host: $1";
     say "path: $2";
 }
-
-#these special variables will be only populated if the match succeeds
