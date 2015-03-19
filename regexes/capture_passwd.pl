4bf2faf1c6e5e29d6a5dac79f7dc52c152d962b2
B3h3m0th <behemoth@autistici.org>
2015-03-19 11:20:06 +0100

    fixed source code and replaced filename

--- regexes/capture_passwd.pl
+++ regexes/capture_passwd.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use v5.14;
 
 my $file = "/etc/passwd";
 
 open(INPUT, "<", "$file") or die "Err. open file: $!";
 
 while (my $line = <INPUT>) {
-    print "login: $1 $2 $3 $4 $5\n" if ($line =~ /(\w+):(\w+):(\d+):(\d+):(\w+|\s+|\h+):/);
+    say "$1 $2 $3 $4 $5 $6" if ($line =~ /(\w+):(\w+):(\d+):(\d+):(.*):(.*)/);
 }
