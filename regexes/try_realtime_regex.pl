49495aceb107a3454c4197671f7b1845a8d97dcf
B3h3m0th <behemoth@autistici.org>
2015-03-19 18:02:52 +0100

    added Usage condition

--- regexes/try_realtime_regex.pl
+++ regexes/try_realtime_regex.pl
@@ -3,5 +3,8 @@ use warnings;
 use strict;
 use v5.14;
 
+my $string = "user:x:1000:1000:name surname:/home/user:/usr/bin/zsh";
 
-die "Usage: $0 <pattern>" if (@ARGV != 1);
+die "Usage: $0 <pattern>\n\nYou\'re working on /etc/passwd field\:\n$string"
+if (@ARGV != 1);
+
