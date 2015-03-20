53fe5b65d0c183aaf4b580c4ff67f1d1d7bffabe
B3h3m0th <behemoth@autistici.org>
2015-03-20 18:47:10 +0100

    added backreferenced regex

--- regexes/11_backreferences.pl
+++ regexes/11_backreferences.pl
@@ -3,6 +3,7 @@ use warnings;
 use strict;
 use v5.14;
 
+
 my $str = "regular expression, 771133 how many pair pf characters are there?";
 
-say "A: $1" if ($str =~ /(\d)\1+/);
+say "A: $1" if ($str =~ /(.)\g1/);
