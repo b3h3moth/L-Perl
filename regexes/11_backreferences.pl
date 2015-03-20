239806e8642d0aaf76d56cd633b6fa573eae2706
B3h3m0th <behemoth@autistici.org>
2015-03-20 18:51:11 +0100

    added new example on backreferences

--- regexes/11_backreferences.pl
+++ regexes/11_backreferences.pl
@@ -5,5 +5,10 @@ use v5.14;
 
 
 my $str = "regular expression";
+my $year = "1233456779";
 
-say "A: $1" if ($str =~ /(.)\g1/);
+#find two identical characters 'aa,'bb','cc' etc...
+say "A: $1" if ($str =~ /(\w)\g1+/);
+
+#find two identical digits '11','22','33' etc...
+say "B: $1" if ($year =~ /(\d)\g1+/);
