e6044776c424702bb64e56211e71f246b234f193
B3h3m0th <behemoth@autistici.org>
2015-03-20 19:05:33 +0100

    added comment about backreferences info

--- regexes/11_backreferences.pl
+++ regexes/11_backreferences.pl
@@ -3,6 +3,9 @@ use warnings;
 use strict;
 use v5.14;
 
+#backreferences are simply matching variables that can be used inside a regexp,
+#they are strictly associated with matching variables $1, $2, $3, etc. (outside
+#a regex), backreferences are g1, g2, g3, etc..
 
 my $str = "regular expression";
 my $year = "1233456779";
@@ -12,3 +15,6 @@ say "A: $1" if ($str =~ /(\w)\g1+/);
 
 #find two identical digits '11','22','33' etc...
 say "B: $1" if ($year =~ /(\d)\g1+/);
+
+#$1,$2,$3 outside a regex
+#g1,g2,g3 inside a regex
