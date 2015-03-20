9b5cad6aa5691f6cdfc1722e6ecd2235800c3fdd
B3h3m0th <behemoth@autistici.org>
2015-03-20 19:29:38 +0100

    fixed initial string, added 2 words backreference

--- regexes/11_backreferences.pl
+++ regexes/11_backreferences.pl
@@ -7,14 +7,17 @@ use v5.14;
 #they are strictly associated with matching variables $1, $2, $3, etc. (outside
 #a regex), backreferences are g1, g2, g3, etc..
 
-my $str = "regular expression";
+my $str = "perl regular expression expression regexp regex RegEX RE";
 my $year = "1233456779";
 
 #find two identical characters 'aa,'bb','cc' etc...
-say "A: $1" if ($str =~ /(\w)\g1+/);
+say "A: character is: $1" if ($str =~ /(\w+)\g1/);
 
+#find two identical words
+say "B: word is: $1" if ($str =~ /(\w+) \g1/);
+#
 #find two identical digits '11','22','33' etc...
-say "B: $1" if ($year =~ /(\d)\g1+/);
+say "C: digit is: $1" if ($year =~ /(\d+)\g1/);
 
 #$1,$2,$3 outside a regex
 #g1,g2,g3 inside a regex
