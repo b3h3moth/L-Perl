17eadc2d07c03b927cb63611555825056a3bf9f4
B3h3m0th <behemoth@autistici.org>
2015-03-22 12:37:16 +0100

    added comment about qr//

--- regexes/13_re_quote-like_operators_1.pl
+++ regexes/13_re_quote-like_operators_1.pl
@@ -3,5 +3,14 @@ use warnings;
 use strict;
 use v5.14;
 
-my $text = qr/sun/;
-say 'sun in shining' if ($text =~ $text);
+# qr/STRING/msixpodual
+# qr /i /m /o /s /x /p
+#
+#This operator quotes its STRING as a regular expression. STRING is
+#interpolated the same way as PATTERN in m/PATTERN/. If "'" is used as the
+#delimiter, no interpolation is done.
+#
+#Returns a Perl value which may be used instead of the corresponding
+#/STRING/msixpodual expression. The returned value is a normalized version of
+#the original pattern.
+
