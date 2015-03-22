9c80a200d7787054b9f3e44234be3804d69b423b
B3h3m0th <behemoth@autistici.org>
2015-03-22 12:42:31 +0100

    fixed comment about qr//

--- regexes/13_re_quote-like_operators_1.pl
+++ regexes/13_re_quote-like_operators_1.pl
@@ -3,14 +3,6 @@ use warnings;
 use strict;
 use v5.14;
 
-# qr/STRING/msixpodual
-# qr /i /m /o /s /x /p
-#
-#This operator quotes its STRING as a regular expression. STRING is
-#interpolated the same way as PATTERN in m/PATTERN/. If "'" is used as the
-#delimiter, no interpolation is done.
-#
-#Returns a Perl value which may be used instead of the corresponding
-#/STRING/msixpodual expression. The returned value is a normalized version of
-#the original pattern.
-
+#The regexp quote qr// does exactly that: qr/string/ compiles the string as a
+#regexp and transforms the result into a form that can be assigned to a
+#variable.
