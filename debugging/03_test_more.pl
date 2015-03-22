94b7ec668e40857e7bcc076e58c889770a4b009e
B3h3m0th <behemoth@autistici.org>
2015-03-22 14:04:49 +0100

    added is(), fixed first ok() example

--- debugging/03_test_more.pl
+++ debugging/03_test_more.pl
@@ -8,10 +8,13 @@ use v5.14;
 my $x = 10;
 my $y = 20;
 
-ok($x <= $y, 'it should be true');
+ok($x <= $y, "$x <= $y it should be true");
 ok(1, 'it should be true');
 ok(0, 'it should be false');
 ok('a', 'it should be true');
 ok('', 'it should be false');
 
+is(30, $x + $y, 'it should be true');
+is(-10, $x - $y, 'it should be true');
+is(10, $x - $y, 'it should be false');
 done_testing();
