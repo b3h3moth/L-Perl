043403effa33510df1953b4d840a5d28121bbd76
B3h3m0th <behemoth@autistici.org>
2015-03-22 19:11:35 +0100

    added $ assertion comment

--- regexes/14_assertions.pl
+++ regexes/14_assertions.pl
@@ -10,7 +10,8 @@ use v5.14;
 #\A match only at beginning of string;
 #\Z match only at end of string, or before newline at the end;
 #\z match only at end of string;
-#\G match only at pos() (e.g. at the end-of-match position of prior m//g).
+# $ match anywhere inside the string right in front of a newline;
+#\G match only at pos().
 
 my $str1 = "unix developers";
 my $str2 = "unix devel";
@@ -29,4 +30,3 @@ say "E: $&" if ($text =~ /\Bdeve\B/);  #false
 #\A
 say "F: $&" if ($str1 =~ /\Auni/);   #true
 say "G: $&" if ($text2 =~ /\Adev/);   #true, true
-
