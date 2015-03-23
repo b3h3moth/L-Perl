ac434e8ebbfa2c678a0000f51e456621402c95f6
B3h3m0th <behemoth@autistici.org>
2015-03-23 11:43:12 +0100

    added basic comments

--- subroutines/01_intro.pl
+++ subroutines/01_intro.pl
@@ -3,9 +3,14 @@ use warnings;
 use strict;
 use v5.14;
 
+#subroutine
+#sub is a key word to indicate subroutine
+#thene there's a name
 sub printout {
     say "my first Perl subroutine";
 }
 
+#first call
 printout;
+#second call
 printout();
