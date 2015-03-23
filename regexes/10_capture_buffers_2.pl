1a111c8e2c38249c850db8c37f39edb4babdf700
B3h3m0th <behemoth@autistici.org>
2015-03-23 20:25:07 +0100

    added second example, multiple addresses matching inside each array element

--- regexes/10_capture_buffers_2.pl
+++ regexes/10_capture_buffers_2.pl
@@ -6,7 +6,7 @@ use v5.14;
 #looping through multiple matches
 
 #capture each e-mail address
-my $email_list = 'bob@dylan.itjohn@from.ukluc@unix.uslarry@perl.org';
+my $email_list = 'bob@dylan.itjohn@from.ukluc@unix.frlarry@perl.us';
 
 while ($email_list =~ m/([\w\.]+@[\w\.]+\.(it|org|us|uk))/gi) {
     say "e-mail: $1";
@@ -14,3 +14,9 @@ while ($email_list =~ m/([\w\.]+@[\w\.]+\.(it|org|us|uk))/gi) {
 
 #assign the result of the matching operator to an array, each array element
 #will contain an email address
+
+my @array = ($email_list =~ m/([\w\.]+@[\w\.]+\.[a-z]{2})/gi);
+
+for (my $i=0; $i<@array; $i++) {
+    say 'from array: '.$array[$i];
+}
