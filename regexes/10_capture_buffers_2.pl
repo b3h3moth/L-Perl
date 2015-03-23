1a7c6a260c62736228c3ec6bf039887d41ec04c3
B3h3m0th <behemoth@autistici.org>
2015-03-23 20:03:01 +0100

    added example, capture e-mail address

--- regexes/10_capture_buffers_2.pl
+++ regexes/10_capture_buffers_2.pl
@@ -4,3 +4,10 @@ use strict;
 use v5.14;
 
 #looping through multiple matches
+
+#capture each e-mail address
+my $email_list = 'bob@dylan.itjohn@from.ukluc@unix.uslarry@perl.org';
+
+while ($email_list =~ m/([\w\.]+@[\w\.]+\.(it|org|us|uk))/gi) {
+    say "e-mail: $1";
+}
