98f4a8992ee6e2134ac4c890aacd9debbc39e694
B3h3m0th <behemoth@autistici.org>
2015-03-24 12:33:23 +0100

    added backticks example with array, fixed example names

--- program_output/01_backticks_operator.pl
+++ program_output/01_backticks_operator.pl
@@ -6,6 +6,12 @@ use v5.14;
 #backticks operator '`' is a common way to run commands and capture the output
 #`command`
 
-my $modules = `cat /etc/passwd`;
+my $modules = `cat /etc/modules`;
 #output returned as a string
 say $modules;
+
+my @passwd = `cat /etc/passwd`;
+#output returned as an array
+foreach my $line (@passwd) {
+    print "$line";
+}
