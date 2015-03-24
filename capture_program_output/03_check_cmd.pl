13e22589fba613537053e8c9d349aa59c085b515
B3h3m0th <behemoth@autistici.org>
2015-03-24 17:13:38 +0100

    added comments about print functions and shift

--- capture_program_output/03_check_cmd.pl
+++ capture_program_output/03_check_cmd.pl
@@ -6,7 +6,7 @@ use v5.14;
 die "Usage: $0 <unix cmd>\n" if (@ARGV != 1);
 
 my $command = shift(@ARGV);
-#the same thing
+#next line is the same thing
 #my $command = shift(@ARGV);
 
 my $path = qx/which $command/;
@@ -15,6 +15,8 @@ chomp($path);
 #$? special variable stores the exit status from any program you're running
 if ($?) {
     print STDERR "Err.running: $command - (exit status: $?)\n";
+    #next line is the same thing
+    #print "Err.running: $command - (exit status: $?)\n";
 } else {
     print STDOUT "$command: $path - (exit status: $?)\n";
 }
