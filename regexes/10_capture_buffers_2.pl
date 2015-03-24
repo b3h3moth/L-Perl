64f3e993964092f8ce92eb27bfcc8a679b6481b7
B3h3m0th <behemoth@autistici.org>
2015-03-24 12:20:56 +0100

    fixed output and comment

--- regexes/10_capture_buffers_2.pl
+++ regexes/10_capture_buffers_2.pl
@@ -9,24 +9,23 @@ use v5.14;
 my $email_list = 'bob@dylan.itjohn@from.ukluc@unix.frlarry@perl.us';
 
 while ($email_list =~ m/([\w\.]+@[\w\.]+\.(it|fr|us|uk))/gi) {
-    say "e-mail: $1";
+    say "(A) e-mail: $1";
 }
 
 #assign the result of the matching operator to an array, each array element
-#will contain an email address
-
+#will contain an email address:
 my @array = ($email_list =~ m/([\w\.]+@[\w\.]+\.[a-z]{2})/gi);
 
 for (my $i=0; $i<@array; $i++) {
-    say 'from array: '.$array[$i];
+    say '(B) e-mail: '.$array[$i];
 }
 
-#the same thing with different way
+#the same thing with different way (but it's not so pretty)
 my $x = 0;
 my @arr;
 
 while ($email_list =~ m/([\w\.]+@[\w\.]+\.(it|fr|us|uk))/gi) {
     $arr[$x] = $1;
-    say $arr[$x];
+    say '(C) e-mail: '.$arr[$x];
     $x++;
 }
