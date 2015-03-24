746b4485b29756e45956c169cb35fff75115ab5f
B3h3m0th <behemoth@autistici.org>
2015-03-24 17:11:29 +0100

    fixed output

--- capture_program_output/03_check_cmd.pl
+++ capture_program_output/03_check_cmd.pl
@@ -10,11 +10,12 @@ my $command = shift(@ARGV);
 #my $command = shift(@ARGV);
 
 my $path = qx/which $command/;
+chomp($path);
 
 #$? special variable stores the exit status from any program you're running
 if ($?) {
     print STDERR "Err.running: $command - (exit status: $?)\n";
 } else {
-    print STDOUT "path of $command: $path\n";
+    print STDOUT "$command: $path - (exit status: $?)\n";
 }
 
