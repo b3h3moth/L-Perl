55bb886e59291c872dbb117665545c4eb0da8fe4
B3h3m0th <behemoth@autistici.org>
2015-03-24 19:09:06 +0100

    added small example about system()

--- executing_external_commands.pl/04_system.pl
+++ executing_external_commands.pl/04_system.pl
@@ -3,4 +3,10 @@ use warnings;
 use strict;
 use v5.14;
 
+#system() executes the command specified but it doesn't capture the output.
 
+die "Usage: $0 <unix command>\n" if (@ARGV != 1);
+
+my $cmd = shift;
+
+system($cmd) == 0 or die "sytem($cmd) failed: $?";
