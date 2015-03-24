f57111e7243ab1e67e5abf0174358da86ea02070
B3h3m0th <behemoth@autistici.org>
2015-03-24 17:09:08 +0100

    added full example about 0, exit status special variable

--- capture_program_output/03_check_cmd.pl
+++ capture_program_output/03_check_cmd.pl
@@ -3,4 +3,18 @@ use warnings;
 use strict;
 use v5.14;
 
+die "Usage: $0 <unix cmd>\n" if (@ARGV != 1);
+
+my $command = shift(@ARGV);
+#the same thing
+#my $command = shift(@ARGV);
+
+my $path = qx/which $command/;
+
+#$? special variable stores the exit status from any program you're running
+if ($?) {
+    print STDERR "Err.running: $command - (exit status: $?)\n";
+} else {
+    print STDOUT "path of $command: $path\n";
+}
 
