c2e913b292f90dd651712397c2092c2735b2ab3e
B3h3m0th <behemoth@autistici.org>
2015-03-24 16:57:38 +0100

    added example about shift

--- functions/shift.pl
+++ functions/shift.pl
@@ -2,3 +2,14 @@
 use warnings;
 use strict;
 use v5.14;
+
+#shift function perform actions on the start of the array (the left side).
+my @club = qw(Juventus Barcellona RealMadrid Bayern);
+
+#Juventus is really my favorite club :)
+my $my_favorite_club = shift(@club);
+#now there is Barcellona etc...
+my $next_club = shift(@club);
+
+say $my_favorite_club;
+say $next_club;
