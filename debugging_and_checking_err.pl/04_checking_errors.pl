3a88945b4ebaf41dd90a27b6d592ff7bb5ec9eda
B3h3m0th <behemoth@autistici.org>
2015-03-25 12:01:38 +0100

    delete program, now file is empty

--- debugging_and_checking_err.pl/04_checking_errors.pl
+++ debugging_and_checking_err.pl/04_checking_errors.pl
@@ -3,20 +3,5 @@ use warnings;
 use strict;
 use v5.14;
 
-die "Usage: $0 <unix cmd>\n" if (@ARGV != 1);
-
-my $command = shift(@ARGV);
-#next line is the same thing
-#my $command = shift(@ARGV);
-
-my $path = qx/which $command/;
-chomp($path);
-
 #$? special variable stores the exit status from any program you're running
 #$! special variable stores the current valure of the C errno variable
-if ($? < 0) {
-    print "Failed: (exit status: $!)\n";
-} else {
-    print "Command ok\n";
-}
-
