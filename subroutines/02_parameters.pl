5989b87bbac63ead927cbcdbe19398c76241a709
B3h3m0th <behemoth@autistici.org>
2015-03-26 20:22:49 +0100

    added few example of passing data to subroutines

--- subroutines/02_parameters.pl
+++ subroutines/02_parameters.pl
@@ -10,3 +10,37 @@ sub language {
 }
 
 language('Perl');
+
+sub languages {
+    say "One of my favorite programming languages is: $_" for @_;
+}
+
+languages('C','C++','Perl','AWK','Lisp');
+
+#each invocation of shift will remove an item from the @_ array
+sub configuration1 {
+    my $os = shift;
+    my $mail_server = shift;
+    my $http_server = shift;
+    my $db_server = shift;
+
+    say "$os\n$mail_server\n$http_server\n$db_server";
+}
+
+configuration1('OpenBSD','OpenSMTPD','apache','SQLite');
+
+#list of variables
+sub configuration2 {
+    my ($os, $mail_server, $http_server, $db_server) = @_;
+    say "$os\n$mail_server\n$http_server\n$db_server";
+}
+
+configuration1('Debian','exim4','nginx','MySQL');
+
+#array
+sub configuration3 {
+    my (@array) = @_;
+    say "@array";
+}
+
+configuration3('FreeBSD','sendmail','http','MariaDB');
