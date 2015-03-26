079afd58fe7b974467173b39425d16bd16ed160f
B3h3m0th <behemoth@autistici.org>
2015-03-26 11:27:34 +0100

    added intro about subroutines

---
+++ subroutines/01_intro_1.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+#we need to create a block of codeand give it a name using 'sub' keyword
+# sub functionName {
+#    ...
+#    body of function
+#    ...
+# }
