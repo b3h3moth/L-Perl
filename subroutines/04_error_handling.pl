6a22ce3075c81670ec0bd679c61fbf6bda262a87
B3h3m0th <behemoth@autistici.org>
2015-03-27 12:01:42 +0100

    added example of caller() subroutine, default list context

--- subroutines/04_error_handling.pl
+++ subroutines/04_error_handling.pl
@@ -5,6 +5,18 @@ use v5.14;
 
 #caller() returns the context of the current subroutine call.
 
-#In list context, returns:
+#Without arguments, in list context, returns:
 # ($package, $filename, $line) = caller
 #  0         1          2
+package metaverso;
+
+metaverso();
+
+sub metaverso {
+    print_info_call();
+}
+
+sub print_info_call {
+    my ($package, $filename, $line) = caller();
+    say "Called from \'$package\' in \'$filename\' at \'$line\' line";
+}
