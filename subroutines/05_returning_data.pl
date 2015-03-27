7e735f25a7da8a9dbcaeced36582b184a8f8c5a9
B3h3m0th <behemoth@autistici.org>
2015-03-27 19:33:26 +0100

    added sum() subroutine example

--- subroutines/05_returning_data.pl
+++ subroutines/05_returning_data.pl
@@ -3,3 +3,16 @@ use warnings;
 use strict;
 use v5.14;
 
+#sometimes is usually to return data from subroutines, it can be done with
+#keyword 'return'
+
+sub sum{
+    my ($x, $y) = @_;
+    my $sum = $x + $y;
+
+    return $sum;
+}
+
+my $num = sum(10, 15);
+say "$num";
+
