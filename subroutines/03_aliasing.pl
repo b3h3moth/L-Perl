57aa5a76e4a35a469294c593585df31bb4efba9c
B3h3m0th <behemoth@autistici.org>
2015-03-27 11:35:54 +0100

    added example, @_, subroutines,

--- subroutines/03_aliasing.pl
+++ subroutines/03_aliasing.pl
@@ -5,4 +5,12 @@ use v5.14;
 
 #the default array '@_' is a local array, but its elements are aliases for the
 #actual scalar parameters.
+#Note: the first element of the array '@_' is $[0]
 
+sub reverse_name {
+    $_[0] = reverse $_[0];
+}
+
+my $name = "mister b3h3m0th";
+reverse_name($name);
+say $name;
