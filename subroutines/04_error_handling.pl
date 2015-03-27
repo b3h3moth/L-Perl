1f59ceb8823fe7cf67211ec432978e7b46681d50
B3h3m0th <behemoth@autistici.org>
2015-03-27 11:54:14 +0100

    added comments about caller()

--- subroutines/04_error_handling.pl
+++ subroutines/04_error_handling.pl
@@ -2,3 +2,9 @@
 use warnings;
 use strict;
 use v5.14;
+
+#caller() returns the context of the current subroutine call.
+
+#In list context, returns:
+# ($package, $filename, $line) = caller
+#  0         1          2
