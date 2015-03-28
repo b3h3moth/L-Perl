f98de513bcffc58999e2290f11b78bb0ccba9fe8
B3h3m0th <behemoth@autistici.org>
2015-03-28 18:49:30 +0100

    added example with carp() and croak()

--- subroutines/06_Carp_routines.pl
+++ subroutines/06_Carp_routines.pl
@@ -4,4 +4,13 @@ use strict;
 use v5.14;
 use Carp; #alternative warn and die for modules
 
+package Foo;
 
+sub foo {
+    main::carp("(carp) called at line ".__LINE__."\n, foo() was called");
+    main::croak("(croak) called at line ".__LINE__."\n, foo() was called");
+}
+
+package main;
+
+Foo::foo();
