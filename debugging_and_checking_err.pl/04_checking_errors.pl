6130746d2525f1fed68d80fef86ad4b39039c672
B3h3m0th <behemoth@autistici.org>
2015-03-28 18:37:34 +0100

    deleted file about 0

--- debugging_and_checking_err.pl/04_checking_errors.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env perl
-use warnings;
-use strict;
-use v5.14;
-
-#$? special variable stores the exit status from any program you're running
-#$! special variable stores the current valure of the C errno variable
