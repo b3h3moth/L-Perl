3d3127cab4bd313205841fa49920dc270940e523
B3h3m0th <behemoth@autistici.org>
2015-03-29 13:01:34 +0200

    added comments about wantarray(), scalar, list or void conext

--- subroutines/07_wantarray.pl
+++ subroutines/07_wantarray.pl
@@ -2,3 +2,11 @@
 use warnings;
 use strict;
 use v5.14;
+
+#The function wantarray() returns:
+# - true if the context of the currently executing subroutine or "eval" is
+#   looking for a list value;
+#
+# - false if the context is looking for a scalar;
+#
+# - the undefined value if the context is looking for no value (void context).
