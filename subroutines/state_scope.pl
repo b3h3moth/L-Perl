d73be534c182195f1d29e3fb4fbf029f09bdc990
B3h3m0th <behemoth@autistici.org>
2015-03-29 14:01:00 +0200

    added results within comments

--- subroutines/state_scope.pl
+++ subroutines/state_scope.pl
@@ -8,26 +8,26 @@ sub counter {
     return $cont++;
 }
 
-say 'a: '.counter();
-say 'a: '.counter();
-say 'a: '.counter();
+say 'a: '.counter(); #print 1
+say 'a: '.counter(); #print 2
+say 'a: '.counter(); #print 3
 
 sub counter_b {
-    my $cont = 1;
+    my $cont = 1; #scalar context
     return $cont++;
 }
 
-say 'b: '.counter_b();
-say 'b: '.counter_b();
-say 'b: '.counter_b();
+say 'b: '.counter_b(); #print 1
+say 'b: '.counter_b(); #print 1
+say 'b: '.counter_b(); #print 1
 
 sub counter_c {
     state $cont = shift;
     return $cont++;
 }
 
-say 'c: '.counter_c(2);
-say 'c: '.counter_c(4);
-say 'c: '.counter_c(6);
-say 'c: '.counter_c(8);
-say 'c: '.counter_c(10);
+say 'c: '.counter_c(2);     #print 2
+say 'c: '.counter_c(40);    #print 3
+say 'c: '.counter_c(600);   #print 4
+say 'c: '.counter_c(8800);  #print 5
+say 'c: '.counter_c(10000); #print 6
