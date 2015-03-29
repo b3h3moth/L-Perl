0d48a4b619302e81224d0d5e1529f3a2fb5da6a2
B3h3m0th <behemoth@autistici.org>
2015-03-29 17:21:03 +0200

    deleted few lines of comments

--- references/05_function_references.pl
+++ references/05_function_references.pl
@@ -3,19 +3,13 @@ use warnings;
 use strict;
 use v5.14;
 
-sub which_unix {
+sub operating_system {
     say 'I\'m on OpenBSD 5.7, real UNIX OS';
 }
 
 #reference operator '\' piu' function sigil '&'
-my $ref = \&which_unix;
+my $ref = \&operating_system;
 
 $ref->();
 
 #&$rif; works but it is not good!
-#
-#anonymous functions
-#si usa la keyword 'sub' senza un nome
-my $new_ref = sub { say 'Information wants to be free'};
-
-$new_ref->();
