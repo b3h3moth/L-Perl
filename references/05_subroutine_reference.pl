bb3c1d55204aa8301082aba72527e813c9d2d32f
B3h3m0th <behemoth@autistici.org>
2015-03-29 18:48:16 +0200

    added full example of anon. subroutines

--- references/05_subroutine_reference.pl
+++ references/05_subroutine_reference.pl
@@ -4,12 +4,50 @@ use strict;
 use v5.14;
 
 sub operating_system {
-    say 'I\'m on OpenBSD 5.7, real UNIX OS';
+    say 'OpenBSD is my OS';
 }
 
-#reference operator '\' piu' function sigil '&'
+#reference operator and function sigil '\&'
 my $ref = \&operating_system;
 
 $ref->();
+#&$rif works but it is not good!
 
-#&$rif; works but it is not good!
+my %greets = (
+
+    Franca => sub {
+        my $person = shift;
+        say "Franca: hey there, $person!";
+    },
+
+    Antonella => sub {
+        my $person = shift;
+        if ($person eq 'Antonella') {
+            say "Antonella: yes, $person";
+        } else {
+            say "Antonella: Hi, $person";
+        }
+    },
+
+    Giulia => sub {
+        my $person = shift;
+        say "Giulia: you're $person";
+    },
+
+    Luca => sub {
+        my $person = shift;
+        say "Luca: well hello $person";
+    }
+);
+
+my @room;
+
+for my $person (qw(Franca Antonella Giulia Luca)) {
+        say "$person walks into the room";
+
+        for my $room_person(@room) {
+            $greets{$person}->($room_person);
+            $greets{$room_person}->($person);
+        }
+        push @room, $person;
+    }
