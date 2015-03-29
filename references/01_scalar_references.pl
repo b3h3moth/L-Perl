eb6eb59c9662e8a89783c2a803ce4d2661970306
B3h3m0th <behemoth@autistici.org>
2015-03-29 17:42:16 +0200

    added few examples with '\' reference

--- references/01_scalar_references.pl
+++ references/01_scalar_references.pl
@@ -3,12 +3,33 @@ use warnings;
 use strict;
 use v5.14;
 
-#L'operatore per i riferimenti è il backslash \
+#You can create a reference to any named variable or subroutine with a
+#backslash '\' operator. This operator works like the & operator C-ANSI.
+
+my $foo = 'perl';
+
+my $scalarref  = \$foo;
+my $constref   = \186_282.42;
+my $arrayref   = \@ARGV;
+my $hashref    = \%ENV;
+my $coderef    = \sub {say 'hello'};
+my $globref    = \*STDOUT;
+
+say $$scalarref;
+say $$constref;
+say $$arrayref[0]; #need one argument
+say $$hashref{HOME};
+say $$coderef;
+say $$globref;
+
+#The backslash operator can do more than produce a single reference. It will
+#generate a whole list of references if applied to a list.
+
 my $os_name = 'DSBnepO';
 my $ref = \$os_name;
 
-say " address memory: $ref";    #indirizzo di memoria
-say "default OS name: $$ref";   #dereferenziazione
+say " address memory: $ref";    #address memory
+say "default OS name: $$ref";   #dereference
 
 #modifica il valore
 sub invert {
