7736d4ffd9259e97c0c212415b9f317bde0db25b
B3h3m0th <behemoth@autistici.org>
2015-03-29 19:05:56 +0200

    added full example about anon.sub., greets

---
+++ subroutines/greets.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+#anonymous subroutines example
+
+my %greets = (
+
+    Franca => sub {
+        my $person = shift;
+        say "Franca: hey there, $person!";
+    },
+
+    Antonella => sub {
+        my $person = shift;
+        if ($person eq 'Antonella') {
+            say "Antonella: yes, $person";
+        } else {
+            say "Antonella: Hi, $person";
+        }
+    },
+
+    Giulia => sub {
+        my $person = shift;
+        say "Giulia: you're $person";
+    },
+
+    Luca => sub {
+        my $person = shift;
+        say "Luca: well hello $person";
+    }
+);
+
+my @room;
+
+for my $person (qw(Franca Antonella Giulia Luca)) {
+        say "$person walks into the room";
+
+        for my $room_person(@room) {
+            $greets{$person}->($room_person);
+            $greets{$room_person}->($person);
+        }
+        push @room, $person;
+    }
