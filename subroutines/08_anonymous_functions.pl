8539c1cecd38a9e7ddc995b2dd53ed46a546f8ae
B3h3m0th <behemoth@autistici.org>
2015-03-29 17:16:00 +0200

    added example with scalar, anon.sub.

--- subroutines/08_anonymous_functions.pl
+++ subroutines/08_anonymous_functions.pl
@@ -6,6 +6,10 @@ use v5.14;
 #An anonymous function is a subroutine without a name, it behaves like a named
 #subroutine but the only way to deal with it is by reference.
 
+my $opinion = sub { say 'Information wants to be free' };
+$opinion->();
+
+
 my $login_name = 'behemoth';
 my $access = sub { print $login_name };
 &$access();
