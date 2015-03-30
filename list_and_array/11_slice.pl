593e9ba69fb4db9f4b0b5b557d837396d0aced1e
B3h3m0th <behemoth@autistici.org>
2015-03-30 21:56:07 +0200

    added comment, slice in brief

--- list_and_array/11_slice.pl
+++ list_and_array/11_slice.pl
@@ -3,6 +3,9 @@ use warnings;
 use strict;
 use v5.14;
 
+#Slice in brief: how to create a new array with elements of another array using
+#indexing.
+
 #In Perl, a slice is a way to select a few elements of an array,  list, or hash
 #instead of the entire set of data. You use the @ sign.
 
