ccbd3285bb23fb4ac8a4e2be0f480e705bf09fcd
B3h3m0th <behemoth@autistici.org>
2015-03-30 22:09:29 +0200

    added comment about output

--- list_and_array/11_slice.pl
+++ list_and_array/11_slice.pl
@@ -30,6 +30,9 @@ foreach my $num (@numbers) {
 #how to working with lists of sequential numbers
 my @number_list = (1..100);
 my @numlist = @number_list[0..9, 89..99];
+
 foreach my $n (@numlist) {
     print "$n ";
 }
+#output is:
+#1 2 3 4 5 6 7 8 9 10 90 91 92 93 94 95 96 97 98 99 100
