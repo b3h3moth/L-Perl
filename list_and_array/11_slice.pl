2f7c4f5b33955b0440d6eda403a5eb3d9023d70e
B3h3m0th <behemoth@autistici.org>
2015-03-30 20:27:11 +0200

    added slice and print solution

--- list_and_array/11_slice.pl
+++ list_and_array/11_slice.pl
@@ -6,4 +6,6 @@ use v5.14;
 #In Perl, a slice is a way to select a few elements of an array,  list, or hash
 #instead of the entire set of data. You use the @ sign.
 
-my @football_club = {'Juventus','P.S.G.','Barcellona','RealMadrid','Monaco'};
+my @football_club = ('Juventus','P.S.G.','Barcellona','RealMadrid','CB');
+my @fav_club = @football_club[0,4];
+say @fav_club;
