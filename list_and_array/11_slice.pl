b7f5eabb6ce51da807ff7cc8f0c1facd6cf70acf
B3h3m0th <behemoth@autistici.org>
2015-03-30 20:19:25 +0200

    added array @football_club for testing slice

--- list_and_array/11_slice.pl
+++ list_and_array/11_slice.pl
@@ -5,3 +5,5 @@ use v5.14;
 
 #In Perl, a slice is a way to select a few elements of an array,  list, or hash
 #instead of the entire set of data. You use the @ sign.
+
+my @football_club = {'Juventus','P.S.G.','Barcellona','RealMadrid','Monaco'};
