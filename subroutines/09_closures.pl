a3b337f4e327da910a48a77e68c36e2f6a2cb575
B3h3m0th <behemoth@autistici.org>
2015-03-31 19:44:25 +0200

    added newprint() function, closures

--- subroutines/09_closures.pl
+++ subroutines/09_closures.pl
@@ -6,3 +6,18 @@ use v5.14;
 #Closure is a notion out of the Lisp world that says if you define an anonymous
 #function in a particular lexical context, it pretends to run in that context
 #even when it's called outside the context.
+
+sub newprint {
+    my $x = shift;
+
+    return sub {
+        my $y = shift;
+        print "$x, $y!\n";
+    };
+}
+
+my $h = newprint("Howdy");
+my $g = newprint("Greetings");
+
+&$h("world");
+&$g("earthlings");
