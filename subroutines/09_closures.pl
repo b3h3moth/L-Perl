58766b3bf1b305777e212d816a88e66c6c808e6e
B3h3m0th <behemoth@autistici.org>
2015-03-31 19:38:19 +0200

    added comment, first impression on closures

--- subroutines/09_closures.pl
+++ subroutines/09_closures.pl
@@ -2,3 +2,7 @@
 use warnings;
 use strict;
 use v5.14;
+
+#Closure is a notion out of the Lisp world that says if you define an anonymous
+#function in a particular lexical context, it pretends to run in that context
+#even when it's called outside the context.
