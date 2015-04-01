8a719b0a427535a5d0a2b8414e558fcfaac4813e
B3h3m0th <behemoth@autistici.org>
2015-04-01 18:41:08 +0200

    closures example added

---
+++ subroutines/09_closures_3.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+#closures example
+
+sub make_iterator {
+    my @elem = @_;
+    my $cont = 0;
+
+    return sub {
+        return if $cont == @elem;
+        return $elem[$cont++];
+    }
+}
+
+my $langs = make_iterator (
+    qw(Perl C C++ AWK Scheme Lisp Python Prolog)
+);
+
+say $langs->() for 1 .. 5;
