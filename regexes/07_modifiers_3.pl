15ba1fa06a9901d96b6d474294db909cebc26d23
B3h3m0th <behemoth@autistici.org>
2015-04-01 12:15:09 +0200

    added code with //g modifier

--- regexes/07_modifiers_3.pl
+++ regexes/07_modifiers_3.pl
@@ -4,4 +4,8 @@ use strict;
 use v5.14;
 
 #example on the global modifier //g
+my $x = "Take a walk on the wild side (Lou Reed)";
 
+while ($x =~ /(\w+)/g) {
+    say "match \'$1\', ends at position: ", pos $x;
+}
