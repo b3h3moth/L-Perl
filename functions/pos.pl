8455e53479cd048008688d4670d34025028e39f7
B3h3m0th <behemoth@autistici.org>
2015-04-01 13:33:57 +0200

    added for loop to print both characters or offset

--- functions/pos.pl
+++ functions/pos.pl
@@ -7,7 +7,13 @@ use v5.14;
 #the variable in question; pos() directly accesses the location used by the
 #regexp engine to store the offset.
 
-my $song = 'Could you be loved (Bob Marley & The wailers)';
+my $song = "could you be loved";
+my @chars = split("", $song);
+
+for (my $i=0, my $x=1; $i<scalar(@chars); $i++, $x++) {
+    print "$chars[$i] $x";
+    say "";
+}
 
 while ($song =~ /(\w+)/g) {
     say "$1 offset: ", pos($song);
