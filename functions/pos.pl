47f0c5862394e17085b120927b2e283ec67669d0
B3h3m0th <behemoth@autistici.org>
2015-04-01 12:21:22 +0200

    added comment on pos() function

--- functions/pos.pl
+++ functions/pos.pl
@@ -2,3 +2,7 @@
 use warnings;
 use strict;
 use v5.14;
+
+#pos() function returns the offset of where the last 'm//g' search left off for
+#the variable in question; pos() directly accesses the location used by the
+#regexp engine to store the offset.
