23879606134a84620e9b93b201ceca8144e9a91d
B3h3m0th <behemoth@autistici.org>
2015-04-03 15:27:55 +0200

    example on here document

--- the_basics/11_here_document.pl
+++ the_basics/11_here_document.pl
@@ -2,3 +2,18 @@
 use warnings;
 use strict;
 use v5.14;
+
+my $date = localtime();
+my $kernel = qx(uname -v);
+chomp($kernel);
+
+#double quotes
+say <<"SYS"
+Today is:
+$date
+
+I'm running on:
+$kernel
+bye
+SYS
+
