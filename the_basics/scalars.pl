0ec034768ce4c8022e2e06b92999ee4bcfc1048d
B3h3m0th <behemoth@autistici.org>
2015-04-03 20:31:50 +0200

    added variable types and their uses

--- the_basics/scalars.pl
+++ the_basics/scalars.pl
@@ -2,7 +2,22 @@
 use warnings;
 use strict;
 
-#le variabili scalari iniziano con $
+# A scalar start with '$' sigil.
+# Variable types and their uses:
+#  Type          Sigil  Example
+#  ----------------------------------------------------------------------------
+#  Scalar        $      $cents      An individual value (number or string)
+#  Array         @      @large      A list of values, keyed by number
+#  Hash          %      %interest   A group of values, keyed by string
+#  Subroutine    &      &how        A callable chunk of Perl code
+#  Typeglob      *      *struck     Everything named struck
+#
+# A scalar is the simplest kind of data that Perl manipulates.
+# Most scalars are either a number or a string of characters.
+#
+# Although you may think of numbers and strings as very different things, Perl
+# uses them nearly interchangeably.
+
 my $codice_postale = 86100;
 my $citta_regione_cap = 'Campobasso, Molise'.' '.$codice_postale;
 print "$citta_regione_cap";
