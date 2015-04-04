c65a7c9a582c0af00cfcc8ab2931926002e2c405
B3h3m0th <behemoth@autistici.org>
2015-04-04 10:58:25 +0200

    example on qr// with pattern more complex

--- regexp/13_re_quote-like_operators_1.pl
+++ regexp/13_re_quote-like_operators_1.pl
@@ -6,6 +6,14 @@ use v5.14;
 # The Regexp-like quote qr/STRING/ compiles the string as a regexp and
 # transforms the result into a form that can be assigned to a variable.
 
+my $pattern = qr/^\d{2}(\w|-)\d{6}/;
+
+my $phone_number_home = '02-345678';
+my $phone_number_work = '02 345678';
+
+say "Good morning..." if $phone_number_home =~ /$pattern/;
+say "I'm sleeping..." if $phone_number_work =~ /$pattern/;
+
 my $str = "sunday bloody sunday by U2";
 my $text = qr/sun+day/;
 
