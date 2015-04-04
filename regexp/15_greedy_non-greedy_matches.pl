6fd5030c076a3ab186f04eb2a9e34a0c1a718078
B3h3m0th <behemoth@autistici.org>
2015-04-04 16:43:33 +0200

    test regex with like

--- regexp/15_greedy_non-greedy_matches.pl
+++ regexp/15_greedy_non-greedy_matches.pl
@@ -2,3 +2,16 @@
 use warnings;
 use strict;
 use v5.14;
+use Test::More;
+
+# Regexp get more powerful through the use of regex quantifiers, they govern
+# how often a regex component may appear in a matching string.
+#
+# The simplest quantifier is the zero or one quantifier, or ?:
+
+my $text = qr/uni?x/;
+
+like('unix', $text, "'unix' matches /uni?x/");
+like('unx', $text, "'unx' matches /uni?t/");
+
+done_testing();
