277a3e964c458932b013e97cea8b1ffb6f9e4843
B3h3m0th <behemoth@autistici.org>
2015-04-04 16:52:10 +0200

    fixed few typo.err

--- regexp/15_greedy_non-greedy_matches.pl
+++ regexp/15_greedy_non-greedy_matches.pl
@@ -7,11 +7,15 @@ use Test::More;
 # Regexp get more powerful through the use of regex quantifiers, they govern
 # how often a regex component may appear in a matching string.
 #
-# The simplest quantifier is the zero or one quantifier, or ?:
+# The simplest quantifier is 'the zero or one quantifier', or ?:
 
 my $text = qr/uni?x/;
+my $os = 'unx';
 
 like('unix', $text, "'unix' matches /uni?x/");
-like('unx', $text, "'unx' matches /uni?t/");
+like('unx', $text, "'unx' matches /uni?x/");
 
 done_testing();
+
+# The + and * quantifiers are greedy: they try to match as much of the input
+# string as possible.
