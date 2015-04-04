f060586f18ed8ed6246c73f07e7ad6d1c2a06c0b
B3h3m0th <behemoth@autistici.org>
2015-04-04 13:56:09 +0200

    added like() func to testing matches

--- regexp/13_qr_operator_2_like_testing.pl
+++ regexp/13_qr_operator_2_like_testing.pl
@@ -4,4 +4,9 @@ use strict;
 use Test::More;
 use v5.14;
 
+my $pattern = qr/^\d{3,4}(\w|-|_|\/)\d{6}/;
+my $phone_num = '039/346790';
 
+like($phone_num, $pattern, "Call me plase, $phone_num");
+
+done_testing();
