510e02999be759b0338388be1c154f3bed40c174
B3h3m0th <behemoth@autistici.org>
2015-04-04 13:58:18 +0200

    how like() works

--- regexp/13_qr_operator_2_like_testing.pl
+++ regexp/13_qr_operator_2_like_testing.pl
@@ -7,6 +7,7 @@ use v5.14;
 my $pattern = qr/^\d{3,4}(\w|-|_|\/)\d{6}/;
 my $phone_num = '039/346790';
 
+# like($got, qr/expected/, $test_name);
 like($phone_num, $pattern, "Call me plase, $phone_num");
 
 done_testing();
