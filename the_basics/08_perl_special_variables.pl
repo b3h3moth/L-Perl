1f3bef859ee8d8de88d70d54b36f26a7d06c90dd
B3h3m0th <behemoth@autistici.org>
2015-04-05 18:49:52 +0200

    added many other special variables

--- the_basics/08_perl_special_variables.pl
+++ the_basics/08_perl_special_variables.pl
@@ -18,63 +18,35 @@ use strict;
 
 # $= Holds the current page length for the default file handle. Changing the default file handle will change $= to reflect the page length of the new file handle.
 
-#$- Holds the number of lines left to print for the default file handle. Changing the default file handle will change $- to reflect the number of lines left to print for the new file handle.
+# $- Holds the number of lines left to print for the default file handle. Changing the default file handle will change $- to reflect the number of lines left to print for the new file handle.
 
-$~
+# $~ Holds the name of the default line format for the default file handle. Normally, it is equal to the file handle's name.
 
--- Holds the name of the default line format for the default file handle. Normally, it is equal to the file handle's name.
+# $^ Holds the name of the default heading format for the default file handle. Normally, it is equal to the file handle's name with _TOP appended to it.
 
-$^
+# $| If nonzero, will flush the output buffer after every write() or print() function. Normally, it is set to 0.
 
--- Holds the name of the default heading format for the default file handle. Normally, it is equal to the file handle's name with _TOP appended to it.
+# $$ This UNIX-based variable holds the process number of the process running the Perl interpreter.
 
-$|
+# $? Holds the status of the last pipe close, back-quote string, or system() function.
 
--- If nonzero, will flush the output buffer after every write() or print() function. Normally, it is set to 0.
+# $& Holds the string that was matched by the last successful pattern match.
 
-$$
+# $` Holds the string that preceded whatever was matched by the last successful pattern match.
 
--- This UNIX-based variable holds the process number of the process running the Perl interpreter.
+# $' Holds the string that followed whatever was matched by the last successful pattern match.
 
-$?
+# $+ Holds the string matched by the last bracket in the last successful pattern match. For example, the statement /Fieldname: (.*)|Fldname: (.*)/ && ($fName = $+); will find the name of a field even if you don't know which of the two possible spellings will be used.
 
--- Holds the status of the last pipe close, back-quote string, or system() function.
+# $* Changes the interpretation of the ^ and $ pattern anchors. Setting $* to 1 is the same as using the /m option with the regular expression matching and substitution operators. Normally, $* is equal to 0.
 
-$&
+# $0 Holds the name of the file containing the Perl script being executed.
 
--- Holds the string that was matched by the last successful pattern match.
+# $<number> This group of variables ($1, $2, $3, and so on) holds the regular expression pattern memory. Each set of parentheses in a pattern stores the string that match the components surrounded by the parentheses into one of the $<number> variables.
 
-$`
+# $[ Holds the base array index. Normally, it's set to 0. Most Perl authors recommend against changing it without a very good reason.
 
--- Holds the string that preceded whatever was matched by the last successful pattern match.
-
-$'
-
--- Holds the string that followed whatever was matched by the last successful pattern match.
-
-$+
-
--- Holds the string matched by the last bracket in the last successful pattern match. For example, the statement /Fieldname: (.*)|Fldname: (.*)/ && ($fName = $+); will find the name of a field even if you don't know which of the two possible spellings will be used.
-
-$*
-
--- Changes the interpretation of the ^ and $ pattern anchors. Setting $* to 1 is the same as using the /m option with the regular expression matching and substitution operators. Normally, $* is equal to 0.
-
-$0
-
--- Holds the name of the file containing the Perl script being executed.
-
-$<number>
-
--- This group of variables ($1, $2, $3, and so on) holds the regular expression pattern memory. Each set of parentheses in a pattern stores the string that match the components surrounded by the parentheses into one of the $<number> variables.
-
-$[
-
--- Holds the base array index. Normally, it's set to 0. Most Perl authors recommend against changing it without a very good reason.
-
-$]
-
--- Holds a string that identifies which version of Perl you are using. When used in a numeric context, it will be equal to the version number plus the patch level divided by 1000.
+# $] Holds a string that identifies which version of Perl you are using. When used in a numeric context, it will be equal to the version number plus the patch level divided by 1000.
 
 $"
 
