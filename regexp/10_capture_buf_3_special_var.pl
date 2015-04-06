de15b0127d962ef442812d545ddf1466422349a4
B3h3m0th <behemoth@autistici.org>
2015-04-06 15:53:22 +0200

    improved comments on $&, $` and $' special variables

--- regexp/10_capture_buf_3_special_var.pl
+++ regexp/10_capture_buf_3_special_var.pl
@@ -2,3 +2,14 @@
 use warnings;
 use strict;
 use v5.14;
+
+# Even if you don't use the grouping metacharacters, it is still possible to
+# find out what part of a string was matched to a pattern.
+#
+# There are three special variable:
+# $& capture the string matched by the last successful pattern match;
+# $` The string preceding whatever was matched by the last successful pattern
+#    match (the text to the left)
+# $' The string following whatever was matched by the last successful pattern
+#    match (the text to the right)
+
