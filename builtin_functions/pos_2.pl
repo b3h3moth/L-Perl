dec70ff152b36d7f58b76e4a23dc6dc6220afe8e
B3h3m0th <behemoth@autistici.org>
2015-04-06 15:20:00 +0200

    example on pos() function

--- builtin_functions/pos_2.pl
+++ builtin_functions/pos_2.pl
@@ -1,4 +1,10 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use v5.14;
+
+my $text = "get up stand up, stand up for your rights";
+
+while ($text =~ /(\w+)/g) {
+    my $position = pos($text);
+    print "Word \'$1\' ends at position $position\n";
+}
