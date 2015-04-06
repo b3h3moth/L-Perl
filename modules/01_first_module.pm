ed1731dde84b025a7608a8cb15fce10762bd2b0e
B3h3m0th <behemoth@autistici.org>
2015-04-06 16:30:55 +0200

    added first full module

--- modules/01_first_module.pm
+++ modules/01_first_module.pm
@@ -1,4 +1,19 @@
-#!/usr/bin/env perl
+package FirstModule;
+
 use warnings;
 use strict;
-use v5.14;
+
+# A function library (package or module) is a file of functions that can be
+# shared among programs.
+#
+# Perl module are saved with .pm suffix.
+#
+# The first line of a moldule use 'package' <name> statement
+
+sub reverse_string {
+    my $text = $_[0];
+    my $text = reverse shift;
+
+    return $text;
+}
+
