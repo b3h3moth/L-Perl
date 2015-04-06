3128b5ebbb52fa7d2b4fcc89be7b6ebf2a5a758c
B3h3m0th <behemoth@autistici.org>
2015-04-06 20:19:04 +0200

    parsing /etc/fstab file, and reading results

--- input_output/reading_and_parsing_file.pl
+++ input_output/reading_and_parsing_file.pl
@@ -1,4 +1,19 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use v5.14;
+use diagnostics;
+
+my $filename = '/etc/fstab';
+
+open(my $spies_to_espy, '<', $filename)
+    or die "Cannot open `$filename' for writing: $!";
+
+while ( my $line = <$spies_to_espy> ) {
+    next if $line =~ /^#/; # skip comments!
+    print $line;
+
+    #my ( $name, $case_number, $description ) = split /\|/, $line;
+    #print "$name ($case_number): $description\n";
+}
+
+close($spies_to_espy) or die "Could not close `$filename': $!";
