b8ed36e17b3595328eb384942581ea39babb24e9
B3h3m0th <behemoth@autistici.org>
2015-04-07 16:52:31 +0200

    added scalar and qr(), they include  and  regexes

--- input_output/reading_and_parsing_file.pl
+++ input_output/reading_and_parsing_file.pl
@@ -8,12 +8,15 @@ my $filename = '/etc/fstab';
 open(my $fh, '<', $filename)
     or die "Cannot open `$filename' for writing: $!";
 
+my $uuid        = qr/^UUID=\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/;
+my $file_system = qr/\/\w{1,4}/;
+
 while ( my $line = <$fh> ) {
     next if $line =~ /^#/; # skip comments
 
     #my ($name, $case_number, $description ) = split /\W/, $line;
     #print "$name ($case_number): $description\n";
-    print "$1 $2\n" if ($line =~ /^UUID=(\w{8}-\w{4}-\w{4}-\w{4}-\w{12})\s(\/\w{1,4})/);
+    print "$1 $2\n" if ($line =~ /($uuid)\s($file_system)/);
 }
 
 close($fh)
