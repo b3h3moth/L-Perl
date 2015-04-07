432f3d0b0df11c00990a0f2dee6b2819649f382a
B3h3m0th <behemoth@autistici.org>
2015-04-07 15:45:37 +0200

    improved regex, first field is now uuid

--- input_output/reading_and_parsing_file.pl
+++ input_output/reading_and_parsing_file.pl
@@ -7,13 +7,14 @@ my $filename = '/etc/fstab';
 
 open(my $spies_to_espy, '<', $filename)
     or die "Cannot open `$filename' for writing: $!";
+my $uuid = qr/^UUID=(\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/;
 
 while ( my $line = <$spies_to_espy> ) {
     next if $line =~ /^#/; # skip comments
 
     #my ($name, $case_number, $description ) = split /\W/, $line;
     #print "$name ($case_number): $description\n";
-    print "$1\n" if ($line =~ /^UUID=(\w{8}-\w{4})/);
+    print "$1\n" if ($line =~ /($uuid)/);
 }
 
 close($spies_to_espy) or die "Could not close `$filename': $!";
