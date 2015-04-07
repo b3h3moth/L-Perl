a7c4139645f7baf38d635d23449a4353b7d575f5
B3h3m0th <behemoth@autistici.org>
2015-04-07 15:50:57 +0200

    replaced spies_to_espy with fh

--- input_output/reading_and_parsing_file.pl
+++ input_output/reading_and_parsing_file.pl
@@ -5,16 +5,17 @@ use diagnostics;
 
 my $filename = '/etc/fstab';
 
-open(my $spies_to_espy, '<', $filename)
+open(my $fh, '<', $filename)
     or die "Cannot open `$filename' for writing: $!";
 my $uuid = qr/^UUID=(\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/;
 
-while ( my $line = <$spies_to_espy> ) {
+while ( my $line = <$fh> ) {
     next if $line =~ /^#/; # skip comments
 
     #my ($name, $case_number, $description ) = split /\W/, $line;
     #print "$name ($case_number): $description\n";
-    print "$1\n" if ($line =~ /($uuid)/);
+    print "$1 $2\n" if ($line =~ /($uuid)(\/)/);
 }
 
-close($spies_to_espy) or die "Could not close `$filename': $!";
+close($fh)
+    or die "Cannot close $filename: $!";
