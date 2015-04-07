ee811cebd830dc766f6c6c2dbdbfbf993fee998c
B3h3m0th <behemoth@autistici.org>
2015-04-07 20:21:22 +0200

    loop foreach for printing each filename

--- file_and_directory/read_dir.pl
+++ file_and_directory/read_dir.pl
@@ -2,10 +2,16 @@
 use warnings;
 use strict;
 
+my $directory = '.';
+
 opendir (my $dh, $directory)
   or die "Cannot open `$directory' for reading: $!";
+
 # get all entries not starting with a dot
-my @entries = grep { !/^\./ } readdir($dh);
+#my @entries = grep { !/^\./ } readdir($dh);
+foreach my $file (readdir $dh) {
+    print "$file\n";
+}
+
 closedir $dh
   or die "Cannot close `$directory': $!";
-
