a3a341e6f49fba8b4b322ae038d4b3004c0144c3
B3h3m0th <behemoth@autistici.org>
2015-04-07 17:13:23 +0200

    added , file system

--- input_output/parsing_fstab.pl
+++ input_output/parsing_fstab.pl
@@ -8,15 +8,15 @@ my $filename = '/etc/fstab';
 open(my $fh, '<', $filename)
     or die "Cannot open `$filename' for writing: $!";
 
+# get UUID, mount point and file system
 my $uuid        = qr/^UUID=\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/;
 my $mount_point = qr/\/\w{1,4}/;
+my $fs          = qr/\w{4}/;
 
 while ( my $line = <$fh> ) {
     next if $line =~ /^#/; # skip comments
 
-    #my ($name, $case_number, $description ) = split /\W/, $line;
-    #print "$name ($case_number): $description\n";
-    print "$1 $2\n" if ($line =~ /($uuid)\s($mount_point)/);
+    print "$1 $2 $3\n" if ($line =~ /($uuid)\s+($mount_point)\s+($fs)/);
 }
 
 close($fh)
