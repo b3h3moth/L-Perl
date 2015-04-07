fcf6e6d63a83ad2a1e309db3db74faaafd31a49e
B3h3m0th <behemoth@autistici.org>
2015-04-07 17:02:33 +0200

    replaced file_system with mount_point

--- input_output/parsing_fstab.pl
+++ input_output/parsing_fstab.pl
@@ -9,14 +9,14 @@ open(my $fh, '<', $filename)
     or die "Cannot open `$filename' for writing: $!";
 
 my $uuid        = qr/^UUID=\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/;
-my $file_system = qr/\/\w{1,4}/;
+my $mount_point = qr/\/\w{1,4}/;
 
 while ( my $line = <$fh> ) {
     next if $line =~ /^#/; # skip comments
 
     #my ($name, $case_number, $description ) = split /\W/, $line;
     #print "$name ($case_number): $description\n";
-    print "$1 $2\n" if ($line =~ /($uuid)\s($file_system)/);
+    print "$1 $2\n" if ($line =~ /($uuid)\s($mount_point)/);
 }
 
 close($fh)
