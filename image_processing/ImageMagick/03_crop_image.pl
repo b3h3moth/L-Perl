a148ac32b2b5ea1732524033d1695c62dae4e987
B3h3m0th <behemoth@autistici.org>
2015-04-08 19:24:16 +0200

    crop image

---
+++ image_processing/ImageMagick/03_crop_image.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Image::Magick;
+
+# instantiate an image object
+my $image  = Image::Magick->new;
+# with size
+# my $image2 = Image::Magick->new(size=>'200x200');
+
+my $filename = 'eve.jpg';
+my $file_copy;
+($file_copy = $filename) =~ s/^\w{3}/copy/;
+
+# read an image
+open(IMAGE, $filename);
+$image->Read(file=>\*IMAGE);
+
+$image->Crop(geometry=>'100x100');
+
+# make an image
+$image->Write($file_copy);
+
+# finally, destroying object
+undef $image;
+
+# close filehandle
+close(IMAGE);
