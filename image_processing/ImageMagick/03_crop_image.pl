20bc35f23cec0b219ba5bf0ad1142ca552b5df3b
B3h3m0th <behemoth@autistici.org>
2015-04-08 20:03:14 +0200

    fixed cropped image

--- image_processing/ImageMagick/03_crop_image.pl
+++ image_processing/ImageMagick/03_crop_image.pl
@@ -4,27 +4,20 @@ use strict;
 use v5.14;
 use Image::Magick;
 
-# instantiate an image object
 my $image  = Image::Magick->new;
-# with size
-# my $image2 = Image::Magick->new(size=>'200x200');
 
 my $filename = 'eve.jpg';
 my $file_copy;
 ($file_copy = $filename) =~ s/^\w{3}/copy/;
 
-# read an image
 open(IMAGE, $filename);
 $image->Read(file=>\*IMAGE);
 
 # crop an image
 $image->Crop(geometry=>'200x200');
 
-# make an image
+# make cropped image
 $image->Write($file_copy);
 
-# finally, destroying object
 undef $image;
-
-# close filehandle
 close(IMAGE);
