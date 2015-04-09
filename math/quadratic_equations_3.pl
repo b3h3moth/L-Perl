0b32580c2301e674a5be88d27f4ef00ce4cbb39a
B3h3m0th <behemoth@autistici.org>
2015-04-09 21:34:53 +0200

    fixed indentation

--- math/quadratic_equations_3.pl
+++ math/quadratic_equations_3.pl
@@ -5,18 +5,20 @@ use Benchmark;
 
 # Compute the larger root of a quadratic polynomial
 sub quadratic {
-my ($a, $b, $c) = @_;
-return (-$b + sqrt($b*$b - 4*$a * $c)) / 2*$a;
+    my ($a, $b, $c) = @_;
+    return (-$b + sqrt($b*$b - 4*$a * $c)) / 2*$a;
 }
-sub bruteforce {
- # Search linearly until we find a good-enough choice
-my ($low, $high) = @_;
-my $x;
-for ($x = $low; $x <= $high; $x += .001) {
-return $x if abs($x * ($x+1) - .999) < .001;
-}
-}
-timethese(10000, { quadratic => 'quadratic(1, 1, -1)',
-bruteforce => 'bruteforce(0, 1)'
-});
 
+# Search linearly until we find a good-enough choice
+sub bruteforce {
+    my ($low, $high) = @_;
+    my $x;
+
+    for ($x = $low; $x <= $high; $x += .001) {
+        return $x if abs($x * ($x+1) - .999) < .001;
+    }
+}
+
+timethese(10000, { quadratic => 'quadratic(1, 1, -1)',
+                   bruteforce => 'bruteforce(0, 1)'
+});
