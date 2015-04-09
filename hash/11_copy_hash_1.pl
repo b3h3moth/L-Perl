3e72d49105ec9e9a82c2255566d9c23f27dc1a0b
B3h3m0th <behemoth@autistici.org>
2015-04-09 19:53:33 +0200

    copy hash, tiny method

--- hash/11_copy_hash_1.pl
+++ hash/11_copy_hash_1.pl
@@ -1,4 +1,20 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use v5.14;
+
+use Data::Dumper;
+my %old_sales = (
+    monday    => { jim => 2, mary => 1 },
+    tuesday   => { jim => 3, mary => 5 },
+    wednesday => { jim => 7, mary => 3 },
+    thursday  => { jim => 4, mary => 5 },
+    friday    => { jim => 1, mary => 2 },
+);
+my %new_sales = %old_sales;
+while ( my ( $day, $sales ) = each %new_s
+    $sales->{jim} = 0;
+    $sales->{mary} = 0;
+}
+print Dumper(\%old_sales, \%new_sales);
+
+
