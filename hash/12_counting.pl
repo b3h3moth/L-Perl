fb57103dfacdd96a52ab3ac2751194f71ae29476
B3h3m0th <behemoth@autistici.org>
2015-04-09 20:18:52 +0200

    counting hash occurences

--- hash/12_counting.pl
+++ hash/12_counting.pl
@@ -1,4 +1,29 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use v5.14;
+
+my @football_club = qw(
+    Juventus Milan Inter RealMadrid juventus
+    BayernMonaco Juventus RealMadrid Manchester
+    Juventus Barcellona Juventus Arsenal Chelsea
+    Juventus Milan Inter RealMadrid juventus
+    Barcellona Juventus Arsenal Chelsea Juventus
+    Juventus Milan Inter RealMadrid Juventus
+    BayernMonaco Juventus RealMadrid Juventus Manchester
+    Barcellona Juventus Arsenal Chelsea
+    BayernMonaco Juventus RealMadrid Manchester
+);
+
+my %count;
+
+foreach (@football_club) {
+    if (exists $count{$_}) {
+         $count{$_}++;
+    } else {
+         $count{$_} = 1;
+    }
+}
+
+foreach (keys %count) {
+    print "$_   \toccurs $count{$_} time(s)\n";
+}
