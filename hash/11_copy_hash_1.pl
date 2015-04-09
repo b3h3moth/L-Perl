948412944a01054f1992ef9616fa0f033d3d69e3
B3h3m0th <behemoth@autistici.org>
2015-04-09 19:57:35 +0200

    fixed loop

--- hash/11_copy_hash_1.pl
+++ hash/11_copy_hash_1.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-
 use Data::Dumper;
+
 my %old_sales = (
     monday    => { jim => 2, mary => 1 },
     tuesday   => { jim => 3, mary => 5 },
@@ -10,11 +10,14 @@ my %old_sales = (
     thursday  => { jim => 4, mary => 5 },
     friday    => { jim => 1, mary => 2 },
 );
+
 my %new_sales = %old_sales;
-while ( my ( $day, $sales ) = each %new_s
+
+while ( my ( $day, $sales ) = each %new_sales) {
     $sales->{jim} = 0;
     $sales->{mary} = 0;
 }
+
 print Dumper(\%old_sales, \%new_sales);
 
 
