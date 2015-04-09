ffa72ef21c28b159ad04da3d21b6ccfc8d58933d
B3h3m0th <behemoth@autistici.org>
2015-04-09 21:27:56 +0200

    quadratic equations, Math::Complex

---
+++ math/quadratic_equations_2.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Math::Complex;
+ 
+($x1,$x2) = solveQuad(1,2,3);
+ 
+print "x1 = $x1, x2 = $x2\n";
+ 
+sub solveQuad
+{
+	my ($a,$b,$c) = @_;
+	my $root = sqrt($b**2 - 4*$a*$c);
+	return ( -$b + $root )/(2*$a), ( -$b - $root )/(2*$a);
+}
