6eb10dfdfbb2e0b01b382608732fb5971a4ffafc
B3h3m0th <behemoth@autistici.org>
2015-04-09 11:50:20 +0200

    autoload function test2

--- subroutines/10_autoload.pl
+++ subroutines/10_autoload.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/env perl
-
 use warnings;
 use strict;
-
 use v5.14;
+use Modern::Perl;
+
+# What happened if within a program there is a called to suborotine that
+# doesn't exist?
+kernel_version(unix => 'bsd');
+
+sub AUTOLOAD{ say 'AUTOLOAD()' }
