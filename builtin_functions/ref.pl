10293ae6320e8c7988ace27760c6d9231cc7433e
B3h3m0th <behemoth@autistici.org>
2015-04-10 11:45:40 +0200

    ref() testing on an array reference

--- builtin_functions/ref.pl
+++ builtin_functions/ref.pl
@@ -18,20 +18,28 @@ use v5.14;
 # VSTRING;
 # Regexp.
 
-my $scalar = 'perl';
-my %hash = ('lang' => 'perl');
+my $scalar  = 'perl';
+my %hash    = ('lang' => 'perl');
+my @array   = qw(nati non foste per viver come bruti);
 
-my $ref_scalar = \$scalar;
-my $ref_hash = \%hash;
+my $ref_scalar  = \$scalar;
+my $ref_hash    = \%hash;
+my $ref_array   = \@array;
 
 if (ref($ref_scalar) eq 'SCALAR') {
-    say $ref_scalar.' is a reference to scalar';
+    say $ref_scalar.' is a reference to \'scalar\'';
 } elsif (ref($ref_scalar) eq '') {
-    say 'It\'s not a scalar reference';
+    say 'It\'s not a \'scalar\' reference';
 }
 
 if (ref($ref_hash) eq 'HASH') {
-    say $ref_hash.' is a reference to hash';
+    say $ref_hash.' is a reference to \'hash\'';
 } elsif (ref($ref_hash) eq '') {
-    say 'It\'s not an hash reference';
+    say 'It\'s not an \'hash\' reference';
+}
+
+if (ref($ref_array) eq 'ARRAY') {
+    say $ref_array.' is a reference to \'array\'';
+} elsif (ref($ref_array) eq '') {
+    say 'It\'s not an \'array\' reference';
 }
