847db2ef54abdd932916e0a1c9706e08db0f20c7
B3h3m0th <behemoth@autistici.org>
2015-04-10 20:06:45 +0200

    added third method

--- lists_and_array/12_two_dimensional_arrays.pl
+++ lists_and_array/12_two_dimensional_arrays.pl
@@ -18,3 +18,11 @@ my @men_women = ( \@men, \@women);
 
 say $men_and_women[0][0].' loves '.$men_and_women[1][0];
 say $men_women[0][1].' loves '.$men_women[1][1];
+
+# Third method: by using anonymous array constructors and named variables
+my $club = ['juventus','chelsea','atletico','sampdoria'];
+my $city = ['torino','londra','madrid','genova'];
+
+my @club_and_city = ( $club, $city );
+
+say $club_and_city[0][0].' =  '.$club_and_city[1][0];
