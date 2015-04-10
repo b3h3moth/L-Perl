a04054abcce434ce5b6cb0108d3abd1832a2f35d
B3h3m0th <behemoth@autistici.org>
2015-04-10 19:55:43 +0200

    added further method by using array references

--- lists_and_array/12_two_dimensional_arrays.pl
+++ lists_and_array/12_two_dimensional_arrays.pl
@@ -8,10 +8,13 @@ use v5.14;
 my @men = ('marco','franco','luca','claudio');
 my @women = ('clara','francesca','giada','giulia');
 
-# first method: by using array references.
+# First method: by using array references:
 my $ref_men = \@men;
 my $ref_women = \@women;
 
 my @men_and_women = ( $ref_men, $ref_women );
+#It's the same thing
+my @men_women = ( \@men, \@women);
 
 say $men_and_women[0][0].' loves '.$men_and_women[1][0];
+say $men_women[0][1].' loves '.$men_women[1][1];
