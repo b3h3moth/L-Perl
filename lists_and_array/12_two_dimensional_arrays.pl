5a9bb9ca5923671670c2b22090f424c8142be92f
B3h3m0th <behemoth@autistici.org>
2015-04-10 20:08:35 +0200

    added second method

--- lists_and_array/12_two_dimensional_arrays.pl
+++ lists_and_array/12_two_dimensional_arrays.pl
@@ -13,10 +13,11 @@ my $ref_men = \@men;
 my $ref_women = \@women;
 
 my @men_and_women = ( $ref_men, $ref_women );
-#It's the same thing
-my @men_women = ( \@men, \@women);
 
 say $men_and_women[0][0].' loves '.$men_and_women[1][0];
+
+# Second method: adding references directly
+my @men_women = ( \@men, \@women);
 say $men_women[0][1].' loves '.$men_women[1][1];
 
 # Third method: by using anonymous array constructors and named variables
