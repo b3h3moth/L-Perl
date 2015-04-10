3dbd8443947aebb5058965e4e3d56bd1cf4cc190
B3h3m0th <behemoth@autistici.org>
2015-04-10 11:33:42 +0200

    ref() testing on a scalar reference

--- builtin_functions/ref.pl
+++ builtin_functions/ref.pl
@@ -2,3 +2,27 @@
 use warnings;
 use strict;
 use v5.14;
+
+# ref(EXPR) function returns a non-empty string if EXPR is a reference, the
+# empty string otherwise. The value returned depends on the type of thing the
+# reference is a reference to:
+# SCALAR;
+# ARRAY;
+# HASH;
+# CODE;
+# REF;
+# GLOB;
+# LVALUE;
+# FORMAT;
+# IO;
+# VSTRING;
+# Regexp.
+
+my $scalar = 'perl';
+my $ref_scalar = \$scalar;
+
+if (ref($ref_scalar) eq 'SCALAR') {
+    say $ref_scalar.' is a reference to scalar';
+} elsif (ref($ref_scalar) eq '') {
+    say $ref_scalar.' is not a reference';
+}
