8dba907c248268381a9e25b9863d5b9c3ef37344
B3h3m0th <behemoth@autistici.org>
2015-04-10 19:39:22 +0200

    reference to create two-dim. arrays, method 1

--- lists_and_array/12_two_dimensional_arrays.pl
+++ lists_and_array/12_two_dimensional_arrays.pl
@@ -8,4 +8,7 @@ my @men = ('marco','franco','luca','claudio');
 my @women = ('clara','francesca','giada','giulia');
 
 # first method: by using array references.
+my $ref_men = \@men;
+my $ref_women = \@women;
 
+my @men_and_women = ( $ref_men, $ref_women );
