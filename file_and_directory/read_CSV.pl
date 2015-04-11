651173b0073eb633b6506d5062989a1dd243c8d1
B3h3m0th <behemoth@autistici.org>
2015-04-11 19:18:22 +0200

    full example, how to read CSV file

--- file_and_directory/read_CSV.pl
+++ file_and_directory/read_CSV.pl
@@ -2,3 +2,18 @@
 use warnings;
 use strict;
 use v5.14;
+
+die ("Usage: $0 <CSV file>\n") if (@ARGV != 1);
+
+my @tab;
+
+while (<>) {
+    my ($first, $second, $third) = split(/,/, $_);
+    push @tab, [$first, $second, $third];
+}
+
+foreach my $row (@tab) {
+    foreach my $column (@$row) {
+        print "$column\n";
+    }
+}
