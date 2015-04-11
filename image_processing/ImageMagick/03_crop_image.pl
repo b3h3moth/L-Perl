9981d7ef75ef731fe487eeb2deacb04d073f3b23
B3h3m0th <behemoth@autistici.org>
2015-04-11 15:22:11 +0200

    improved output, fixed input/output file

--- image_processing/ImageMagick/03_crop_image.pl
+++ image_processing/ImageMagick/03_crop_image.pl
@@ -6,18 +6,21 @@ use Image::Magick;
 
 my $image  = Image::Magick->new;
 
-my $filename = 'eve.jpg';
-my $file_copy;
-($file_copy = $filename) =~ s/^.+\./crop\./;
+die ("Usage: $0 <input img> (crop)>\n") if (@ARGV != 1);
 
-open(IMAGE, $filename);
+my $file_input = $ARGV[0];
+(my $file_output = $file_input) =~ s/^.+\./crop\./;
+
+open(IMAGE, $file_input);
 $image->Read(file=>\*IMAGE);
 
 # crop an image
 $image->Crop(geometry=>'200x200');
 
 # make cropped image
-$image->Write($file_copy);
+$image->Write($file_output);
+
+say 'input img: '.$file_input."\n".'output img (cropped): '.$file_output;
 
 undef $image;
 close(IMAGE);
