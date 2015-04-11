8a7530e1d33b6bee1de1f0a5042cc21901b6c2ff
B3h3m0th <behemoth@autistici.org>
2015-04-11 15:58:52 +0200

    improved source code, now is complete

--- image_processing/ImageMagick/06_add_border.pl
+++ image_processing/ImageMagick/06_add_border.pl
@@ -4,18 +4,23 @@ use strict;
 use v5.14;
 use Image::Magick;
 
+die ("Usage: $0 <input img> (border)>\n") if (@ARGV != 1);
+
 my $image  = Image::Magick->new;
 
-my $filename = 'crop.jpg';
-my $new_file;
-($new_file = $filename) =~ s/.+\./border\./;
+my $file_input = $ARGV[0];
+(my $file_output = $file_input) =~ s/^.+\./border\./;
 
-open(IMAGE, $filename);
+open(IMAGE, $file_input);
 $image->Read(file=>\*IMAGE);
 
-# make a border
+# make border
 $image->Border(geometry=>'5x5',bordercolor=>'#ffffff');
 
-$image->Write($new_file);
+# make image with border
+$image->Write($file_output);
+
+say 'input img: '.$file_input."\n".'output img (with border): '.$file_output;
+
 undef $image;
 close(IMAGE);
