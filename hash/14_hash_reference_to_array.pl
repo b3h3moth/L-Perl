f269047ca67d1e9682753c8b0ab9387c52299c3d
B3h3m0th <behemoth@autistici.org>
2015-04-12 22:06:49 +0200

    added comment on hash reference into array

--- hash/14_hash_reference_to_array.pl
+++ hash/14_hash_reference_to_array.pl
@@ -9,4 +9,4 @@ my $record = { name => 'luca', email => 'my@mail.com'};
 # to add a hash reference to an array, push it on:
 my @database;
 push @database, $record;
-
+# @database is an array, each element is an hash reference
