3aed955d61f98c08c33878104e0bcffec174d92f
B3h3m0th <behemoth@autistici.org>
2015-04-12 19:55:31 +0200

    improved comment, added reference to no name hash

--- hash/13_anonymous_hash.pl
+++ hash/13_anonymous_hash.pl
@@ -3,4 +3,7 @@ use warnings;
 use strict;
 use v5.14;
 
-# make anonymous hash simply substitute curly bracket for the parentheses:
+# Make anonymous hash simply substitute curly bracket for the parentheses; this
+# produces a hash which has no name, but which is referenced by the variable
+# $ref_hash.
+my $ref_hash = {uno => 1, due => 2};
