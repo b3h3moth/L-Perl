4c163dd56eb90f72518c54665d9706824e3c2add
B3h3m0th <behemoth@autistici.org>
2015-04-12 20:11:36 +0200

    added IT/EN line

--- hash/13_anonymous_hash.pl
+++ hash/13_anonymous_hash.pl
@@ -17,11 +17,13 @@ say %$ref_hash;
 # dereference only keys
 my @it_colors = keys %{$ref_hash};
 
+say "IT";
 for my $colori (@it_colors) {
     say "$colori";
 }
 
 # dereference only values
+say "EN";
 my @en_colors = values %{$ref_hash};
 for my $colors (@en_colors) {
     say "$colors";
