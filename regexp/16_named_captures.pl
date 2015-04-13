9f81ec9764d736b5d966c7ee0e7db33e0fc1ba46
B3h3m0th <behemoth@autistici.org>
2015-04-13 19:18:20 +0200

    full example of named captures

--- regexp/16_named_captures.pl
+++ regexp/16_named_captures.pl
@@ -2,3 +2,15 @@
 use warnings;
 use strict;
 use v5.14;
+
+# To attach a name to a capturing group, you write either (?<name>...) or
+# (?'name'...).
+my $text = 'My telephone number is 0874-112233';
+
+my $prefix          = qr/\d{4}/;
+my $local_number     = qr/\d{6}/;
+my $telephone_number = qr/$prefix(\s|-)?$local_number/;
+
+if ($text =~ /(?<phone>$telephone_number)/ ) {
+    say "Hi, your telephone number is: $+{phone}";
+}
