e27125062303357575f3432e91bc433975c9a84c
B3h3m0th <behemoth@autistici.org>
2015-04-13 20:12:06 +0200

    added new assertions

---
+++ regexp/14_assertions_2.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+# (?!...)   zero-width negative look-ahead assertion
+# (?=...)   zero-width positive look-ahead assertion
+# (?<!...)  zero-width negaitve look-behind assertion
+# (?<=...)  zero-width positive look-behind assertion
+# \K        keep assertion
+
