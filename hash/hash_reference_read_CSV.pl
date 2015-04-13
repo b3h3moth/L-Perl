24ba1bbd5467335dc80d8b44b1c5d4ada46c5630
B3h3m0th <behemoth@autistici.org>
2015-04-13 12:02:33 +0200

    small example on hash reference to array

--- hash/hash_reference_read_CSV.pl
+++ hash/hash_reference_read_CSV.pl
@@ -5,20 +5,19 @@ use v5.14;
 
 die ("Usage: $0 <CSV file>\n") if (@ARGV != 1);
 
-my @tab;
+my @db;
 
 while (<>) {
-    my ($first, $second, $third) = split(/,/, $_);
-    push @tab, [$first, $second, $third];
+    my ($name, $city, $tel_number) = split(/,/, $_);
+    push @db, {
+        name => $name,
+        city => $city,
+        tel  => $tel_number
+    };
 }
 
-foreach my $row (@tab) {
-    foreach my $column (@$row) {
-        print "$column\n";
-    }
+foreach my $rec (@db) {
+    say $rec->{name};
 }
 
 # csv file is:
-# genova, milano, torino, venezia
-# roma, napoli, firenze, bologna
-# palermo, catania, perugia, lecce
