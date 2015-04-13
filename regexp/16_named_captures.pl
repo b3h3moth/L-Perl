07069a559c2b51a7244c01f170200f6c646660b1
B3h3m0th <behemoth@autistici.org>
2015-04-13 19:26:35 +0200

    improved comment about named captures

--- regexp/16_named_captures.pl
+++ regexp/16_named_captures.pl
@@ -3,8 +3,18 @@ use warnings;
 use strict;
 use v5.14;
 
-# To attach a name to a capturing group, you write either (?<name>...) or
-# (?'name'...).
+# Perl 5.10 added named captures, which allow you to capture portions of matches from
+# applying a regular expression and access them later.
+#
+# To attach a name to a capturing group: (?<name>...) or (?'name'...):
+#
+# Parentheses enclose the capture. The ?< name > construct immediately follows
+# the opening parenthesis and provides a name for this particular capture.
+#
+# When a match against the enclosing pattern succeeds, Perl updates the magic
+# variable %+. In this hash, the key is the name of the capture and the value
+# is the portion of the string which matched the capture.
+
 my $text = 'My telephone number is 0874-112233';
 
 my $prefix          = qr/\d{4}/;
