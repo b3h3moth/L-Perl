c9addc124add44b922b9807b6ad1b5614915282d
B3h3m0th <behemoth@autistici.org>
2015-04-13 17:38:40 +0200

    full example of length() function

--- builtin_functions/length.pl
+++ builtin_functions/length.pl
@@ -1,15 +1,16 @@
 #!/usr/bin/env perl
-length(string)
-    This function returns the number of characters in its argument. If no argument is given, length()
-returns the number of characters in Perl's default variable $_. An example of the code follows:
-#!/usr/bin/perl
-# length.pl
 use warnings;
 use strict;
-my $song = 'The Great Gig in the Sky';
-print 'length of $song: ', length($song), "\n";
-# the *real* length is 4:44
-$_ = 'Us and Them';
-print 'length of $_: ', length, "\n";
-# this one is 7:40
+use v5.14;
+
+# length(string)
+# This function returns the number of characters in its argument. If no
+# argument is given, length() returns the number of characters in Perl's
+# default variable '$_':
+
+my $song = 'Roots, Rock, Reggae';
+say 'length of $song: '.length($song);
+
+$_ = 'Positive vibration';
+print 'length of $_: '.length;
 
