49981835ce1a082f658792ed3efcc888ba34008a
B3h3m0th <behemoth@autistici.org>
2015-04-13 17:48:44 +0200

    added full example, index() function

--- builtin_functions/index.pl
+++ builtin_functions/index.pl
@@ -3,8 +3,20 @@ use warnings;
 use strict;
 use v5.14;
 
-# The index function searches for one string within another, It returns the
-# position of the first occurrence of substring in string:
+# index(str, substr, position)
+#
+# The index() function searches for one string within another, It returns the
+# position of the first occurrence of substring in string.
+#
+# The index() function has an optional third argument that indicates the
+# starting position from which it should start looking.
+#
+#
 my $string = 'The Perl language programming by Larry Wall';
 my $substr = 'ming';
-say index($string, $substr);
+
+if ((my $result = index($string, $substr)) == -1) {
+    die ("\'$substr\' was not found in \'$string\'\n");
+} else {
+    say "Found \'$substr\' in \'$string\' at index: \'$result\'";
+}
