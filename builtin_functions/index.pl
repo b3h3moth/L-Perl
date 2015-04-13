ec4ce3fe2f43c5323e7bcbca56e1ed891e410869
B3h3m0th <behemoth@autistici.org>
2015-04-13 17:30:00 +0200

    added index() example

--- builtin_functions/index.pl
+++ builtin_functions/index.pl
@@ -3,5 +3,8 @@ use warnings;
 use strict;
 use v5.14;
 
-# The index function searches for one string within another.
-
+# The index function searches for one string within another, It returns the
+# position of the first occurrence of substring in string:
+my $string = 'The Perl language programming by Larry Wall';
+my $substr = 'ming';
+say index($string, $substr);
