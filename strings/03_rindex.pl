91461c1b4fadff4fabb8a7c2df8400e260bad142
B3h3m0th <behemoth@autistici.org>
2015-04-14 19:45:28 +0200

    added comment about rindex()

--- strings/03_rindex.pl
+++ strings/03_rindex.pl
@@ -2,3 +2,12 @@
 use warnings;
 use strict;
 use v5.14;
+
+# rindex(string, substring)
+# The rindex()function is similar to index() except that it searches the string
+# from right to left, instead ofleft to right.
+#
+# Except for the name of the function itself, the syntax for calling rindex()
+# is exactly the same as for index().
+#
+# It returns ­1 if the substring is not found.
