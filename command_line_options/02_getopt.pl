4af028209f52ad23753b5e6bc14e14225fdbc518
B3h3m0th <behemoth@autistici.org>
2015-04-14 20:18:07 +0200

    added basics getopts()

--- command_line_options/02_getopt.pl
+++ command_line_options/02_getopt.pl
@@ -2,3 +2,22 @@
 use warnings;
 use strict;
 use v5.14;
+use Getopt::Std;
+
+my $usage = "Usage: $0 [options] <arguments...>
+
+Options:
+-h help
+-v version
+";
+
+die $usage unless @ARGV;
+
+my %opt;
+
+getopts('hv:', \%opt);
+
+if ($opt{h}) {print $usage; exit}
+if ($opt{v}) {print "version 1.0\n"; exit}
+
+say "Other arguments were: @ARGV";
