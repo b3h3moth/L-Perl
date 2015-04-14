55985f69789da348a5d3e9596d4825c56b27012c
B3h3m0th <behemoth@autistici.org>
2015-04-14 19:41:27 +0200

    added full example about index()

--- strings/01_index_2.pl
+++ strings/01_index_2.pl
@@ -2,3 +2,18 @@
 use warnings;
 use strict;
 use v5.14;
+
+die("Usage: $0 <string><substring>\n") if (@ARGV != 2);
+
+chomp(my $string = $ARGV[0]);
+chomp(my $substring = $ARGV[1]);
+
+my $result = index($string, $substring);
+
+if ($result != -1) {
+    say 'string: '.$string;
+    say "the substring was found at index: $result";
+} else {
+    say "the substring was not found";
+}
+
