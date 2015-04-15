5cf82c201887478250ec94d84982f72ccc28193e
B3h3m0th <behemoth@autistici.org>
2015-04-15 19:37:29 +0200

    improved code and comments

--- regexp/04_alternation_metacharacter.pl
+++ regexp/04_alternation_metacharacter.pl
@@ -3,8 +3,7 @@ use warnings;
 use strict;
 use v5.14;
 
-my $lang = "perl";
-
 #how to create a pattern to match more than one string with '|', it's a pipe
 #or alternation metacharacter
+my $lang = "perl";
 say "$lang is awesome" if ($lang =~ m/C|C++|AWK|Perl|Lua|Python|Lisp|Ruby/i);
