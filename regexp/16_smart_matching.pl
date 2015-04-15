f3a1ce09c68c2c371f1e4f7182a77a6f7ae0e3c2
B3h3m0th <behemoth@autistici.org>
2015-04-15 20:01:58 +0200

    example? of smart matching

--- regexp/16_smart_matching.pl
+++ regexp/16_smart_matching.pl
@@ -5,3 +5,7 @@ use v5.20.1;
 
 # The smart match operator '~~' compares two operands and returns a true value
 # if they match.
+my $left_operand;
+my $right_operand;
+
+say 'I don\'t know why but they match' if ($left_operand ~~ $right_operand);
