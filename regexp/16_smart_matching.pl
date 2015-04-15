1772590d8e1fa31c5ff1ea9d95286d2cff6a81a8
B3h3m0th <behemoth@autistici.org>
2015-04-15 19:58:01 +0200

    fixed comment

--- regexp/16_smart_matching.pl
+++ regexp/16_smart_matching.pl
@@ -3,5 +3,5 @@ use warnings;
 use strict;
 use v5.20.1;
 
-# The smart match operator, ~~, compares two operands and returns a true value
+# The smart match operator '~~' compares two operands and returns a true value
 # if they match.
