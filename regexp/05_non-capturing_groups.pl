81c0e21d6008052dd7c0f712dc806d30df4eb4bb
B3h3m0th <behemoth@autistici.org>
2015-04-15 19:33:45 +0200

    example with (?:)

--- regexp/05_non-capturing_groups.pl
+++ regexp/05_non-capturing_groups.pl
@@ -4,3 +4,5 @@ use strict;
 use v5.20.1;
 
 # (?:) groups a series of atoms without making a capture.
+my $band = "Bob Marley & the Wailers";
+say "A: I Love Reggae music" if ($band =~ /(?:bob|marley|wailers)/i);
