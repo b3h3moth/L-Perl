9b376fd8683858525fc78f7ff7b5789cfa0d65fc
B3h3m0th <behemoth@autistici.org>
2015-04-16 22:13:16 +0200

    removed line, output elements

--- lists_and_array/13_passing_array_to_subroutine.pl
+++ lists_and_array/13_passing_array_to_subroutine.pl
@@ -8,7 +8,6 @@ my @names_a = qw(luca franco giovanni angela pino alberto guido giusy maria);
 my @names_b = qw(giusy franco claudia giovanna maurizio guido mario maria);
 
 my @array_test = compare_array(\@names_a, \@names_b);
-my $len_arr = @array_test;
 
 print Dumper \@array_test;
 
@@ -18,7 +17,6 @@ sub compare_array {
 
     for (my $x=0; $x < @$arr1; $x++) {
         for (my $y=0; $y < @$arr2; $y++) {
-            #say "$arr1->[$x] = $arr2->[$y]";
             if ($arr1->[$x] eq $arr2->[$y]) {
                 push(@result, $arr1->[$x]);
             }
