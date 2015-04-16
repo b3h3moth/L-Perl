b7ca97a92356c0424d96f5e294e9614369358344
B3h3m0th <behemoth@autistici.org>
2015-04-16 20:06:52 +0200

    compare_array() subroutine added

--- lists_and_array/13_passing_array_to_subroutine.pl
+++ lists_and_array/13_passing_array_to_subroutine.pl
@@ -6,4 +6,16 @@ use v5.14;
 my @names = qw(luca alberto guido giusy);
 my @country = qw(italy france greece spain hollan);
 
+sub compare_array {
+    my (@arr1, @arr2) = @_;
+
+    foreach my $elem1 (@arr1) {
+        foreach my $elem2 (@arr2) {
+            if ($arr1[$elem1] eq $arr2[$elem2]) {
+                say 'uguali';
+            }
+        }
+    }
+}
+
 
