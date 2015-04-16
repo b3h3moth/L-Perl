574317b53a11fe97d9ce08c81c70f06d3d064993
B3h3m0th <behemoth@autistici.org>
2015-04-16 21:56:04 +0200

    replaced array names

--- lists_and_array/13_passing_array_to_subroutine.pl
+++ lists_and_array/13_passing_array_to_subroutine.pl
@@ -3,17 +3,20 @@ use warnings;
 use strict;
 use v5.14;
 
-my @names = qw(luca alberto guido giusy);
-my @country = qw(italy france greece spain hollan);
+my @names_a = qw(l alberto guido giusy);
+my @names_b = qw(italy france greece spain hollan);
 
-compare_array(\@names, \@country);
+compare_array(\@names_a, \@names_b);
 
 sub compare_array {
     my ($arr1, $arr2) = @_;
 
     for (my $x=0; $x < @$arr1; $x++) {
         for (my $y=0; $y < @$arr2; $y++) {
-            say "$arr1->[$x] = $arr2->[$y]";
+            #say "$arr1->[$x] = $arr2->[$y]";
+            if ($arr1->[$x] eq $arr2->[$y]) {
+                say 'equal';
+            }
         }
     }
 }
