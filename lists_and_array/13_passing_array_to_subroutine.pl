cb5c137f45471fc5b6864d8c53a4db9745ad52f1
B3h3m0th <behemoth@autistici.org>
2015-04-16 22:07:17 +0200

    added temp array to store results

--- lists_and_array/13_passing_array_to_subroutine.pl
+++ lists_and_array/13_passing_array_to_subroutine.pl
@@ -6,24 +6,21 @@ use v5.14;
 my @names_a = qw(luca alberto guido giusy);
 my @names_b = qw(giusy franco claudia giovanna);
 
-if (compare_array(\@names_a, \@names_b) == 0) {
-    say 'compare_array() returns 0, true';
-} else {
-    say 'compare_array() returns 0, false';
-}
+my @array_test = compare_array(\@names_a, \@names_b);
 
 
 sub compare_array {
     my ($arr1, $arr2) = @_;
+    my @result;
 
     for (my $x=0; $x < @$arr1; $x++) {
         for (my $y=0; $y < @$arr2; $y++) {
             #say "$arr1->[$x] = $arr2->[$y]";
             if ($arr1->[$x] eq $arr2->[$y]) {
-                return 0;
-            } else {
-                return -1;
+                push(@result, $arr1->[$x]);
             }
         }
     }
+
+    return @result;
 }
