c04440cbc4c25638b0357a1931547a907aa935e4
B3h3m0th <behemoth@autistici.org>
2015-04-17 22:19:29 +0200

    added comment on unless

--- control_flow/01_unless.pl
+++ control_flow/01_unless.pl
@@ -2,3 +2,8 @@
 use warnings;
 use strict;
 use v5.14;
+
+# In an if control structure, the block of code is executed only when the
+# conditional expression is true. If you want to execute a block of code only
+# when the conditional is false, change if to unless.
+
