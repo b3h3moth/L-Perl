548de76da88a5d96fe6eb521435bfdbb126b7057
B3h3m0th <behemoth@autistici.org>
2015-04-17 18:21:10 +0200

    improved code

--- hash/15_passing_hash_to_subroutine.pl
+++ hash/15_passing_hash_to_subroutine.pl
@@ -6,22 +6,16 @@ use Data::Dumper;
 
 my %hash_club_a = (
     juventus    => 'torino',
-    milan       => 'milano',
-    inter       => 'milano',
     sampdoria   => 'genova',
     genoa       => 'genova',
-    arsenal     => 'londra',
     chelsea     => 'londra'
 );
 
 my %hash_club_b = (
     atletico    => 'madrid',
+    arsenal     => 'londra',
     barca       => 'barcellona',
-    lazio       => 'roma',
-    roma        => 'roma',
-    campobasso  => 'campobasso',
     juventus    => 'torino',
-    dinamo      => 'kiev'
 );
 
 my %hash_test = compare_hash(\%hash_club_a, \%hash_club_b);
@@ -35,17 +29,8 @@ sub compare_hash {
 
     my $rifa = keys %$rif_a;
     my $rifb = keys %$rif_b;
+
     say "$rifa:$rifb";
-    exit;
-    for (my $x=0; $x < @$rif_a; $x++) {
-        for (my $y=0; $y < @$rif_b; $y++) {
-            # useful for debugging purpose, compare each array element
-            # say "$rif_a->[$x] = $rif_b->[$y]";
-            if ($rif_a->[$x] eq $rif_b->[$y]) {
-                push(@result, $rif_a->[$x]);
-            }
-        }
-    }
 
     return %result;
 }
