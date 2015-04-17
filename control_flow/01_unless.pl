a4c4c52262a4693dc2fe4c270420e08d9c8bb59d
B3h3m0th <behemoth@autistici.org>
2015-04-17 22:23:29 +0200

    fixed name

--- control_flow/01_unless.pl
+++ control_flow/01_unless.pl
@@ -6,4 +6,5 @@ use v5.14;
 # In an if control structure, the block of code is executed only when the
 # conditional expression is true. If you want to execute a block of code only
 # when the conditional is false, change if to unless.
-
+my $name = 'wisp';
+say "You're not $name" unless ($name eq 'wow');
