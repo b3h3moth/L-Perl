3ec59c0faaa20967ff8d3b39582b9d5a8df4219f
B3h3m0th <behemoth@autistici.org>
2015-04-18 22:24:24 +0200

    sha1_hex example

--- strings/sha_string.pl
+++ strings/sha_string.pl
@@ -6,5 +6,8 @@ use Digest::SHA qw(sha1 sha1_hex sha1_base64 sha256 sha384_hex sha512_base64);
 
 # How to calculate SHA1 on a string
 my $text = "no woman, no cry";
-my $result_sha1 = sha1($text);
-say $result_sha1;
+my $result = sha1($text);
+say 'sha1: '.$result;
+undef $result;
+$result = sha1_hex($text);
+say 'sha1_hex: '.$result;
