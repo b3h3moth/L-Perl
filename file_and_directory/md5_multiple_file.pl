eedd6bbe3fb90fa1838d73459afdcaca6111f98e
B3h3m0th <behemoth@autistici.org>
2015-04-18 20:05:02 +0200

    example, how to calculate md5 hash on multiple file

---
+++ file_and_directory/md5_multiple_file.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Digest::MD5; 
+
+# How to calculate md5 hash of a multiple files
+
+die("Usage: $0 <filename 1><filename 2>...<filename N>\n") if (@ARGV == 0);
+
+my (@infile) = @ARGV;
+
+for my $file (@infile) {
+    open(FILE, '<', "$file") 
+        or die("Can't open file \'$file\': $!\n");
+    
+    my $result = Digest::MD5->new->addfile(*FILE)->hexdigest;
+    
+    say $file."\t".$result;
+    
+    close(FILE);
+}
