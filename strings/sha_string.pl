e9bcb7018c61c7577370d0b4a643d8b6523e1364
B3h3m0th <behemoth@autistici.org>
2015-04-18 22:20:15 +0200

    sha1 example

--- strings/sha_string.pl
+++ strings/sha_string.pl
@@ -2,3 +2,9 @@
 use warnings;
 use strict;
 use v5.14;
+use Digest::SHA qw(sha1 sha1_hex sha1_base64 sha256 sha384_hex sha512_base64);
+
+# How to calculate SHA1 on a string
+my $text = "no woman, no cry";
+my $result_sha1 = sha1($text);
+say $result_sha1;
