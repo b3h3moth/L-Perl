15aa8c34c0d49570afe5e58009936ca9139878d6
B3h3m0th <behemoth@autistici.org>
2015-04-18 19:43:56 +0200

    open file with open() and check errors

--- file_and_directory/md5.pl
+++ file_and_directory/md5.pl
@@ -7,3 +7,8 @@ use Digest::MD5;
 # How to calculate md5 hash of a file
 
 die("Usage: $0 <filename>\n") if (@ARGV != 1);
+
+my ($infile) = $ARGV[0];
+
+open(FILE, <, "$infile") or die("Can't open $infile: $!\n");
+close(FILE);
