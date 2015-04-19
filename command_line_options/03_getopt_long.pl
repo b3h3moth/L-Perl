6b1c19f9b698b248738f7f125afa69dbe40471ee
B3h3m0th <behemoth@autistici.org>
2015-04-19 19:53:54 +0200

    Usage options added

--- command_line_options/03_getopt_long.pl
+++ command_line_options/03_getopt_long.pl
@@ -3,3 +3,17 @@ use warnings;
 use strict;
 use v5.14;
 use Getopt::Long;
+
+my $usage = "Usage: $0 [options] <arguments...>
+
+Options:
+--version
+--help
+--flag
+--number <number>
+--string <string>
+";
+
+my $flag;
+my $number;
+my $string;
