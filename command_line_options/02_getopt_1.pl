8a297026fafd988f76f0eb4a0d6a248de8b0cb4b
B3h3m0th <behemoth@autistici.org>
2015-04-19 18:12:58 +0200

    die() comment

--- command_line_options/02_getopt_1.pl
+++ command_line_options/02_getopt_1.pl
@@ -5,6 +5,8 @@ use v5.14;
 use Getopt::Std;
 
 # Getopt::Std is used for single-character options
+
+# options supported by the program
 my $usage = "Usage: $0 [options] <arguments...>
 
 Options:
@@ -12,6 +14,8 @@ Options:
 -v version
 ";
 
+# the die() function on line 12 will print the usage message only if the 
+# program is not given any arguments on the command line. 
 die $usage unless @ARGV;
 
 my %opt;
