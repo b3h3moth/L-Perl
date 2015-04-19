ae06eb3e36326bb812dc9751346eade100e57441
B3h3m0th <behemoth@autistici.org>
2015-04-19 19:57:38 +0200

    added GetOptions

--- command_line_options/03_getopt_long.pl
+++ command_line_options/03_getopt_long.pl
@@ -17,3 +17,23 @@ Options:
 my $flag;
 my $number;
 my $string;
+
+GetOptions(
+    "flag"      => \$flag,
+    "number=i"  => \$number,
+    "string=s"  => \$string,
+    "version"   => sub {print "1.0\n"; exit},
+    "help"      => sub {print $usage; exit},
+);
+
+if ($flag) {
+    print "flag turned on\n";
+}
+     
+if ($string) {
+    print "string set: $string\n";
+}
+     
+if ($number) {
+    print "number set: $number\n";
+}
