518e4ad929d60e07c37c1a131033b166bf030b6b
B3h3m0th <behemoth@autistici.org>
2015-04-20 16:16:22 +0200

    perl special character's list

--- the_basics/special_characters.pl
+++ the_basics/special_characters.pl
@@ -3,6 +3,16 @@ use warnings;
 use strict;
 use v5.14;
 
+# \n newline
+# \r return
+# \t tab
+# \\ backslash
+# \' single quote
+# \" double quote
+# \l lowercase next character
+# \L lowercase all folowing character until \E
+# \u uppercase next character
+# \U uppercase all folowing character until \E
 my $text = "\lPerl";
 my $name = "\ul\Uuca";
 say $text.' '.$name;
