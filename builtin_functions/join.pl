36cda835409580e946ea9c5bcfb50a7159f9cc6c
B3h3m0th <behemoth@autistici.org>
2015-04-21 20:21:53 +0200

    full example on join() added

--- builtin_functions/join.pl
+++ builtin_functions/join.pl
@@ -2,3 +2,15 @@
 use warnings;
 use strict;
 use v5.14;
+
+# join(expr, list)
+# Joins the separate strings of list into a single string with fields separated
+# by the value of expr, and returns that new string.
+
+my @words = qw(stand-up for your);
+my $word = 'rights';
+my @band = qw(Bob Marley & the Wailers);
+
+my $text = join('__', "get-up","stand-up", @words, $word, "by", @band);
+
+say $text;
