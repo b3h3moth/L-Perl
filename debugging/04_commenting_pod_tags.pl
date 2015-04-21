a40b6d2a0b953b0bf56875ce1679dc7b1b22fc96
B3h3m0th <behemoth@autistici.org>
2015-04-21 13:42:02 +0200

    pod tags example

--- debugging/04_commenting_pod_tags.pl
+++ debugging/04_commenting_pod_tags.pl
@@ -2,3 +2,13 @@
 use warnings;
 use strict;
 use v5.14;
+
+my @skill = qw(C C++ Perl Python Zsh Bash TCP/IP Lisp Awk Regex);
+=debug
+my $lang = 'perl';
+say "$lang programming";
+each line is a comment
+=cut
+for (@skill) {
+    say $_;
+}
