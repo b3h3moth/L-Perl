19aa2cca3f60633719656a33149d57c552e90af4
B3h3m0th <behemoth@autistici.org>
2015-04-21 19:13:26 +0200

    initialize multiple elements

--- hash/16_hash_slice.pl
+++ hash/16_hash_slice.pl
@@ -2,3 +2,16 @@
 use warnings;
 use strict;
 use v5.14;
+
+# A hash slice is a list of keys or values of a hash indexed in a single 
+# operation. 
+
+my %colors = (
+    bl => 'blue',
+    or => 'orange',
+    wh => 'white',
+);
+
+# To initialize multiple elements of a hash at once:
+@colors{qw(gr cy ma re)} = (1) x 4;
+
