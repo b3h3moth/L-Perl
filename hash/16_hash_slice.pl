52bc4656b8ba3a706830f3cb29b053e36bd5e84d
B3h3m0th <behemoth@autistici.org>
2015-04-21 19:23:53 +0200

    info about slice concept in Perl

--- hash/16_hash_slice.pl
+++ hash/16_hash_slice.pl
@@ -3,14 +3,17 @@ use warnings;
 use strict;
 use v5.14;
 
-# A hash slice is a list of keys or values of a hash indexed in a single 
-# operation. 
+# A slice is a way to select a few elements of an array, list, or hash instead 
+# of the entire set of data. You use the @ sign.
 
 my %colors = (
     bl => 'blue',
     or => 'orange',
     wh => 'white',
 );
+
+# A hash slice is a list of keys or values of a hash indexed in a single 
+# operation. 
 
 # To initialize multiple elements of a hash at once:
 @colors{qw(green cyan red gray violet)} = (0) x 5 ;
