127cb553baa4e06dbbcb20420e5ffcfce7896f13
B3h3m0th <behemoth@autistici.org>
2015-04-21 13:57:12 +0200

    added commenting with __END__ full example

--- debugging/05_commenting_with_END.pl
+++ debugging/05_commenting_with_END.pl
@@ -2,3 +2,21 @@
 use warnings;
 use strict;
 use v5.14;
+
+# Perl stops parsing the script once it sees the __END__ token.
+my %club = (
+    juventus     => 'serie A',
+    arsenal      => 'premier',
+    barcellona   => 'liga',
+    psg          => 'league 1',
+    bayernMonaco => 'bundesliga',
+);
+
+while (my ($name, $serie) = each %club) {
+    say "$name plays to $serie";
+}
+__END__
+a
+lot
+of
+comments
