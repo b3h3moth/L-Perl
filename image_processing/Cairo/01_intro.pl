b9440c01b06136c440d64e13ee53437e66381519
B3h3m0th <behemoth@autistici.org>
2015-04-22 17:48:14 +0200

    basics example with Cairo

--- image_processing/Cairo/01_intro.pl
+++ image_processing/Cairo/01_intro.pl
@@ -2,3 +2,19 @@
 use warnings;
 use strict;
 use v5.14;
+use Cairo;
+
+my $surface = Cairo::ImageSurface->create ('argb32', 100, 100);
+my $cr = Cairo::Context->create ($surface);
+
+$cr->rectangle (10, 10, 40, 40);
+$cr->set_source_rgb (0, 0, 0);
+$cr->fill;
+
+$cr->rectangle (50, 50, 40, 40);
+$cr->set_source_rgb (1, 1, 1);
+$cr->fill;
+
+$cr->show_page;
+
+$surface->write_to_png ('output.png');
