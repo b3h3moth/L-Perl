3675ff0a3165375bae6daacb9218407b2fe0fa8d
B3h3m0th <behemoth@autistici.org>
2015-04-22 18:23:41 +0200

    full source code revision

--- image_processing/Cairo/01_intro.pl
+++ image_processing/Cairo/01_intro.pl
@@ -7,17 +7,15 @@ use Cairo;
 # Cairo provides Perl bindings for the 2D vector graphics library cairo.  
 # It supports multiple output targets, including PNG, PDF and SVG.  
 # Cairo produces identical output on all those targets.
-my $surface = Cairo::ImageSurface->create ('argb32', 100, 100);
 
-# Cairo::Context is the main object.
-my $cr = Cairo::Context->create ($surface);
+# Create a drawing surface, it's 100x100
+my $surface = Cairo::ImageSurface->create('argb32', 100, 100);
 
-$cr->rectangle (10, 10, 40, 40);
+# Create a drawing context. Cairo::Context is the main object.
+my $cr = Cairo::Context->create($surface);
+
+$cr->rectangle (50, 50, 0, 0);
 $cr->set_source_rgb (0, 0, 0);
-$cr->fill;
-
-$cr->rectangle (50, 50, 40, 40);
-$cr->set_source_rgb (1, 1, 1);
 $cr->fill;
 
 $cr->show_page;
