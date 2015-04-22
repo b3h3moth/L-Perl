6dfa29656c5851c1bc8f537b5d72307dc16f7843
B3h3m0th <behemoth@autistici.org>
2015-04-22 12:29:49 +0200

    added hash

--- hash/17_hash_idioms.pl
+++ hash/17_hash_idioms.pl
@@ -2,3 +2,32 @@
 use warnings;
 use strict;
 use v5.14;
+
+my %hash = (
+    A => 189,
+    nome => 'luca',
+    arsenal => 'londra',
+    tmux => 'multiplexer',
+    vim => 'editor',
+    A => 189,
+    nome => 'luca',
+    A => 189,
+    nome => 'luca',
+    lang => 'perl',
+    vim => 'editor',
+    asso => 'picche',
+    asso => 'quadri',
+    B => 89,
+    arsenal => 'londra',
+    tmux => 'multiplexer',
+    vim => 'editor',
+    C => 110,
+    asso => 'fiori',
+    asso => 'cuori',
+    juventus => 'torino',
+    lang => 'perl',
+    asso => 'picche',
+    asso => 'quadri',
+    B => 89,
+);
+
