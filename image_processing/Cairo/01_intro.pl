21d48dceb973c722b317399f78f5ed368a49a124
B3h3m0th <behemoth@autistici.org>
2015-04-22 18:45:33 +0200

    fixed comments and source code

--- image_processing/Cairo/01_intro.pl
+++ image_processing/Cairo/01_intro.pl
@@ -4,12 +4,12 @@ use strict;
 use v5.14;
 use Cairo;
 
+# Scope of the program is to create Italian Flag.
+my $italian_flag = 'italia.png';
+
 # Cairo provides Perl bindings for the 2D vector graphics library cairo.  
 # It supports multiple output targets, including PNG, PDF and SVG.  
 # Cairo produces identical output on all those targets.
-
-# Scope of the program is to create Italian Flag.
-my $italian_flag = 'italia.png';
 
 # Create a drawing surface, it's 100x100
 my $surface = Cairo::ImageSurface->create('argb32', 100, 100);
@@ -17,11 +17,11 @@ my $surface = Cairo::ImageSurface->create('argb32', 100, 100);
 # Create a drawing context. Cairo::Context is the main object.
 my $cr = Cairo::Context->create($surface);
 
-# green strip
 # Create a rectangle (x, y, width, height)
+# green strip
 $cr->rectangle (0, 0, 33, 100);
 # Set the colour of the firt stripe (Red, Green, Blue)
-$cr->set_source_rgb (0, 25, 0);
+$cr->set_source_rgb (0, 1, 0);
 # Fill the rectangle
 $cr->fill;
 
