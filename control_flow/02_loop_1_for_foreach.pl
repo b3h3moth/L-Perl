7895d873ec28cc4c4f63c745a70abb07a9c8b1e9
B3h3m0th <behemoth@autistici.org>
2015-04-23 18:57:15 +0200

    improved each loop method

--- control_flow/02_loop_1_for_foreach.pl
+++ control_flow/02_loop_1_for_foreach.pl
@@ -5,20 +5,16 @@ use v5.14;
 use Test::More;
 use Data::Dumper;
 
-# foreach loop
-foreach (1 .. 5)
-{
-    say "A: $_ * $_ = ", $_ * $_;
-}
-
 # postfix 
 say "B: $_ * $_ = ", $_ * $_ for 6 .. 10;
 
+# for mith scalar
 for my $i (1 .. 5)
 {
     say "C: $i * $i = ", $i * $i;
 }
 
+# for and list
 my @numbers = (1..5);
 for (@numbers)
 {
@@ -26,15 +22,24 @@ for (@numbers)
     say "D: $_ ";
 }
 
+# for with '..' operator
 for (1..5) {
     say "E: $_";
 }
 
+# for with array
 my @skill = qw(perl c c++ awk zsh bash python sed);
 for (@skill) {
     say "F: $_";
 }
 
+# foreach loop
+foreach (1 .. 5)
+{
+    say "A: $_ * $_ = ", $_ * $_;
+}
+
+# foreach loop with array
 foreach (@skill) {
     say "G: $_";
 }
