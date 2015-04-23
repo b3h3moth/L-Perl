8cf9afda4f37139a0cae0cb1dc0648c9c4201f03
B3h3m0th <behemoth@autistici.org>
2015-04-23 18:43:43 +0200

    comment about how grep works

--- lists_and_array/grep.pl
+++ lists_and_array/grep.pl
@@ -2,3 +2,8 @@
 use warnings;
 use strict;
 use v5.14;
+
+# The grep builtin takes a list and produces another list of all values 
+# matching grep's criteria. In scalar context, returns the number of times the 
+# expression was true.
+
