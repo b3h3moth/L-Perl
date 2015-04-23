50ce51200e6e2d9bc1630000e6b932df17402cf9
B3h3m0th <behemoth@autistici.org>
2015-04-23 19:53:06 +0200

    added comment to each grep() example

--- lists_and_array/grep.pl
+++ lists_and_array/grep.pl
@@ -10,11 +10,13 @@ use v5.14;
 # grep BLOCK       LIST;
 # grep EXPRESSION, LIST;
 
+# get list from numbers greater than 5 and print results
 my @integers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 my @greater_five = grep {$_ > 5} @integers;
 
 say join(', ', @greater_five);
 
+# get list from words that start with vowels and print results
 my @song = qw(no woman no cry
               said I remember when we used to sit
               in the government yard in Trenchtown
@@ -23,9 +25,8 @@ my @words = grep {/^[aeiou]/i} @song;
 
 say join(', ', @words);
 
-
-my @numbers = (1, -9, 80, 18.9, -64, 4, 2.3, 7, -5, 29, 28.8, 2.4, 0, 83, -2);
-
+# get sorted list from numbers greater equals to 15 and print results
+my @numbers = (1, -9, 15, 18.9, -64, 4, 2.3, 7, -5, 16, 28.8, 2.4, 0, 83, -2);
 my @result = sort { $a <=> $b } grep { $_ >= 10 } @numbers;
 
 say join ', ', @result;
