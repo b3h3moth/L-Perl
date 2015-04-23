f64c347a02f4312c75e58193df4cc016916d6316
B3h3m0th <behemoth@autistici.org>
2015-04-23 19:16:38 +0200

    added two method of grep, BLOCK and EXPR

--- lists_and_array/grep.pl
+++ lists_and_array/grep.pl
@@ -6,7 +6,11 @@ use v5.14;
 # The grep builtin takes a list and produces another list of all values 
 # matching grep's criteria. In scalar context, returns the number of times the 
 # expression was true.
+#
+# grep BLOCK       LIST;
+# grep EXPRESSION, LIST;
 
+# firstss method ith BLOCK
 my @list = qw(1 2 3 4 5 6 7 8 9 10);
 my @new_list = grep { $_ > 5} @list;
 
