8eef708642d976568e3a21f5e301ca23bcc50b37
B3h3m0th <behemoth@autistici.org>
2015-04-23 18:08:22 +0200

    added pair of hash, improved output

--- hash/04_exists_defined_2.pl
+++ hash/04_exists_defined_2.pl
@@ -4,25 +4,39 @@ use strict;
 use v5.14;
 
 #Usage: ./$0 Italy Fr
-#   Code: It
-#Country: France
-die "Usage $0 <country><country two-letter code>\n" if @ARGV != 2;
+#
+#    Code: It
+# Country: France
 
-my ($state, $code) = @ARGV;
+die "Usage $0 <country> or <country two-letter code>\n" if (@ARGV != 1);
+
+my $text = $ARGV[0];
+chomp($text);
 
 my %country = (
     "Italy"     => "It",
     "France"    => "Fr",
     "England"   => "Uk",
-    "Germany"   => "Ge"
+    "Germany"   => "Ge",
+    "Spain"     => "Es",
+    "Argentina" => "Ar",
+    "Brasil"    => "Brasil",
 );
 
 my %country_code = (
     "It"        => "Italy",
     "Fr"        => "France",
     "Uk"        => "England",
-    "Ge"        => "Germany"
+    "Ge"        => "Germany",
+    "Es"        => "Spain",
+    "Ar"        => "Argentina",
+    "Br"        => "Brasil",
 );
 
-say "   Code: $country{$state}" if exists $country{$state};
-say "Country: $country_code{$code}" if exists $country_code{$code};
+if (exists $country{$text} && length($text) > 2) {
+    say "   Code: $country{$text}";
+}
+
+if (exists $country_code{$text} && length($text) == 2) {
+    say "Country: $country_code{$text}";
+}
