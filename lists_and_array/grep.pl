e00f7871a31a41b5ddea226eb62bb6794353f377
B3h3m0th <behemoth@autistici.org>
2015-04-23 19:48:07 +0200

    grep() test with numbers list

--- lists_and_array/grep.pl
+++ lists_and_array/grep.pl
@@ -10,8 +10,8 @@ use v5.14;
 # grep BLOCK       LIST;
 # grep EXPRESSION, LIST;
 
-my @numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-my @greater_five = grep {$_ > 5} @numbers;
+my @integers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+my @greater_five = grep {$_ > 5} @integers;
 
 say join(', ', @greater_five);
 
@@ -23,3 +23,9 @@ my @words = grep {/^[aeiou]/i} @song;
 
 say join(', ', @words);
 
+
+my @numbers = (1, -9, 80, 18.9, -64, 4, 2.3, 7, -5, 29, 28.8, 2.4, 0, 83, -2);
+
+my @result = sort { $a <=> $b } grep { $_ >= 10 } @numbers;
+
+say join ', ', @result;
