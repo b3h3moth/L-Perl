989007bce30e70ed4969d3f6bdfc79f2f639a735
B3h3m0th <behemoth@autistici.org>
2015-04-24 13:13:47 +0200

    comment about scope of the program

--- lists_and_array/map.pl
+++ lists_and_array/map.pl
@@ -13,5 +13,6 @@ use v5.14;
 # value composed of the results of each such evaluation. In scalar context, 
 # returns the total number of elements so generated.
 
+# convert lowercase characters to uppercase characters
 my @lower = (a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s);
 my @upper = map { uc } @lower;
