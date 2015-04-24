3a5544ff6f4feb1750be7ceee136fbf5f3731f24
B3h3m0th <behemoth@autistici.org>
2015-04-24 23:26:05 +0200

    default list added, @numbers

--- lists_and_array/list-util/reduce.pl
+++ lists_and_array/list-util/reduce.pl
@@ -15,11 +15,15 @@ use List::Util qw(reduce);
 # undef is returned. If @list only contains one element then that element is 
 # returned and BLOCK is not executed.
 
+# default list
+my @numbers = qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20);
+say "default list: ".join ',',@numbers;
+
 # how to get maximum
-my $max = reduce { $a > $b ? $a : $b } 25..75;
+my $max = reduce { $a > $b ? $a : $b } 1..20;
 say "max: $max";
 
 # how to get minimum
-my $min = reduce { $a > $b ? $b : $a } 25..75;
+my $min = reduce { $a > $b ? $b : $a } @numbers;
 say "min: $min";
 
