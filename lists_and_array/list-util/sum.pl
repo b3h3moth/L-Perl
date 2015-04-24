21f8583a6218eed6409d1c884639f3c04f0243ff
B3h3m0th <behemoth@autistici.org>
2015-04-24 22:54:10 +0200

    sum() List::Util example

--- lists_and_array/list-util/sum.pl
+++ lists_and_array/list-util/sum.pl
@@ -2,3 +2,13 @@
 use warnings;
 use strict;
 use v5.14;
+use List::Util qw(sum);
+
+my @greater_five = qw(1 2 3 4 5);
+my @greater_six = qw(6 7 8 9 10);
+
+my $total_five = sum @greater_five;
+say "A: $total_five";
+
+my $total = sum @greater_five, @greater_six;
+say "B: $total";
