77bb1cada8c803c4ba359ed0a6f320702ce922af
B3h3m0th <behemoth@autistici.org>
2015-04-24 13:08:50 +0200

    map syntax

--- lists_and_array/map.pl
+++ lists_and_array/map.pl
@@ -3,10 +3,14 @@ use warnings;
 use strict;
 use v5.14;
 
+# map BLOCK       LIST;
+# map EXPRESSION, LIST;
+#
 # The map builtin function, like the grep builtin function, takes a list and 
 # returns a new list, but it operates on every element of a list.
 # 
 # Evaluates the BLOCK or EXPR for each element of LIST and returns the list 
 # value composed of the results of each such evaluation. In scalar context, 
 # returns the total number of elements so generated.
+
 
