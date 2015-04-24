f2ec1d3d276ab8189c5fbe2400f1c69293e046f7
B3h3m0th <behemoth@autistici.org>
2015-04-24 23:27:05 +0200

    fixed comments

--- lists_and_array/list-util/reduce.pl
+++ lists_and_array/list-util/reduce.pl
@@ -4,16 +4,12 @@ use strict;
 use v5.14;
 use List::Util qw(reduce);
 
-# $result = reduce { BLOCK } @list
+# reduce { BLOCK } @list
 #
 # Reduces @list by calling BLOCK in a scalar context multiple times, setting 
 # $a and $b each time. The first call will be with $a and $b set to the first 
 # two elements of the list, subsequent calls will be done by setting $a to the 
 # result of the previous call and $b to the next element in the list.
-#
-# Returns the result of the last call to the BLOCK. If @list is empty then 
-# undef is returned. If @list only contains one element then that element is 
-# returned and BLOCK is not executed.
 
 # default list
 my @numbers = qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20);
