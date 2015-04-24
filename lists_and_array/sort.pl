4f962bd5c8dcb675afe5d17305e189ccf6d3510c
B3h3m0th <behemoth@autistici.org>
2015-04-24 13:51:55 +0200

    comments about sort() syntax

--- lists_and_array/sort.pl
+++ lists_and_array/sort.pl
@@ -6,6 +6,13 @@ use strict;
 # sort BLOCK LIST
 # sort SUBNAME LIST
 #
+# In list context, this sorts the LIST and returns the sorted list value. 
+# If SUBNAME or BLOCK is omitted, sorts in standard string comparison order.
+# If SUBNAME is specified, it gives the name of a subroutine that returns an 
+# integer less than, equal to, or greater than 0, depending on how the elements
+# of the list are to be ordered.
+
+# Sorting lexically
 my @data = qw(31 BSD linux 3  OpenBSD 1973 0 unix freeBSD kernel 69 48 25);
 print "before sorting: @data\n";
 
