f1257f024d8af69dccb44eaf2b1d12748b136b4f
B3h3m0th <behemoth@autistici.org>
2015-04-25 15:41:27 +0200

    get lower element of the list with min()

--- lists_and_array/list-util/min.pl
+++ lists_and_array/list-util/min.pl
@@ -7,3 +7,8 @@ use List::Util qw(min);
 # min LIST
 # Returns the entry in the list with the lowest numerical value. If the list is
 # empty then undef is returned.
+my @numbers = qw(784 342 54 698 436 890 2134 345 7645 534 654 8 6345 23 235);
+my $min_elem = min @numbers;
+
+say "list: ".join ', ',@numbers;
+say "lowest value: $min_elem";
