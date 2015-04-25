0db40ed3eabf08b7050fe0b35021639e4eb73fcc
B3h3m0th <behemoth@autistici.org>
2015-04-25 15:24:07 +0200

    max() comment added

--- lists_and_array/list-util/max.pl
+++ lists_and_array/list-util/max.pl
@@ -3,3 +3,8 @@ use warnings;
 use strict;
 use v5.14;
 use List::Util qw(max);
+
+# max LIST
+# Returns the entry in the list with the highest numerical value. If the list 
+# is empty then undef is returned.
+
