294f789b276cb263fc31b1c475b5c45abf74feb1
B3h3m0th <behemoth@autistici.org>
2015-04-25 15:52:57 +0200

    comment on minstr() added

--- lists_and_array/list-util/minstr.pl
+++ lists_and_array/list-util/minstr.pl
@@ -3,3 +3,8 @@ use warnings;
 use strict;
 use v5.14;
 use List::Util qw(minstr);
+
+# minstr LIST
+# Treats all the entries in the list as strings and returns the lowest string
+# as defined by the lt (less than) operator. If the list is empty then undef is
+# returned.
