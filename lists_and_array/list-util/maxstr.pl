58e1f5efc3c032d652e47be1118397519c444667
B3h3m0th <behemoth@autistici.org>
2015-04-25 15:51:42 +0200

    get highest string of the list with maxstr()

--- lists_and_array/list-util/maxstr.pl
+++ lists_and_array/list-util/maxstr.pl
@@ -8,3 +8,8 @@ use List::Util qw(maxstr);
 # Treats all the entries in the list as strings and returns the highest string
 # as defined by the gt (greater than) operator. If the list is empty then undef
 # is returned.
+my @numbers = qw(take 12 a 23 walk 34 on 45 the 56 wild 67 side 78 Lou Reed);
+my $max_str = maxstr @numbers;
+
+say "list: ".join ', ',@numbers; 
+say "highest string: $max_str";
