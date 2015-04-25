4aba770f79b0501aeb8fe2cc4053ca40e93af088
B3h3m0th <behemoth@autistici.org>
2015-04-25 15:27:49 +0200

    added comment on min()

--- lists_and_array/list-util/min.pl
+++ lists_and_array/list-util/min.pl
@@ -3,3 +3,7 @@ use warnings;
 use strict;
 use v5.14;
 use List::Util qw(min);
+
+# min LIST
+# Returns the entry in the list with the lowest numerical value. If the list is
+# empty then undef is returned.
