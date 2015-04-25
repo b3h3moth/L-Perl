78b36d9a42fbd4f587e120d7533d7ec56713eb63
B3h3m0th <behemoth@autistici.org>
2015-04-25 15:44:38 +0200

    added comment on maxstr()

--- lists_and_array/list-util/maxstr.pl
+++ lists_and_array/list-util/maxstr.pl
@@ -3,3 +3,8 @@ use warnings;
 use strict;
 use v5.14;
 use List::Util qw(maxstr);
+
+# maxstr LIST
+# Treats all the entries in the list as strings and returns the highest string
+# as defined by the gt (greater than) operator. If the list is empty then undef
+# is returned.
