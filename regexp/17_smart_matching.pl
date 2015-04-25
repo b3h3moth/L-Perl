53705ee514724c7d2851d404ce686fcdcb99d088
B3h3m0th <behemoth@autistici.org>
2015-04-25 16:44:17 +0200

    improved comment on smart matching operator ~~

--- regexp/17_smart_matching.pl
+++ regexp/17_smart_matching.pl
@@ -3,10 +3,13 @@ use warnings;
 use strict;
 use v5.20.1;
 
-# This feature is experimental.
-
-# The smart match operator '~~' compares two operands and returns a true value
-# if they match.
+# The smart match operator ~~ looks at both of its operands and decides on its
+# own how it should compare them. If the operands look like numbers, it does a
+# numeric comparison. If they look like strings, it does a string comparison.
+# If one of the operands is a regular expression, it does a pattern match.
+# It can also do some complex tasks that would otherwise take a lot of code,
+# so it keeps you from doing too much typing.
+# Returns a true value if operands match.
 
 my $x = 10;
 my $y = 20;
