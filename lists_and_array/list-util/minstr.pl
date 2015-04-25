099af7a7fc1900cf81d1d36f0f3fda0900f4bc21
B3h3m0th <behemoth@autistici.org>
2015-04-25 15:58:08 +0200

    get lowest string of the list with minstr()

--- lists_and_array/list-util/minstr.pl
+++ lists_and_array/list-util/minstr.pl
@@ -8,3 +8,8 @@ use List::Util qw(minstr);
 # Treats all the entries in the list as strings and returns the lowest string
 # as defined by the lt (less than) operator. If the list is empty then undef is
 # returned.
+my @list = qw(1 perl a language hi programming unix is 8 awesome);
+my $min_str = minstr @list;
+
+say "list: ".join ', ',@list;
+say "lowest string: $min_str";
