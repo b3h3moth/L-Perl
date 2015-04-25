f596523db29157daa25a195b350ba9cd8c5d016c
B3h3m0th <behemoth@autistici.org>
2015-04-25 15:26:28 +0200

    get max element of the list with max()

--- lists_and_array/list-util/max.pl
+++ lists_and_array/list-util/max.pl
@@ -8,3 +8,7 @@ use List::Util qw(max);
 # Returns the entry in the list with the highest numerical value. If the list 
 # is empty then undef is returned.
 
+my @numbers = qw(827 47 10, 1 83 38 81 39 5 45 100 820 829 37 83 743 93 483);
+my $max_elem = max @numbers;
+say "List: ".join ', ',@numbers;
+say "max: $max_elem";
