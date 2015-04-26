1c7b0422be2b7157efe81ee866eb006bfd13e58d
B3h3m0th <behemoth@autistici.org>
2015-04-26 22:03:01 +0200

    test with bless()

--- builtin_functions/bless.pl
+++ builtin_functions/bless.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/env perl
+package Test;
 use warnings;
 use strict;
 use v5.14;
@@ -8,3 +9,9 @@ use v5.14;
 #
 # The bless() takes a REF and turns it into an object in the CLASSNAME package.
 # If CLASSNAME is omitted, the current package is used.
+my $a = {};
+print '$a is a ', ref($a), " reference\n";
+bless($a, "Person1");
+print '$a is a ', ref($a), " reference\n";
+bless($a, "Animal::Vertebrate::Mammal");
+print '$a is a ', ref($a), " reference\n";
