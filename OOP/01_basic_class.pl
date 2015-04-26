9edaab39681e2961c37384e3efb9b07a885777fb
B3h3m0th <behemoth@autistici.org>
2015-04-26 18:27:49 +0200

    added three rules about Perl's OOP

--- OOP/01_basic_class.pl
+++ OOP/01_basic_class.pl
@@ -3,4 +3,8 @@ use warnings;
 use strict;
 use v5.14;
 
+# There are three rules to know about Perl's OOP:
+# 1) a class is a package;
+# 2) an object is a reference that knows its class;
+# 3) a method is a subroutine.
 
