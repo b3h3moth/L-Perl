1016806299a9ab69c633f5dedf2f56fe9bad07e0
B3h3m0th <behemoth@autistici.org>
2015-04-26 19:54:56 +0200

    renamed file, now his extension is .pm

--- OOP/01_intro.pm
+++ OOP/01_intro.pm
@@ -1,6 +1,8 @@
 package Person;
 
-# A class is a package, so the simplest class is just a package.
+# A class is a package, so the simplest class is just a package, in this case
+# it's a totally empty class.
+# The extension of the file is .pm
 
 # There are three rules to know about Perl's OOP:
 # 1) a class is a package;
