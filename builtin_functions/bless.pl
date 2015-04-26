cbf33a83b7572d304563eae9cadbe02f2ef3b30e
B3h3m0th <behemoth@autistici.org>
2015-04-26 20:23:01 +0200

    added the purprose of the bless() function

--- builtin_functions/bless.pl
+++ builtin_functions/bless.pl
@@ -5,3 +5,6 @@ use v5.14;
 
 # bless REF,CLASSNAME
 # bless REF
+#
+# The bless() takes a REF and turns it into an object in the CLASSNAME package.
+# If CLASSNAME is omitted, the current package is used.
