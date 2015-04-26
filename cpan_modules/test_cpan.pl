eb5b38a190d2107c5cb74464d3d8b5dbe1a6f283
B3h3m0th <behemoth@autistici.org>
2015-04-26 19:45:20 +0200

    testing cpan with ftp

--- cpan_modules/test_cpan.pl
+++ cpan_modules/test_cpan.pl
@@ -2,3 +2,11 @@
 use warnings;
 use strict;
 use v5.14;
+use Net::FTP;
+
+my $ftp = Net::FTP->new("ftp.cpan.org") or die "Couldn't connect: $@\n";
+
+$ftp->login("anonymous");
+$ftp->cwd("/pub/CPAN");
+$ftp->get("README.html");
+$ftp->close();
