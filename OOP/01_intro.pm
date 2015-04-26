b2f182b28a54455c402892ff4c29b1deb2ca4d9a
B3h3m0th <behemoth@autistici.org>
2015-04-26 20:15:34 +0200

    added few comments

--- OOP/01_intro.pm
+++ OOP/01_intro.pm
@@ -1,11 +1,13 @@
 package Person;
 
-# A class is a package, so the simplest class is just a package. 
-# The extension of the class file is .pm.
-# Generally he name of the package is the same as the name of file.
-# Perl expects to see a true value as the last thing in the package.
-#
-# There are three rules to know about Perl's OOP:
+# Rules to know about OOP and Perl:
+# A class is a package, so the simplest class is just a package;
+# The extension of the class file is .pm;
+# Usually the name of the package is the same as the name of file;
+# Perl expects to see a true value '1' as the last thing in the package:
+# A program can add 'use Person;' and start using the class;
+# 
+# And:
 # - a class is a package;
 # - an object is a reference that knows its class;
 # - a method is a subroutine.
