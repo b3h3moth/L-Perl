b6b59f26332b3b9e5dd7b04fa3ecee0dcb16d7d9
B3h3m0th <behemoth@autistici.org>
2015-04-26 19:47:58 +0200

    added comments, open and close ftp

--- cpan_modules/get_readme_cpan_via_ftp.pl
+++ cpan_modules/get_readme_cpan_via_ftp.pl
@@ -4,9 +4,12 @@ use strict;
 use v5.14;
 use Net::FTP;
 
+# open ftp connection
 my $ftp = Net::FTP->new("ftp.cpan.org") or die "Couldn't connect: $@\n";
 
 $ftp->login("anonymous");
 $ftp->cwd("/pub/CPAN");
 $ftp->get("README.html");
+
+# close connection
 $ftp->close();
