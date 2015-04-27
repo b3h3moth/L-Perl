5d5fcef0a107acbe8b917753840fba377432772d
B3h3m0th <behemoth@autistici.org>
2015-04-27 17:40:05 +0200

    improved comments, added opendir() syntax

--- directory/01_open_directory.pl
+++ directory/01_open_directory.pl
@@ -2,3 +2,17 @@
 use warnings;
 use strict;
 use v5.14;
+
+# Modern operating systems organize files into directories, 'directory handle' 
+# is the way to get a list of (file)names from a directory. 
+#
+# opendir DIRHANDLE, EXPR
+# DIRHANDLE hold the directory handle, EXPR is relative or absolute path to the
+# directory.
+#
+# Directory handle works like a filehandle:
+# - open with opendir() instead of open();
+# - read from it with readdir() instead of readline(); 
+# - close it with closedir() instead of close().
+
+
