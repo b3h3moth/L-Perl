96c910d69ca4d0c350515897dbf84e2e09986f0c
B3h3m0th <behemoth@autistici.org>
2015-04-27 19:13:07 +0200

    added new file, how to read only perl files

---
+++ directory/05_read_dir_only_perl.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+# Directory I would like to read:
+my $dir = shift // '.';
+
+# Open directory:
+opendir(my $dh, $dir) or die "Could not open $dir for reading: $!\n";
+
+# Read from $dh 'directory handle' into a scalar variable $content:
+while (my $content = readdir $dh) {
+    
+    # skip current directory '.' and parent directory '..':
+    if ($content eq '.' or $content eq '..') {
+        next;
+    }
+
+    next unless ($content =~ /\.pl$/);
+    
+    say $content;
+}
+
+# Close directory handle:
+closedir($dh);
