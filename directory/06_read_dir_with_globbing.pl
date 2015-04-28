b0983891c558011589ddca966361d3142a48d46d
B3h3m0th <behemoth@autistici.org>
2015-04-28 20:34:28 +0200

    fixed and improved code

--- directory/06_read_dir_with_globbing.pl
+++ directory/06_read_dir_with_globbing.pl
@@ -9,20 +9,19 @@ my $dir = shift // '.';
 # open directory:
 opendir(DH, $dir) or die "Couldn't open $dir for reading: $!";
 
-# Empty list:
+# empty list:
 my @files = ();
 
 while(my $file = readdir DH){
 
-    # find only perl, C, C++, C header and perl modules
+    # find perl, C, C++, C header and perl modules
     next unless ($file =~ /\.(pl|c|h|cpp|pm)$/i);
     
     # add directory name to each file
     my $filename = "$dir/$file";
 
-    # push each result into list
+    # push results into list
     push(@files, $filename) if (-T $filename);
 }
 
-say "ls \'$dir\':";
 say join "\n",@files;
