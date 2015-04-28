c600626fabd2e542909ff766c34f43f692fb6ade
B3h3m0th <behemoth@autistici.org>
2015-04-28 19:46:40 +0200

    improved comment of Logical defined-or operator

--- scalars/operators/logical_defined-or_operator.pl
+++ scalars/operators/logical_defined-or_operator.pl
@@ -2,9 +2,16 @@
 use warnings;
 use strict;
 
+# Logical Defined-or operator //
+#
+# It's exactly the same as '||', except that it tests the left hand side's
+# definedness instead of its truth.
+# 
+# "$a // $b" is similar to "defined($a) || $b", except that it returns the
+# value of $a rather than the value of "defined($a)"
+#
+# This is very useful for providing default values for variables. 
 
-#defined_op operator //
-#utile per i valodi di default
 sub default_OS {
     my $os_name = shift // 'OpenBSD';
 }
