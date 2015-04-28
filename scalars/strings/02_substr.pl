266a60b6588fa24da255c8bb6ef6cac337245fa1
B3h3m0th <behemoth@autistici.org>
2015-04-28 18:30:51 +0200

    replaced all substr and the default text

--- scalars/strings/02_substr.pl
+++ scalars/strings/02_substr.pl
@@ -4,15 +4,17 @@ use warnings;
 use strict;
 
 # substr(expr, offset, length)
-my $quote = "Probabilmente fra qualche ora cadra' parecchia neve!";
-print substr($quote,0,13), "\n";
-print substr($quote,14,3), "\n";
-print substr($quote,18,7), "\n";
-print substr($quote,26,3), "\n";
-print substr($quote,30,6), "\n";
-print substr($quote,37,9), "\n";
-print substr($quote,47,5), "\n";
+my $quote = "This is the end my only friend, the end (The doors)";
+print substr($quote,  0, 4), "\n";
+print substr($quote,  5, 2), "\n";
+print substr($quote,  8, 3), "\n";
+print substr($quote, 12, 3), "\n";
+print substr($quote, 16, 2), "\n";
+print substr($quote, 19, 4), "\n";
+print substr($quote, 24, 7), "\n";
+print substr($quote, 32, 3), "\n";
+print substr($quote, 36, 3), "\n";
 
-#string replacement
-substr($quote,47,5) = "Figa!";
+# string replacement
+substr($quote, 47, 5) = "Figa!";
 print $quote, "\n";
