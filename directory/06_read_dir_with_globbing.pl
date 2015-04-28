e6e7f9506063d72a3dab50524f94b4e3f7c176b9
B3h3m0th <behemoth@autistici.org>
2015-04-28 20:25:03 +0200

    read directory example

--- directory/06_read_dir_with_globbing.pl
+++ directory/06_read_dir_with_globbing.pl
@@ -2,3 +2,24 @@
 use warnings;
 use strict;
 use v5.14;
+
+# directory to read:
+my $dir = shift // '.';
+
+# open directory:
+opendir(DH, $dir) or die "Couldn't open $dir for reading: $!";
+
+# Empty list:
+my @files = ();
+
+while(my $file = readdir DH){
+
+    # find only perl, C, C++, C header and perl modules
+    next unless ($file =~ /\.(pl|c|h|cpp|pm)$/i);
+    
+    # 
+    my $filename = "$dir/$file";
+    push(@files, $filename) if -T $filename;
+}
+
+say @files;
