5eac0a5b90b7cc80562a56e731faa51700aabe3c
B3h3m0th <behemoth@autistici.org>
2015-04-29 19:55:46 +0200

    how to exchange values, test

--- scalars/strings/exchanging_values.pl
+++ scalars/strings/exchanging_values.pl
@@ -2,3 +2,11 @@
 use warnings;
 use strict;
 use v5.14;
+
+# How to exchange the values of two scalar variables (no temp variable):
+my $name = 'luca';
+my $nickname = 'behemoth';
+
+say "before: name/$name nickname/$nickname";
+($name, $nickname) = ($nickname, $name);
+say " after: name/$name nickname/$nickname";
