9e2bf992d54d33428e06431b9f0968caf60b4c6a
B3h3m0th <behemoth@autistici.org>
2015-04-29 12:00:52 +0200

    fixed typographical error

--- lists_and_array/list-util/max.pl
+++ lists_and_array/list-util/max.pl
@@ -8,7 +8,7 @@ use List::Util qw(max);
 # Returns the entry in the list with the highest numerical value. If the list 
 # is empty then undef is returned.
 
-my @numbers = qw(827 47 10, 1 83 38 81 39 5 45 100 820 829 37 83 743 93 483);
+my @numbers = qw(827 47 10 1 83 38 81 39 5 45 100 820 829 37 83 743 93 483);
 my $max_elem = max @numbers;
 
 say "list: ".join ', ',@numbers;
