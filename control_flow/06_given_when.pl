31998cdc32858ad8acdb2083ebe12abd0cfa6893
B3h3m0th <behemoth@autistici.org>
2015-04-29 22:33:36 +0200

    fixed pattern matching of every lang string

--- control_flow/06_given_when.pl
+++ control_flow/06_given_when.pl
@@ -8,28 +8,28 @@ use feature "switch";
 # argument to given satisfies a condition, it is Perl's equivalent to C's 
 # switch statement.
 
-say "Which is you favorite programming language? ";
+die ("Usage: $0 <language programming>\n") if (@ARGV != 1);
 
 given ($ARGV[0]) {
     when (/perl/i) {
         print "Good boy :)\n";
     }
-    when (/C/i) {
+    when (/^c$/i) {
         print "Old school, nice!\n";
     }
-    when (/(C++|Cpp)/i) {
+    when (/(^C\+\+$|^Cpp$)/i) {
         print "Do you like hard OOP? Yeah!\n";
     }
-    when (/awk/i) {
+    when (/^awk$/i) {
         print "I like programming UNIX too\n";
     }
-    when (/python/i) {
+    when (/^python$/i) {
         print "Modern programming is good\n";
     }
-    when (/C#/i) {
+    when (/^C#$/i) {
         print "What? Only serious programmer please. \n";
     }
     default {
-        print "Hey, are you a programmer yes or not\n";
+        print "Hey, are you a programmer yes or not?\n";
     }
 }
