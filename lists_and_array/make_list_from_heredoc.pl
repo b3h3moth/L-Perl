31680d3cf5caff6ee221f7c8411839bc991e71ca
B3h3m0th <behemoth@autistici.org>
2015-04-29 20:26:14 +0200

    full example, lines from heredoc

--- lists_and_array/make_list_from_heredoc.pl
+++ lists_and_array/make_list_from_heredoc.pl
@@ -3,5 +3,22 @@ use warnings;
 use strict;
 use v5.14;
 
-# Make a list with here document and extract lines
+# Make a list with here document and extract lines:
 
+my @dante_inferno = (<< "_END_DOC_" =~ /^\s*(.+)/gm);
+	Nel mezzo del cammin di nostra vita
+    mi ritrovai per una selva oscura
+    ché la diritta via era smarrita.
+    Ahi quanto a dir qual era è cosa dura
+    esta selva selvaggia e aspra e forte
+    che nel pensier rinova la paura!
+    Tant'è amara che poco è più morte;
+    ma per trattar del ben ch'i' vi trovai,
+    dirò de l'altre cose ch'i' v'ho scorte.
+    Io non so ben ridir com'i' v'intrai,
+    tant'era pien di sonno a quel punto
+    che la verace via abbandonai.
+    ...
+_END_DOC_
+
+say join "\n",@dante_inferno;
