b206ddb412e3871221a2d47e2d3bd9f5f550cf6a
B3h3m0th <behemoth@autistici.org>
2015-04-30 23:03:55 +0200

    fixed url, now works with csv file

--- networking/get_file_from_net.pl
+++ networking/get_file_from_net.pl
@@ -7,14 +7,15 @@ use Data::Dumper;
 
 # Retrieving files from internet
 
-my $remote_url = 'http://www.openbsd.org/57.html';
+#my $remote_url = 'http://www.openbsd.org/57.html';
+my $remote_url = 'http://bixsolutions.net/profiles.csv';
 my $remote_file = get($remote_url) or die ("Unable to fetch file\n");
 
 my @data = split("\r\n", $remote_file);
 
 # process header
 my $data_line = shift(@data);
-my @header = split("\n", $data_line);
+my @header = split(',', $data_line);
 
 my @sample_data;
 
