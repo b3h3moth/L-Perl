3160e6153423fa5544bda5a68b3cb0fd80ceb1bf
B3h3m0th <behemoth@autistici.org>
2015-04-30 22:52:44 +0200

    added @data array to sore info

--- networking/get_file_from_net.pl
+++ networking/get_file_from_net.pl
@@ -8,4 +8,6 @@ use Data::Dumper;
 # Retrieving files from internet
 
 my $remote_url = 'http://www.openbsd.org/57.html';
-my $data_file = get($remote_url) or die ("Unable to fetch file\n");
+my $remote_file = get($remote_url) or die ("Unable to fetch file\n");
+
+my @data = split("\r\n", $remote_file);
