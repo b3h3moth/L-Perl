3c77d578aa6aa3efec14ec1d52110d910c8948ec
B3h3m0th <behemoth@autistici.org>
2015-04-30 22:01:31 +0200

    fixed comment

--- references/02_array_references_2.pl
+++ references/02_array_references_2.pl
@@ -6,7 +6,8 @@ use v5.14;
 my @numbers = qw(1 2 3 4 5 6);
 my $ref_to_numbers = \@numbers;
 
-# $ref_to_numbers refers to entire array
+# We can place any reference to an array in curly braces in place of the name 
+# of an array. Now reference refers to the entire array.
 for my $item (@{$ref_to_numbers}) {
     say $item;
 }
