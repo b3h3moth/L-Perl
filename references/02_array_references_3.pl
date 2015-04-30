4b6830ae077b50433b98c4548284dc131cf1a3d2
B3h3m0th <behemoth@autistici.org>
2015-04-30 22:06:07 +0200

    full example of array reference

---
+++ references/02_array_references_3.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Data::Dumper;
+
+# Few people are on Minnow ship.
+
+sub check_required_items {
+    my $who   = shift;
+    my $items = shift;
+   
+    # $items points to a different array, so the same code applies to different
+    # arrays each time we invoke it.
+    my %whos_items = map {$_,1} @{$items};
+
+    my @required = qw(preserver sunscreen water_bottle jacket);
+    
+    for my $item (@required) {
+        unless ( $whos_items{$item} ) {
+            print "$who is missing $item\n";
+        }
+    }
+}
+
+my @skipper = qw(blue_shirt hat jacket preserver sunscreen);
+my @gillian = qw(red_shirt hat lucky_socks water_bottle);
+my @prof = qw(sunscreen water_bottle slide_rule batteries radio);
+
+check_required_items('gillian', \@gillian);
+check_required_items('skipper', \@skipper);
+check_required_items('prof', \@prof);
