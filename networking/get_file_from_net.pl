7888c8e285051cbbd17cdb6f671a8e69655c9850
B3h3m0th <behemoth@autistici.org>
2015-04-30 23:01:56 +0200

    added basic example, now is not complete

--- networking/get_file_from_net.pl
+++ networking/get_file_from_net.pl
@@ -11,3 +11,25 @@ my $remote_url = 'http://www.openbsd.org/57.html';
 my $remote_file = get($remote_url) or die ("Unable to fetch file\n");
 
 my @data = split("\r\n", $remote_file);
+
+# process header
+my $data_line = shift(@data);
+my @header = split("\n", $data_line);
+
+my @sample_data;
+
+foreach (@data) {
+    my @sample = split(",", $_);
+
+    my %sample_hash;
+
+    for (my $i=0; $i<scalar(@sample); $i++) {
+        $sample_hash{$header[$i]} = $sample[$i];
+    }
+
+    push (@sample_data, \%sample_hash);
+}
+
+print Dumper (@sample_data);
+
+exit;
