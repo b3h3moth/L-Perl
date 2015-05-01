3b02bd8358b404f8e78d4f6f94731c27f7750c2f
B3h3m0th <behemoth@autistici.org>
2015-05-01 16:32:45 +0200

    changed position of arrays

--- references/02_array_references_3.pl
+++ references/02_array_references_3.pl
@@ -34,18 +34,18 @@ sub check_required_items {
 my @skipper = qw(blue_shirt hat jacket preserver sunscreen);
 my @skipper_with_name = ('Skipper'=> \@skipper);
 
-my @gillian = qw(red_shirt hat lucky_socks water_bottle);
-my @gillian_with_name = ('Gillian'=> \@gillian);
-
 my @prof = qw(sunscreen water_bottle slide_rule batteries radio);
 my @prof_with_name = ('Prof'=> \@prof);
+
+my @gillian = qw(red_shirt hat lucky_socks water_bottle);
+my @gillian_with_name = ('Gillian'=> \@gillian);
 
 # We have three elements in @all_with_names, each of which is a reference to an
 # array with two elements: the name and its initial provisions.
 my @all_with_names = (
     \@skipper_with_name,
+    \@prof_with_name,
     \@gillian_with_name,
-    \@prof_with_name
 );
 
 for my $person (@all_with_names) {
