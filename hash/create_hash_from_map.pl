6acfe2dfa0b6bdd67d8b39eb0cd06c16dc3adc62
B3h3m0th <behemoth@autistici.org>
2015-05-01 17:22:15 +0200

    added new file, create hash from map and array reference

---
+++ hash/create_hash_from_map.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Data::Dumper;
+
+sub set_hash_by_reference {
+    my $items = shift;
+    my %hash = map {$_, 1} @{$items};
+    say Dumper \%hash;
+}
+
+my @array = qw(1 2 3 4 5 6 7 8 9 10);
+set_hash_by_reference(\@array);
