89d7d3a6c1fdcf1e552db80f91ea430b42a4e10c
B3h3m0th <behemoth@autistici.org>
2015-05-02 16:37:28 +0200

    getstore() example

--- networking/lwp/open_webpage_with_browser.pl
+++ networking/lwp/open_webpage_with_browser.pl
@@ -2,3 +2,10 @@
 use warnings;
 use strict;
 use v5.14;
+use LWP::Simple;
+
+my $remote_url = 'htttp://www.opensdssorg/faq/upgrade57.html';
+my $filename = 'doc.html';
+
+die("Unable to fetch $remote_url") unless (getstore($remote_url, $filename));
+
