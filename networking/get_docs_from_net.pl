e737f79e165c074a5878e40b0a68eb4c854cf31c
B3h3m0th <behemoth@autistici.org>
2015-05-02 15:24:46 +0200

    How to get web page content with get()

--- networking/get_docs_from_net.pl
+++ networking/get_docs_from_net.pl
@@ -6,15 +6,16 @@ use LWP::Simple;
 
 # Usage of libwww-perl library.
 
+# get(URL)
 # get document specified by URL:
 my $remote_url = 'https://gnu.org/philosophy/philosophy.html';
 
-my $doc = get($remote_url) or die("Unable to fetch $remote_url\n");
+my $content = get($remote_url) or die("Unable to fetch $remote_url\n");
 
 # count how many times 'gnu' word is mentioned:
 my $count;
 
-while ($doc =~ /gnu/gi) {
+while ($content =~ /gnu/gi) {
     $count++;
 }
 
