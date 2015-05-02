6be5be43e6484d8aa01888cb4d82e8530b6f44d4
B3h3m0th <behemoth@autistici.org>
2015-05-02 22:29:40 +0200

    added new file, looks like number function

---
+++ scalars/scalar-util/looks_like_number.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Scalar::Util qw(looks_like_number);
+
+# the looks_like_number() function returns a boolean value indicating whether a
+# string looks like a number
