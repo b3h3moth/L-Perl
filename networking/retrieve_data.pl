0a2f6f7f436fcea16a62592f783d16feeaa509f4
B3h3m0th <behemoth@autistici.org>
2015-05-02 15:47:23 +0200

    example added, retrieve webpage and count its length in bytes

--- networking/retrieve_data.pl
+++ networking/retrieve_data.pl
@@ -4,4 +4,10 @@ use strict;
 use v5.14;
 use LWP::Simple;
 
-# Get a web page and retrieve the length of the content un bytes
+# Get a web page and retrieve the length of the content in bytes
+
+my $remote_url = 'https://gnu.org/philosophy/free-sw.html';
+
+my $content = get($remote_url) or die("Unable to fetch $remote_url\n");
+
+say "$remote_url: ".length($content).' bytes';
