ba3c08270e85a426c98f989085f79ea887a81e14
B3h3m0th <behemoth@autistici.org>
2015-05-02 16:53:21 +0200

    replaced error control with is_error()

--- networking/lwp/count_bytes_of_doc.pl
+++ networking/lwp/count_bytes_of_doc.pl
@@ -7,7 +7,10 @@ use LWP::Simple; # LWP - The World-Wide Web library for Perl
 # Get a web page and retrieve the length of the content in bytes
 
 my $remote_url = 'https://gnu.org/philosophy/free-sw.html';
+my $content = get($remote_url);
 
-my $content = get($remote_url) or die("Unable to fetch $remote_url\n");
+if (is_error($content)) {
+    die "Unable to fetch <$remote_url>\n";
+}
 
 say "$remote_url: ".length($content).' bytes';
