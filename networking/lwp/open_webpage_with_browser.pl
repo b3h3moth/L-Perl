b9e86cba7c44377b3b3c5a43cb689b02bfe4caf5
B3h3m0th <behemoth@autistici.org>
2015-05-02 16:53:58 +0200

    getstore() full example added

--- networking/lwp/open_webpage_with_browser.pl
+++ networking/lwp/open_webpage_with_browser.pl
@@ -4,8 +4,11 @@ use strict;
 use v5.14;
 use LWP::Simple;
 
-my $remote_url = 'htttp://www.opensdssorg/faq/upgrade57.html';
+my $remote_url = 'http://www.openbsd.org/faq/upgrade57.html';
 my $filename = 'doc.html';
 
-die("Unable to fetch $remote_url") unless (getstore($remote_url, $filename));
+if ( is_error(getstore($remote_url, $filename)) ) {
+    die "Unable to fetch <$remote_url>\n";
+}
 
+system "lynx $filename";
