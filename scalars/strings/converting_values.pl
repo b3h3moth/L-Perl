925835b1cf26edaba8fc4667129463033967062e
B3h3m0th <behemoth@autistici.org>
2015-05-03 23:18:05 +0200

    ord() example with just one character

--- scalars/strings/converting_values.pl
+++ scalars/strings/converting_values.pl
@@ -2,3 +2,12 @@
 use warnings;
 use strict;
 use v5.14;
+
+# ord EXPR
+# It converts a character to a number. It returns the numeric value of the 
+# first character of EXPR.
+
+my $character = "a";
+my $result_number_conversion = ord($character);
+say "$character:$result_number_conversion";
+
