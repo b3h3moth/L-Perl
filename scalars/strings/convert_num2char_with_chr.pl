fdee5f3e5f53eb05b017b83b60a025b3dbc0a614
B3h3m0th <behemoth@autistici.org>
2015-05-04 12:09:12 +0200

    converting number to ascii, chr() example added

--- scalars/strings/convert_num2char_with_chr.pl
+++ scalars/strings/convert_num2char_with_chr.pl
@@ -6,3 +6,11 @@ use v5.14;
 # chr NUMBER
 # It converts a number to a character. It returns the character rapresented by
 # NUMBER in the craracter set.
+
+my $number = 97;
+my $result_char_conversion = chr($number);
+say "$number:$result_char_conversion";
+
+foreach(97..122) {
+    say "$_ = ASCII ".chr($_);
+}
