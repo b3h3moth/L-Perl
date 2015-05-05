06d6bc7b69924a1c521daef8c026219e600f8a07
B3h3m0th <behemoth@autistici.org>
2015-05-05 19:23:39 +0200

    added syntax of insert_after()

--- lists_and_array/list-moreutils/insert_after.pl
+++ lists_and_array/list-moreutils/insert_after.pl
@@ -3,3 +3,8 @@ use warnings;
 use strict;
 use v5.14;
 use List::MoreUtils qw(insert_after);
+
+# insert_after BLOCK VALUE LIST
+#
+# Inserts VALUE after the first item in LIST for which the criterion in BLOCK 
+# is true.
