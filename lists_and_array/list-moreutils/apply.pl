90cf06395f6461bab18e28d8fb9280210cbc0a14
B3h3m0th <behemoth@autistici.org>
2015-05-05 22:12:24 +0200

    improved output, apply()

--- lists_and_array/list-moreutils/apply.pl
+++ lists_and_array/list-moreutils/apply.pl
@@ -8,7 +8,7 @@ use List::MoreUtils qw(apply);
 # Applies BLOCK to each item in LIST and returns a list of the values after 
 # BLOCK has been applied.
 my @numbers = qw (5 10 15 20 25);
-say join ', ',@numbers;
+say 'before: '.join ', ',@numbers;
 
 my @multiples = apply { $_ *= 2 } @numbers;
-say join ', ',@multiples;
+say ' after: '.join ', ',@multiples;
