6b75333daaa0d2d47fe0538a964efc92d68fd17c
B3h3m0th <behemoth@autistici.org>
2015-05-05 11:43:10 +0200

    added join() to each print(), added false branch to if

--- lists_and_array/list-util/any.pl
+++ lists_and_array/list-util/any.pl
@@ -9,15 +9,17 @@ use constant MAX_NUM => 10;
 # It evalutes BLOCK, setting $_ to each element of LIST in turn. It Returns
 # true if any elements makes the BLOCK return a true value, false otherwise.
 my @numbers = qw(4 2 3 6 11 8 23 7 41 5 98 9 101 10);
-say @numbers;
+say join ', ',@numbers;
 
 if ( any { $_ > MAX_NUM } @numbers ) {
     say 'At least one number is greater than '.MAX_NUM;
 }
 
 my @strings = qw(una matina mi son svegliato o bella ciao. Partigiani sempre);
-say @strings;
+say join ', ',@strings;
 
 if ( any { length($_) > MAX_NUM } @strings ) {
     say 'At least one string is greater than '.MAX_NUM;
+} else {
+    say 'OPS ... there are not strings greater than '.MAX_NUM;
 }
