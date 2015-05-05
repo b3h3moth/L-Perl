9555d5ac04c8fd81369ab64af61c9ba5c354de5c
B3h3m0th <behemoth@autistici.org>
2015-05-05 22:11:04 +0200

    improved output, insert_after_string()

--- lists_and_array/list-moreutils/insert_after_string.pl
+++ lists_and_array/list-moreutils/insert_after_string.pl
@@ -7,5 +7,6 @@ use List::MoreUtils qw(insert_after_string);
 # insert_after_string STRING VALUE LIST;
 # Inserts VALUE after the first item in LIST which is equal to STRING.
 my @song = qw(We're jamming i wanna jam it with);
+say 'before: '.join ' ',@song;
 insert_after_string "with", "you" => @song;
-say join ' ',@song;
+say ' after: '.join ' ',@song;
