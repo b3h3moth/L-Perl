74e1e209db1f907e34bbc6a29bcb7a37a8d3f5cf
B3h3m0th <behemoth@autistici.org>
2015-05-05 19:29:36 +0200

    insert_after() example added

--- lists_and_array/list-moreutils/insert_after.pl
+++ lists_and_array/list-moreutils/insert_after.pl
@@ -8,3 +8,6 @@ use List::MoreUtils qw(insert_after);
 #
 # Inserts VALUE after the first item in LIST for which the criterion in BLOCK 
 # is true.
+my @band = qw(Bob & the wailers);
+insert_after { $_ eq "Bob" } "Marley" => @band;
+say join ' ',@band;
