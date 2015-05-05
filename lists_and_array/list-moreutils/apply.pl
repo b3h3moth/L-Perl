151708d59a3e17457a389e2ed78aacfbb7cda116
B3h3m0th <behemoth@autistici.org>
2015-05-05 19:09:01 +0200

    comment about apply() added

--- lists_and_array/list-moreutils/apply.pl
+++ lists_and_array/list-moreutils/apply.pl
@@ -2,3 +2,9 @@
 use warnings;
 use strict;
 use v5.14;
+
+# apply BLOCK LIST;
+# Applies BLOCK to each item in LIST and returns a list of the values after 
+# BLOCK has been applied. In scalar context, the last element is returned.  
+# This function is similar to map but will not modify the elements of the 
+# input list.
