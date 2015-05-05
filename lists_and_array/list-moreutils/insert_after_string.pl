3062c54e9841db1ff15f3eac4176c6f103078a25
B3h3m0th <behemoth@autistici.org>
2015-05-05 20:13:45 +0200

    added module List::MoreUtils

--- lists_and_array/list-moreutils/insert_after_string.pl
+++ lists_and_array/list-moreutils/insert_after_string.pl
@@ -2,6 +2,7 @@
 use warnings;
 use strict;
 use v5.14;
+use List::MoreUtils qw(insert_after_string);
 
 # insert_after_string STRING VALUE LIST;
 # Inserts VALUE after the first item in LIST which is equal to STRING.
