cd4d898c9c49cd642f63177f8949c2186840ace7
B3h3m0th <behemoth@autistici.org>
2015-05-05 11:08:21 +0200

    any() syntax added

--- lists_and_array/list-util/any.pl
+++ lists_and_array/list-util/any.pl
@@ -3,3 +3,7 @@ use warnings;
 use strict;
 use v5.14;
 use List::Util;
+
+# any { BLOCK } LIST;
+# It evalutes BLOCK, setting $_ to each element of LIST in turn. It Returns
+# true if any elements makes the BLOCK return a true value.
