747f4da378c9af0dd0e2b9ebfda22cf84cf52576
B3h3m0th <behemoth@autistici.org>
2015-05-05 11:49:44 +0200

    all() syntax added

--- lists_and_array/list-util/all.pl
+++ lists_and_array/list-util/all.pl
@@ -4,3 +4,7 @@ use strict;
 use v5.14;
 use List::Util qw(all);
 use constant MAX_NUM => 10;
+
+# all { BLOCK } LIST;
+# It evalutes BLOCK, setting $_ to each element of LIST in turn. It returns 
+# true if all elements make the BLOCK return a true value, false otherwise.
