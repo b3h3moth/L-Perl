5c0aeb0390b4dc973525b28063426299bcb9aaf6
B3h3m0th <behemoth@autistici.org>
2015-05-05 11:34:37 +0200

    replaced scalar with constant MAX_NUM

--- lists_and_array/list-util/any.pl
+++ lists_and_array/list-util/any.pl
@@ -4,14 +4,17 @@ use strict;
 use v5.14;
 use List::Util qw(any);
 use diagnostics;
+use constant MAX_NUM => 10;
 
 # any { BLOCK } LIST;
 # It evalutes BLOCK, setting $_ to each element of LIST in turn. It Returns
 # true if any elements makes the BLOCK return a true value.
 my @numbers = qw(4 2 3 6 11 8 23 7 41 5 98 9 101 10);
-my $value = 10;
 say @numbers;
 
-if ( any { $_ > $value } @numbers ) {
-    say "At least one number is greater than $value";
+if ( any { $_ > MAX_NUM } @numbers ) {
+    say 'At least one number is greater than '.MAX_NUM;
 }
+
+my @new_list = any { $_ > MAX_NUM } @numbers;
+say @new_list;
