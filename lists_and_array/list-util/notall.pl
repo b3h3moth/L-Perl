11a2b584b6457a5556b54b47c2fd258d0ea4bf98
B3h3m0th <behemoth@autistici.org>
2015-05-05 18:46:33 +0200

    added commento about notall()

--- lists_and_array/list-util/notall.pl
+++ lists_and_array/list-util/notall.pl
@@ -2,3 +2,9 @@
 use warnings;
 use strict;
 use v5.14;
+use List::Util qw(notall);
+
+# notall { BLOCK } LIST;
+# It returns true only if no value in the LIST causes the BLOCK to return true, 
+# false otherwise.
+
