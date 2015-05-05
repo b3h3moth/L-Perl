a16ed4c9fa1c7a03a9adb9efa6b29721fe8afa11
B3h3m0th <behemoth@autistici.org>
2015-05-05 11:40:00 +0200

    any(), added secondo example with strings

--- lists_and_array/list-util/any.pl
+++ lists_and_array/list-util/any.pl
@@ -3,12 +3,11 @@ use warnings;
 use strict;
 use v5.14;
 use List::Util qw(any);
-use diagnostics;
 use constant MAX_NUM => 10;
 
 # any { BLOCK } LIST;
 # It evalutes BLOCK, setting $_ to each element of LIST in turn. It Returns
-# true if any elements makes the BLOCK return a true value.
+# true if any elements makes the BLOCK return a true value, false otherwise.
 my @numbers = qw(4 2 3 6 11 8 23 7 41 5 98 9 101 10);
 say @numbers;
 
@@ -16,5 +15,9 @@ if ( any { $_ > MAX_NUM } @numbers ) {
     say 'At least one number is greater than '.MAX_NUM;
 }
 
-my @new_list = any { $_ > MAX_NUM } @numbers;
-say @new_list;
+my @strings = qw(una matina mi son svegliato o bella ciao. Partigiani sempre);
+say @strings;
+
+if ( any { length($_) > MAX_NUM } @strings ) {
+    say 'At least one string is greater than '.MAX_NUM;
+}
