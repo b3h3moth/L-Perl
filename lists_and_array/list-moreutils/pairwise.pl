e6a6c26ac3c954d7ea4d61086402da01cde21624
B3h3m0th <behemoth@autistici.org>
2015-05-05 22:28:56 +0200

    pairwise() example added

--- lists_and_array/list-moreutils/pairwise.pl
+++ lists_and_array/list-moreutils/pairwise.pl
@@ -8,3 +8,9 @@ use List::MoreUtils qw(pairwise);
 # Evaluates BLOCK for each pair of elements in ARRAY1 and ARRAY2 and returns a
 # new list consisting of BLOCK's return values. The two elements are set to $a
 # and $b.
+
+my @odd = qw(1 3 5 7 9);
+my @even = qw(2 4 6 8 10);
+
+my @numbers = pairwise { $a, $b } @odd, @even;
+say join ', ',@numbers;
