d99ff714b45231f049e5dfb29927fbc55868e447
B3h3m0th <behemoth@autistici.org>
2015-05-05 22:18:55 +0200

    added pairwise() syntax

--- lists_and_array/list-moreutils/pairwise.pl
+++ lists_and_array/list-moreutils/pairwise.pl
@@ -2,3 +2,8 @@
 use warnings;
 use strict;
 use v5.14;
+
+# pairwise BLOCK ARRAY1 ARRAY2;
+# Evaluates BLOCK for each pair of elements in ARRAY1 and ARRAY2 and returns a
+# new list consisting of BLOCK's return values. The two elements are set to $a
+# and $b.
