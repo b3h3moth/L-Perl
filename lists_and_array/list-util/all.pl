e7b8ac1467f63ada0a6f4735ee9a51087081fac7
B3h3m0th <behemoth@autistici.org>
2015-05-05 11:55:23 +0200

    added all() full example

--- lists_and_array/list-util/all.pl
+++ lists_and_array/list-util/all.pl
@@ -3,8 +3,16 @@ use warnings;
 use strict;
 use v5.14;
 use List::Util qw(all);
-use constant MAX_NUM => 10;
+use constant MAX_NUM => 5;
 
 # all { BLOCK } LIST;
 # It evalutes BLOCK, setting $_ to each element of LIST in turn. It returns 
 # true if all elements make the BLOCK return a true value, false otherwise.
+my @colors = qw(orange black white green);
+say join ', ',@colors;
+
+if ( all { length($_) > MAX_NUM } @colors ) {
+    say 'All strings are greater than '.MAX_NUM;
+} else {
+    say 'OPS ... any string is not greater than '.MAX_NUM;
+}
