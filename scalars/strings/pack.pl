28fdcd2261ef519b95eed6dfe0f88d6d3b1dcf44
B3h3m0th <behemoth@autistici.org>
2015-05-06 16:10:02 +0200

    pack() syntax added

--- scalars/strings/pack.pl
+++ scalars/strings/pack.pl
@@ -2,3 +2,8 @@
 use warnings;
 use strict;
 use v5.14;
+
+# pack TEMPLATE, LIST;
+# It takes a LIST of ordinary Perl values, converts them into a string of bytes
+# according to the TEMPLATE, and returns this string.
+
