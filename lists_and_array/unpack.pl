1073d544e71fe6b82182c3024cb444dd9537e788
B3h3m0th <behemoth@autistici.org>
2015-05-06 16:53:46 +0200

    unpack() comment added

--- lists_and_array/unpack.pl
+++ lists_and_array/unpack.pl
@@ -2,3 +2,10 @@
 use warnings;
 use strict;
 use v5.14;
+
+# unpack TEMPLATE, EXPR;
+# It expands a string (EXPR) into a list of values according to the TEMPLATE
+# and returns those values. This function does the reverse of pack.
+#
+# The TEMPLATE is a sequence of characters that give the order and type of
+# values: perldoc -f unpack (template characters table).
