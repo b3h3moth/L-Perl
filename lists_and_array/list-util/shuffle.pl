f2052cd164264a2f4d2517a2862b5babc107dbec
B3h3m0th <behemoth@autistici.org>
2015-05-06 12:23:13 +0200

    shuffle() example

--- lists_and_array/list-util/shuffle.pl
+++ lists_and_array/list-util/shuffle.pl
@@ -6,3 +6,8 @@ use List::Util qw(shuffle);
 
 # shuffle LIST;
 # It returns the values of the input in a random order.
+my @colors = (1..20);
+say 'before: '.join ', ',@colors;
+
+my @random_colors = shuffle @colors;
+say ' after: '.join ', ',@random_colors;
