954a2d4257887ab1380554884c744bd516660efd
B3h3m0th <behemoth@autistici.org>
2015-05-06 19:45:39 +0200

    pack() unpack() example

--- lists_and_array/convert_hal2ibm_with.pl
+++ lists_and_array/convert_hal2ibm_with.pl
@@ -2,3 +2,14 @@
 use warnings;
 use strict;
 use v5.14;
+
+# how to convert from HAL to IBM: 
+my $hal = "HAL";
+my @byte = unpack("C*", $hal);
+
+foreach my $val (@byte) {
+    $val++;
+}
+
+my $ibm = pack("C*", @byte);
+say "$ibm";  
