cff7c42f082fcb911d606612b00c90540b7e8a28
B3h3m0th <behemoth@autistici.org>
2015-05-07 19:27:20 +0200

    added localtime() syntax

--- dates_and_times/localtime.pl
+++ dates_and_times/localtime.pl
@@ -2,3 +2,13 @@
 use warnings;
 use strict;
 use v5.14;
+
+# localtime EXPR;
+# 
+# Converts a time as returned by the time function to a 9-element list with the
+# time analyzed for the local time zone.
+#
+# Typically used as follows:
+#   0     1     2      3      4     5      6      7      8
+# ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = 
+# localtime(time);
