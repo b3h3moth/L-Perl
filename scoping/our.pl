d598a91cd5e517606ac566963a9d9020f2fecce2
B3h3m0th <behemoth@autistici.org>
2015-05-07 20:23:30 +0200

    our syntax added

--- scoping/our.pl
+++ scoping/our.pl
@@ -2,3 +2,8 @@
 use warnings;
 use strict;
 use v5.14;
+
+# our VARIABLE;
+# our (LIST OF VARIABLES);
+# It allows you to declare package variables in the current package without
+# needing to use the full package name.
