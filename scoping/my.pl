bfc0691aee4cfe07555ef6cbacfb7d75df8900a6
B3h3m0th <behemoth@autistici.org>
2015-05-07 20:11:50 +0200

    my() syntax added

--- scoping/my.pl
+++ scoping/my.pl
@@ -5,3 +5,6 @@ use v5.14;
 
 # my VARIABLE;
 # my (LIST OF VARIABLES);
+# It declares the listed variables to be local to the enclosing block, file, 
+# or 'eval'. If more than one value is listed, the list must be placed in
+# parentheses.
