a05aad8bc0298aefcaa593f39011b394f3fa4fa8
B3h3m0th <behemoth@autistici.org>
2015-05-07 19:19:20 +0200

    improved constan example with few print out

--- scalars/constant_variables.pl
+++ scalars/constant_variables.pl
@@ -2,11 +2,14 @@
 use warnings;
 use strict;
 use v5.14;
-use constant BUF => 1024;
+use constant YEAR   => 2015;
 use constant EMAIL => 'behemoth@autistici.org';
+use constant NAMES => qw(francesca claudia ilaria ludovica);
+use constant CLUBS => { TO => 'Juventus' };
 
 # It declares the named symbol to be an immutable constant with the given
 # scalar or list value.
 
-say BUF;
-say EMAIL;
+say 'We are at '.YEAR.' my e-mail is: '.EMAIL;
+say 'All these women are beautiful: '.join ', ',NAMES;
+say 'My favorite club is: '.CLUBS->{'TO'}.' Football Club 1897';
