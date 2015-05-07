01b723317e030728b2b4f44c0c0c6d15688bf47d
B3h3m0th <behemoth@autistici.org>
2015-05-07 20:34:10 +0200

    state() example added

--- scoping/state.pl
+++ scoping/state.pl
@@ -6,3 +6,12 @@ use v5.14;
 # state VARIABLE;
 # It's like declaring variables with my(), but it's initialized only once and 
 # retain their value.
+
+sub counter {
+    state $counter = 1;
+    $counter++;
+}
+
+for (1..5) { 
+    say counter(); 
+}
