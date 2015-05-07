107e1b636908040c06e99bf704e948f1dc1636ed
B3h3m0th <behemoth@autistici.org>
2015-05-07 20:14:42 +0200

    local() syntax added

--- scoping/local.pl
+++ scoping/local.pl
@@ -2,3 +2,9 @@
 use warnings;
 use strict;
 use v5.14;
+
+# local VARIABLE;
+# local (LIST OF VARIABLES);
+# It scopes the value of a package variable or list of package variables to the
+# current file, block, or eval. Any changes made to 'localized' variables 
+# inside of that scope are forgotten outside of that scope.
