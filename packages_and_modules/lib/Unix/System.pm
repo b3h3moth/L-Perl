d28d8e7618ef5aa302ec73cfaa9ec41f1b56f762
B3h3m0th <behemoth@autistici.org>
2015-05-08 22:18:29 +0200

    System.pm module added

---
+++ packages_and_modules/lib/Unix/System.pm
@@ -0,0 +1,21 @@
+package Unix::System;
+
+use strict;
+use warnings;
+
+sub get_osname() {
+    my $os = qx/uname -o/ // 'OpenBSD';
+    chomp($os);
+    return $os;
+}
+
+# It's the same file, but different package
+package Unix::System::Path;
+
+sub get_sed_path() {
+    my $sed = qx/which sed/ // '/bin/sed';
+    chomp($sed);
+    return $sed;
+}
+
+1;
