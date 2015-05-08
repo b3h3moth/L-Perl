01255fe1e95355a0b071d13e26d2689c63256fde
B3h3m0th <behemoth@autistici.org>
2015-05-08 20:23:24 +0200

    fixed and improved comment about Module

--- packages_and_modules/lib/Unix/Utilities.pm
+++ packages_and_modules/lib/Unix/Utilities.pm
@@ -7,23 +7,24 @@ use Sys::Hostname;
 # A namespace is a place to organize logically related code and data, all 
 # subroutines and package variables declared in that namespace cannot be 
 # accessed outside of that namespace unless you prepend the package name to 
-# them or if the package "exports" the subroutines to other packages. This 
-# allows you to reuse names in different namespaces without worrying about
-# collision.
+# them or if the package exports the subroutines to other packages. This allows
+# you to reuse names in different namespaces without worrying about collision.
+
+# A package name is one or more identifiers separated by double colons.
 # 
-# A package name is one or more identifiers separated by double colons. 
-# By convention, a module starts his name with uppercase letter. By convention,
-# this module should correspond to a path and filename of 'Module.pm' and it 
-# should usually be located in a 'lib' directory (This example is 
-# lib/Unix/Utilities.pm).
+# By convention:
+# - a module starts his name with uppercase letter;
+# - a module should correspond to a path and filename of 'Module.pm' and it
+#   should usually be located in a 'lib' directory. The path of this module is
+#   lib/Unix/Utilities.pm.
 #
-# The .pm extension is what Perl uses to identify a given module. A module is 
-# simply a file that contains one or more packages, although it's generally 
+# The .pm extension is what Perl uses to identify a given module. A module is
+# simply a file that contains one or more packages, although it's generally
 # recommended to have one package per module.
 
-# It's also strongly recommended that module and package names correspond.
-# For example, a file called 'System/Unix/BSD/Kernel.pm' should contain a 
-# package named 'System::Unix::BSD::Kernel'.
+# It's also strongly recommended that module and package names correspond. For
+# example, a file called 'System/Unix/BSD/Kernel.pm' should contain a package 
+# named 'System::Unix::BSD::Kernel'.
 
 our $VERSION = 1.0;
 
