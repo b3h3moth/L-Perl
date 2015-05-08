6e19b9673a787841b868072c47925c12402b3a72
B3h3m0th <behemoth@autistici.org>
2015-05-08 20:42:49 +0200

    removed files, they are totally useless

--- packages_and_modules/01_first_module.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use warnings;
-use strict;
-use First; # module name
-
-die ("Usage: $0 <string>\n") if (@ARGV != 1);
-
-# routine from module is called (prepend the package name)
-print First::reverse_string($ARGV[0]);
