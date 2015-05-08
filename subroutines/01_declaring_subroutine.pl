9fd02ce54bae3944b927cdb67ced0c9ec3b5f969
B3h3m0th <behemoth@autistici.org>
2015-05-08 18:22:39 +0200

    comment about prgama added

--- subroutines/01_declaring_subroutine.pl
+++ subroutines/01_declaring_subroutine.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/env perl
 use warnings;
-use strict;
 use v5.14;
+use strict; # <use strict/warning/v5.14> is a prgama. 
+            # A pragma is a hint to a compiler, telling it something about the
+            # code. In this case, the <use strict> pragma  tells Perl's 
+            # internal compiler that it should enforce some good programming 
+            # rules for the rest of this block or source file.
+
 
 # We need to create a block of code and give it a name using 'sub' keyword:
 # sub function_name {
