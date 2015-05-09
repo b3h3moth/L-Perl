c999a67d372081946c8e8c1ce77d471708dec849
B3h3m0th <behemoth@autistici.org>
2015-05-09 17:28:27 +0200

    get package variable

--- packages_and_modules/lib/01_os_info.pl
+++ packages_and_modules/lib/01_os_info.pl
@@ -10,3 +10,6 @@ use Unix::Utilities;
 my $os_host = Unix::Utilities::get_hostname();
 my $id = Unix::Utilities::get_id();
 say 'My host is '.$os_host.', my ID is '.$id;
+
+# get package variable:
+say 'Module version: '.$Unix::Utilities::VERSION;
