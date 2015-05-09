66d91ad40ea152e861de338ed37ef4adb5f111a2
B3h3m0th <behemoth@autistici.org>
2015-05-09 20:03:06 +0200

    improved  sentence

--- packages_and_modules/lib/Unix/System.pm
+++ packages_and_modules/lib/Unix/System.pm
@@ -3,6 +3,9 @@ package Unix::System;
 use strict;
 use warnings;
 
+our $VERSION = '0.001';
+$VERSION = eval $VERSION;
+
 sub get_osname() {
     my $os = qx/uname -o/ // 'OpenBSD';
     chomp($os);
