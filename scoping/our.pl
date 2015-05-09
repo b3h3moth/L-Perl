fccb475298b2867177865c9dacd1029cb39a13cd
B3h3m0th <behemoth@autistici.org>
2015-05-09 16:26:40 +0200

    improved and fixed comment

--- scoping/our.pl
+++ scoping/our.pl
@@ -6,8 +6,8 @@ use v5.14;
 # our VARIABLE;
 # our (LIST OF VARIABLES);
 # It allows you to declare package variables in the current package without
-# needing to use the full package name.
-
+# needing to use the full package name. It's for package variables and not 
+# lexically scoped ones.
 package Bsd;
 
 our $version = 'OpenBSD 5.7 is out (1st May 2015)';
