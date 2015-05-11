b72582873e956286b24e2be9974a95c89ed02f13
B3h3m0th <behemoth@autistici.org>
2015-05-11 14:12:29 +0200

    fixed bytes_to_bit()

--- packages_and_modules/lib/Data/Conversion.pm
+++ packages_and_modules/lib/Data/Conversion.pm
@@ -3,15 +3,18 @@ package Data::Conversion;
 use warnings;
 use strict;
 use diagnostics;
-
-use constant BYTE_PER_BIT => 8;
+use Exporter 'import';
+our @EXPORT_OK = qw(bytes_to_bit);
+our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 
 our $VERSION = '0.001';
 $VERSION = eval $VERSION;
 
+use constant BYTE_PER_BIT => 8;
+
 sub bytes_to_bit {
     my $bytes = shift;
-    return (bytes_to_bit($bytes) * BYTE_PER_BIT);
+    return ($bytes * BYTE_PER_BIT);
 }
 
 1;
