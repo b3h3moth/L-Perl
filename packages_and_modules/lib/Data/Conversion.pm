197ecd73fc07891dd0c687bfc0fc25ffe12937ca
B3h3m0th <behemoth@autistici.org>
2015-05-11 19:05:17 +0200

    added kbytes_to_byte() function inside module

--- packages_and_modules/lib/Data/Conversion.pm
+++ packages_and_modules/lib/Data/Conversion.pm
@@ -4,17 +4,23 @@ use warnings;
 use strict;
 use diagnostics;
 use Exporter 'import';
-our @EXPORT_OK = qw(bytes_to_bit);
+our @EXPORT_OK = qw(bytes_to_bit kbytes_to_byte);
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 
 our $VERSION = '0.001';
 $VERSION = eval $VERSION;
 
-use constant BYTE_PER_BIT => 8;
+use constant   BYTE_PER_BIT => 8;
+use constant KBYTE_PER_BYTE => 1024;
 
 sub bytes_to_bit {
     my $bytes = shift;
     return ($bytes * BYTE_PER_BIT);
+}
+
+sub kbytes_to_byte {
+    my $kb = shift;
+    return ($kb * KBYTE_PER_BYTE);
 }
 
 1;
