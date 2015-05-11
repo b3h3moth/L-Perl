b4d19ce3a513cf9df413a0cb9fff78c35f3896a5
B3h3m0th <behemoth@autistici.org>
2015-05-11 13:48:47 +0200

    fixed return's value

--- packages_and_modules/lib/Data/Conversion.pm
+++ packages_and_modules/lib/Data/Conversion.pm
@@ -2,15 +2,16 @@ package Data::Conversion;
 
 use warnings;
 use strict;
+use diagnostics;
 
-use constant BYTE_PER_BIT = 8;
+use constant BYTE_PER_BIT => 8;
 
 our $VERSION = '0.001';
 $VERSION = eval $VERSION;
 
 sub bytes_to_bit {
     my $bytes = shift;
-    return bytes_to_bit($bytes) * BYTE_PER_BIT;
+    return (bytes_to_bit($bytes) * BYTE_PER_BIT);
 }
 
 1;
