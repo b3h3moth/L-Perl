4c0d24d70c8620931e0858e324389717c90da7d5
B3h3m0th <behemoth@autistici.org>
2015-05-11 13:42:22 +0200

    added bytes_to_bit() function

--- packages_and_modules/lib/Data/Conversion.pm
+++ packages_and_modules/lib/Data/Conversion.pm
@@ -1,11 +1,16 @@
-package Package::Package;
+package Data::Conversion;
 
 use warnings;
 use strict;
 
-use lib 'library name';
+use constant BYTE_PER_BIT = 8;
 
 our $VERSION = '0.001';
 $VERSION = eval $VERSION;
+
+sub bytes_to_bit {
+    my $bytes = shift;
+    return bytes_to_bit($bytes) * BYTE_PER_BIT;
+}
 
 1;
