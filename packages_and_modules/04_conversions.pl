256f3f8d1e00090f05e5a0031fe8040f58dd05c3
B3h3m0th <behemoth@autistici.org>
2015-05-11 19:05:52 +0200

    added kbytes_to_byte() testing

--- packages_and_modules/04_conversions.pl
+++ packages_and_modules/04_conversions.pl
@@ -4,6 +4,7 @@ use strict;
 use v5.14;
 
 use lib 'lib';
-use Data::Conversion qw(bytes_to_bit);
+use Data::Conversion qw(bytes_to_bit kbytes_to_byte);
 
-say 'There are '.bytes_to_bit(4).'bit in 4byte';
+say 'There are '.bytes_to_bit(1).'bit in 1byte';
+say 'There are '.kbytes_to_byte(1).'byte in 1Kb';
