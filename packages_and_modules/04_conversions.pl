5ad00b994b14e216541e48a36f4ca029831f4ee9
B3h3m0th <behemoth@autistici.org>
2015-05-11 20:36:07 +0200

    testing bytes_to_bit() with @array

--- packages_and_modules/04_conversions.pl
+++ packages_and_modules/04_conversions.pl
@@ -6,5 +6,9 @@ use v5.14;
 use lib 'lib';
 use Data::Conversion qw(bytes_to_bit kbytes_to_byte);
 
-say 'There are '.bytes_to_bit(1).'bit in 1byte';
+my @numbers = qw(1 2 4 6 8 10 100);
+
+foreach my $num (@numbers) {
+    say 'There are '.bytes_to_bit($num).'bit in '.$num.'byte';
+}
 say 'There are '.kbytes_to_byte(1).'byte in 1Kb';
