9141cd1af69d4a92320313448acebc53cf6dedb4
B3h3m0th <behemoth@autistici.org>
2015-05-12 22:41:13 +0200

    replaced all conversion functions name

--- packages_and_modules/04_conversions.pl
+++ packages_and_modules/04_conversions.pl
@@ -4,18 +4,18 @@ use strict;
 use v5.14;
 
 use lib 'lib';
-use Data::Conversion qw(bytes_to_bit kbytes_to_byte mbytes_to_kbytes);
+use Data::Conversion qw(byte_to_bit kbyte_to_byte mbyte_to_kbyte);
 
 my @numbers = qw(1 2 4 6 8 10 100);
 
 foreach my $num (@numbers) {
-    say 'There are '.bytes_to_bit($num).'bit in '.$num.'byte';
+    say 'There are '.byte_to_bit($num).'bit in '.$num.'byte';
 }
 
 foreach my $num (@numbers) {
-    say 'There are '.kbytes_to_byte($num).'byte in '.$num.'Kb';
+    say 'There are '.kbyte_to_byte($num).'byte in '.$num.'Kb';
 }
 
 foreach my $num (@numbers) {
-    say 'There are '.mbytes_to_kbytes($num).'Kb in '.$num.'Mb';
+    say 'There are '.mbyte_to_kbyte($num).'Kb in '.$num.'Mb';
 }
