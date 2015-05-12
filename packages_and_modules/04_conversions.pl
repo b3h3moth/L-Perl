a180a26a3932f12c2a6f4beb8b868a029adb93b6
B3h3m0th <behemoth@autistici.org>
2015-05-12 22:34:53 +0200

    testing mbytes_to_kbytes()

--- packages_and_modules/04_conversions.pl
+++ packages_and_modules/04_conversions.pl
@@ -4,7 +4,7 @@ use strict;
 use v5.14;
 
 use lib 'lib';
-use Data::Conversion qw(bytes_to_bit kbytes_to_byte);
+use Data::Conversion qw(bytes_to_bit kbytes_to_byte mbytes_to_kbytes);
 
 my @numbers = qw(1 2 4 6 8 10 100);
 
@@ -14,4 +14,8 @@ foreach my $num (@numbers) {
 
 foreach my $num (@numbers) {
     say 'There are '.kbytes_to_byte($num).'byte in '.$num.'Kb';
+}
+
+foreach my $num (@numbers) {
+    say 'There are '.mbytes_to_kbytes($num).'Kb in '.$num.'Mb';
 }
