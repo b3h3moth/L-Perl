c8c9366b79065ae2c1ae9aecee815e494a137288
B3h3m0th <behemoth@autistici.org>
2015-05-12 22:35:22 +0200

    added mbytes_to_kbytes() to Data::Conversion module

--- packages_and_modules/lib/Data/Conversion.pm
+++ packages_and_modules/lib/Data/Conversion.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use diagnostics;
 use Exporter 'import';
-our @EXPORT_OK = qw(bytes_to_bit kbytes_to_byte);
+our @EXPORT_OK = qw(bytes_to_bit kbytes_to_byte mbytes_to_kbytes);
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 
 our $VERSION = '0.001';
@@ -12,6 +12,7 @@ $VERSION = eval $VERSION;
 
 use constant   BYTE_PER_BIT => 8;
 use constant KBYTE_PER_BYTE => 1024;
+use constant MBYTE_PER_KBYTE => 1024;
 
 sub bytes_to_bit {
     my $bytes = shift;
@@ -21,6 +22,11 @@ sub bytes_to_bit {
 sub kbytes_to_byte {
     my $kb = shift;
     return ($kb * KBYTE_PER_BYTE);
+}
+
+sub mbytes_to_kbytes {
+    my $mb = shift;
+    return ($mb * MBYTE_PER_KBYTE);
 }
 
 1;
