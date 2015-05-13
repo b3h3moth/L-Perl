3c4745f9a7c92170df66aba876f57da13c174c7e
B3h3m0th <behemoth@autistici.org>
2015-05-13 17:33:44 +0200

    finddepth() syntax

--- filesystem/file-find/finddepth.pl
+++ filesystem/file-find/finddepth.pl
@@ -4,4 +4,10 @@ use strict;
 use v5.14;
 use File::Find 'finddepth';
 
-
+# finddepth(\&wanted,  @directories);
+#
+# finddepth() works just like find() except that it invokes the wanted() 
+# function for a directory after invoking it for the directory's contents. It 
+# does a postorder traversal instead of a preorder traversal, working from the
+# bottom of the directory tree up where find() works from the top of the tree
+# down.
