21ed5e22f5cc3f1a7ec5e88855523f98389803d1
B3h3m0th <behemoth@autistici.org>
2015-05-13 14:04:10 +0200

    File::Find 'find' syntax and comment added

--- filesystem/file-find/find.pl
+++ filesystem/file-find/find.pl
@@ -4,6 +4,10 @@ use strict;
 use v5.14;
 use File::Find 'find';
 
+# find(\&wanted,  @directories);
+# find() does a depth-first search over the given @directories in the order 
+# they are given. For each file or directory found, it calls the &wanted 
+# subroutine.
 my $dir = '/etc';
 
 opendir(my $dh, $dir) or die "Could not open $dir for reading: $!\n";
