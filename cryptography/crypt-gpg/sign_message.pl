767e74b854a430b20a0da074e949b079af4fa829
B3h3m0th <behemoth@autistici.org>
2015-05-14 11:24:12 +0200

    Crypt::GPG, new object

--- cryptography/crypt-gpg/sign_message.pl
+++ cryptography/crypt-gpg/sign_message.pl
@@ -3,3 +3,15 @@ use warnings;
 use strict;
 use v5.14;
 use Crypt::GPG;
+
+# Creates new object
+my $gpg = new Crypt::GPG;
+
+# Path of gpg executable
+$gpg->gpgbin('/usr/bin/gpg');
+
+# Set ID of default secret key
+$gpg->secretkey('0x67FD0AE6');
+
+# Set passphrase
+$gpg->passphrase('1gl3wxba9f5r');
