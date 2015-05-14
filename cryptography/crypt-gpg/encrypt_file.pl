4ac07342c65700ab310d9cf10dc8582e590353ff
B3h3m0th <behemoth@autistici.org>
2015-05-14 22:21:22 +0200

    encrypt file added

---
+++ cryptography/crypt-gpg/encrypt_file.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Crypt::GPG;
+
+# Config informations
+my $mail = 'behemoth@autistici.org';
+my $secret_key_line = qx/gpg --list-key $mail | grep pub/;
+my ($secret_key) = $secret_key_line =~ /.*\/(.*) /;
+my $passphrase = '1gl3wxba9f5r';
+
+# Get new gpg object
+my $gpg = new Crypt::GPG;
+# Path of gpg executable
+$gpg->gpgbin('/usr/bin/gpg');
+# Set ID of default secret key
+$gpg->secretkey($secret_key);
+# Set passphrase 
+$gpg->passphrase($passphrase);
+# Debug
+$gpg->debug();
+
+open(my $INPUT, "< test.txt");
+open(my $OUTPUT, "> test.txt.gpg");
+
+my @encrypted = $gpg->encrypt($INPUT, $mail);
+
+foreach (@encrypted) {
+    print $OUTPUT $_;
+}
+
+close($INPUT);
+close($OUTPUT);
