ad152bcf14e3e053add34149b79f907bb1cdf41b
B3h3m0th <behemoth@autistici.org>
2015-05-14 22:24:31 +0200

    replaced input and output files

--- cryptography/crypt-gpg/encrypt_file.pl
+++ cryptography/crypt-gpg/encrypt_file.pl
@@ -9,6 +9,8 @@ my $mail = 'behemoth@autistici.org';
 my $secret_key_line = qx/gpg --list-key $mail | grep pub/;
 my ($secret_key) = $secret_key_line =~ /.*\/(.*) /;
 my $passphrase = '1gl3wxba9f5r';
+my $input_file = 'clear_text.txt';
+my $encrypted_file = "$input_file.gpg";
 
 # Get new gpg object
 my $gpg = new Crypt::GPG;
