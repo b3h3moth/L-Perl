a1f043a866619c332cfbe1ce11e866cd97148337
B3h3m0th <behemoth@autistici.org>
2015-05-15 18:59:34 +0200

    parameters control

--- cryptography/crypt-gpg/decrypt_file.pl
+++ cryptography/crypt-gpg/decrypt_file.pl
@@ -9,3 +9,5 @@ my $mail = 'behemoth@autistici.org';
 my $secret_key_line = qx/gpg --list-key $mail | grep pub/;
 my ($secret_key) = $secret_key_line =~ /.*\/(.*) /;
 my $passphrase = '1gl3wxba9f5r';
+
+die("Usage: $0 <file to encrypt>") if (@ARGV != 1);
