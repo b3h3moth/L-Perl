f7a2b9fbc7c75eaed28eb285039033feb45d1f86
B3h3m0th <behemoth@autistici.org>
2015-05-15 19:05:42 +0200

    fixed comments

--- regexp/02_operator_2_substitute.pl
+++ regexp/02_operator_2_substitute.pl
@@ -5,8 +5,8 @@ use v5.14;
 
 my $sequence = "AACTAGCGGATTCCAGACCGT";
 
-#'s///' when we want to substitute a pattern with a string
-#'s/match/replacement/'
+# 's///' when we want to substitute a pattern with a string
+# 's/match/replacement/'
 say "Before: $sequence";
 $sequence =~ s/GATTCCA/_PERL_LANGUAGE_/;
 say " After: $sequence";
@@ -14,14 +14,14 @@ say " After: $sequence";
 my $url_one = "http://iloveperl.com";
 say "Before: $url_one";
 
-#with escaping is ok but there are better alternatives
+# with escaping is ok but there are better alternatives
 $url_one =~ s/http:\/\///;
 say " After: $url_one";
 
-#alternatives are:
-#$url_one =~ s!http://!!;
-#$url_one =~ s@http://@@;
-#$url_one =~ s#http://##;
+# alternatives are:
+# $url_one =~ s!http://!!;
+# $url_one =~ s@http://@@;
+# $url_one =~ s#http://##;
 
 my $url_two = "http://www.perl.com";
 say "Before: $url_two";
