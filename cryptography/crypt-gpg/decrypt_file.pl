29f082ac513cbe40830336d93b746e2d09673500
B3h3m0th <behemoth@autistici.org>
2015-05-15 18:58:55 +0200

    secret key and passphrase

--- cryptography/crypt-gpg/decrypt_file.pl
+++ cryptography/crypt-gpg/decrypt_file.pl
@@ -7,3 +7,5 @@ use Crypt::GPG;
 # Config informations:
 my $mail = 'behemoth@autistici.org';
 my $secret_key_line = qx/gpg --list-key $mail | grep pub/;
+my ($secret_key) = $secret_key_line =~ /.*\/(.*) /;
+my $passphrase = '1gl3wxba9f5r';
