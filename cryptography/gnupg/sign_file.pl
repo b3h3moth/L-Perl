d51f4e1f7cecb0349f6a5e65902b01241738de5d
B3h3m0th <behemoth@autistici.org>
2015-05-16 19:10:02 +0200

    sign() file

---
+++ cryptography/gnupg/sign_file.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use GnuPG;
+
+use constant USERID     => 'behemoth@autistici.org';
+use constant UNTRUSTED  => 'Mister Behemoth';
+use constant PASSWD     => '1gl3wxba9f5r';
+
+my $gpg = new GnuPG();
+
+$gpg->sign( 
+    recipient   => [ USERID ],
+    plaintext   => 'test.txt',
+    output      => 'test.sig',
+    armor       => 1,
+    passphrase  => PASSWD
+);
