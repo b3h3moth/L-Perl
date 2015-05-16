76f3dcf4b0182a61fcfe8ecd8c8f2b707b482b77
B3h3m0th <behemoth@autistici.org>
2015-05-16 18:58:39 +0200

    added new file

---
+++ cryptography/gnupg/export_secret_key.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use GnuPG;
+
+use constant USERID     => 'behemoth@autistici.org';
+
+my $gpg = new GnuPG();
+
+$gpg->export_keys(
+    keys    => USERID,
+    armor   => 1,
+    secret  => 1,
+    output  => 'key.sec'
+);
