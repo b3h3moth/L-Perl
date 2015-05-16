70b612e34c424bba48171f334b74feb359cfa501
B3h3m0th <behemoth@autistici.org>
2015-05-16 18:54:51 +0200

    export_keys() example added

--- cryptography/gnupg/export_key.pl
+++ cryptography/gnupg/export_key.pl
@@ -3,3 +3,13 @@ use warnings;
 use strict;
 use v5.14;
 use GnuPG;
+
+use constant USERID     => 'behemoth@autistici.org';
+
+my $gpg = new GnuPG();
+
+$gpg->export_keys(
+    keys    => USERID,
+    armor   => 1,
+    output  => 'key.pub'
+);
