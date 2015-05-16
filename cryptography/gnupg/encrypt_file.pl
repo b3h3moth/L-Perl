c1b287999cf183ee4421ad50e6c2b30045b4044e
B3h3m0th <behemoth@autistici.org>
2015-05-16 18:10:21 +0200

    encrypt file with GnuPG

--- cryptography/gnupg/encrypt_file.pl
+++ cryptography/gnupg/encrypt_file.pl
@@ -3,3 +3,16 @@ use warnings;
 use strict;
 use v5.14;
 use GnuPG;
+
+use constant USERID     => 'behemoth@autistici.org';
+use constant UNTRUSTED  => 'Mister Behemoth';
+use constant PASSWD     => '1gl3wxba9f5r';
+my $gpg = new GnuPG();
+
+$gpg->encrypt( 
+    recipient => [ USERID, UNTRUSTED ],
+    plaintext    => 'test.txt',
+    output       => 'test.gpg',
+    armor        => 1,
+    passphrase   => PASSWD
+);
