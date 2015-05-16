d202b508fe92eee89462219bb9e0fdedd9eac089
B3h3m0th <behemoth@autistici.org>
2015-05-16 18:23:13 +0200

    decrypt() file with GnuPG

--- cryptography/gnupg/decrypt_file.pl
+++ cryptography/gnupg/decrypt_file.pl
@@ -2,3 +2,16 @@
 use warnings;
 use strict;
 use v5.14;
+use GnuPG;
+
+use constant USERID     => 'behemoth@autistici.org';
+use constant UNTRUSTED  => 'Mister Behemoth';
+use constant PASSWD     => '1gl3wxba9f5r';
+
+my $gpg = new GnuPG();
+
+$gpg->decrypt( 
+    output      => 'test.txt',
+    ciphertext  => 'test.gpg',
+    passphrase  => PASSWD
+);
