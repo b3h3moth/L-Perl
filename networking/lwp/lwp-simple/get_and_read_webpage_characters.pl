7afc7eb6ff94a794a27f250a617590bab4cdcc0b
B3h3m0th <behemoth@autistici.org>
2015-05-17 21:04:07 +0200

    get() testing

--- networking/lwp/lwp-simple/get_and_read_webpage_characters.pl
+++ networking/lwp/lwp-simple/get_and_read_webpage_characters.pl
@@ -3,3 +3,7 @@ use warnings;
 use strict;
 use v5.14;
 use LWP::Simple 'get';
+
+my $remote_url = 'http://www.defensecode.com/public/DefenseCode_Unix_WildCards_Gone_Wild.txt';
+my $filename = get($remote_url);
+say "Page starts with: ", substr $filename, 0, 94;
