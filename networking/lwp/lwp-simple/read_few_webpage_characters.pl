02da857a73bc096a8a5b49aea65160ba0ca56e32
B3h3m0th <behemoth@autistici.org>
2015-05-17 21:29:27 +0200

    constant and comment added

--- networking/lwp/lwp-simple/read_few_webpage_characters.pl
+++ networking/lwp/lwp-simple/read_few_webpage_characters.pl
@@ -4,6 +4,9 @@ use strict;
 use v5.14;
 use LWP::Simple 'get';
 
+use constant MAX_CHAR => 94;
+
+# Read first 94 characters of webpage stored into $filename
 my $remote_url = 'http://www.defensecode.com/public/DefenseCode_Unix_WildCards_Gone_Wild.txt';
 my $filename = get($remote_url);
-say "Page starts with: ", substr $filename, 0, 94;
+say "Page starts with: ", substr $filename, 0, MAX_CHAR;
