eb3f80f0f4309c40f26e8c68e957d2eb26c6e581
B3h3m0th <behemoth@autistici.org>
2015-05-17 19:57:05 +0200

    defined() syntax added

--- builtin_functions/defined.pl
+++ builtin_functions/defined.pl
@@ -2,3 +2,7 @@
 use warnings;
 use strict;
 use v5.14;
+
+# defined EXPR
+# Returns a boolean value telling whether EXPR has a value other than the 
+# undefined value "undef". If EXPR is not present, $_ is checked.
