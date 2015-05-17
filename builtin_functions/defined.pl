ca518d1b823613a642a1133be4b6f105a601a04b
B3h3m0th <behemoth@autistici.org>
2015-05-17 19:57:19 +0200

    defined() testing

--- builtin_functions/defined.pl
+++ builtin_functions/defined.pl
@@ -6,3 +6,9 @@ use v5.14;
 # defined EXPR
 # Returns a boolean value telling whether EXPR has a value other than the 
 # undefined value "undef". If EXPR is not present, $_ is checked.
+
+my $name = 'behemoth';
+my $surname;
+
+say $name if (defined($name));
+say $surname if (defined($surname)) or die('$surname has undef value');
