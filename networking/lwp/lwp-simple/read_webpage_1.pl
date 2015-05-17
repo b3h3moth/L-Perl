3b62d53da0c088cbf7a7f93cabe0a8022a4beb99
B3h3m0th <behemoth@autistici.org>
2015-05-17 21:27:39 +0200

    added the program's purpose

--- networking/lwp/lwp-simple/read_webpage_1.pl
+++ networking/lwp/lwp-simple/read_webpage_1.pl
@@ -4,6 +4,7 @@ use strict;
 use v5.14;
 use LWP::Simple 'get';
 
+# How to read webpage with scalar
 my $remote_url = 'http://www.lib.ru/unixhelp/security.txt';
 my $filename = get($remote_url);
 say $filename;
