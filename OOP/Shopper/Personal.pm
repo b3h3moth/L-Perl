4b18348586b6cf7090d12f34f6b58e5be0a10cdb
B3h3m0th <behemoth@autistici.org>
2015-05-18 19:51:12 +0200

    blueprint constructor

--- OOP/Shopper/Personal.pm
+++ OOP/Shopper/Personal.pm
@@ -4,4 +4,10 @@ use warnings;
 use strict;
 
 sub new {
+    my ($class, $arg_for) = @_;
+
+    return bless {
+        name    => $arg_for->{name},
+        budget  => $arg_for->{budget},
+    }, $class;
 }
