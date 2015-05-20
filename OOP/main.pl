aae0e843d5f3a81ad440677760bb9669e6fa223c
B3h3m0th <behemoth@autistici.org>
2015-05-20 18:02:51 +0200

    comment about parameter of Class->method invocation

--- OOP/main.pl
+++ OOP/main.pl
@@ -12,7 +12,7 @@ use Sheep;
 #Horse::speak;
 #Sheep::speak;
 
-my @pasture = qw(Cow Horse Sheep Sheep Cow Cow Horse Horse Sheep);
+#my @pasture = qw(Cow Horse Sheep Sheep Cow Cow Horse Horse Sheep);
 #foreach my $beast (@pasture) {
 #    no strict 'refs';
 #    # symbolic coderef dereferencing
@@ -26,6 +26,10 @@ my @pasture = qw(Cow Horse Sheep Sheep Cow Cow Horse Horse Sheep);
 #Sheep->speak;
 
 # without symbolic coderefs.
-foreach my $beast (@pasture) {
-    $beast->speak;
-}
+#foreach my $beast (@pasture) {
+#    $beast->speak;
+#}
+
+# The invocation of Cow->speak() attempts to invoke the subroutine 
+# Cow::speak('Class', @args); this means that we get the class name as the 
+# first parameter, or the only parameter, if no arguments are given.
