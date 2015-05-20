f448e6c99b26b3314b2a57cecd8e0768c336d36a
B3h3m0th <behemoth@autistici.org>
2015-05-20 17:57:50 +0200

    previous line became comments

--- OOP/main.pl
+++ OOP/main.pl
@@ -8,22 +8,22 @@ use Horse;
 use Sheep;
 
 # Called with full package name
-Cow::speak;
-Horse::speak;
-Sheep::speak;
+#Cow::speak;
+#Horse::speak;
+#Sheep::speak;
 
 my @pasture = qw(Cow Horse Sheep Sheep Cow Cow Horse Horse Sheep);
-foreach my $beast (@pasture) {
-    no strict 'refs';
-    # symbolic coderef dereferencing
-    &{$beast."::speak"};
-}
+#foreach my $beast (@pasture) {
+#    no strict 'refs';
+#    # symbolic coderef dereferencing
+#    &{$beast."::speak"};
+#}
 
 # In Perl isn't a difference between a subroutine and a method.
 # Method invocation arrow.
-Cow->speak;
-Horse->speak;
-Sheep->speak;
+#Cow->speak;
+#Horse->speak;
+#Sheep->speak;
 
 # without symbolic coderefs.
 foreach my $beast (@pasture) {
