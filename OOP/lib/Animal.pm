2fd57bf3cb2f2f3e6a3cea68404b370ac66f221c
B3h3m0th <behemoth@autistici.org>
2015-05-20 19:22:17 +0200

    Added Animal class

--- OOP/lib/Animal.pm
+++ OOP/lib/Animal.pm
@@ -1,6 +1,14 @@
 package Animal;
 
-use strict;
-use warning;
+use v5.14;
+
+sub speak {
+    my $class = shift;
+    say "A $class goes ", $class->sound;
+}
+
+sub sound {
+    die 'You have define sound() in a subclass';
+}
 
 1;
