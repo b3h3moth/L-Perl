14281382ec56056c0867f1dae20ce1b7a2c82c22
B3h3m0th <behemoth@autistici.org>
2015-05-20 12:23:26 +0200

    coderef dereferencing & comment about difference between subroutine and method

--- OOP/main.pl
+++ OOP/main.pl
@@ -12,7 +12,11 @@ Cow::speak;
 Horse::speak;
 Sheep::speak;
 
-Cow->speak;
-Horse->speak;
-Sheep->speak;
+my @pasture = qw(Cow Horse Sheep Sheep Cow Cow Horse Horse Sheep);
+foreach my $anim (@pasture) {
+    no strict 'refs';
+    # symbolic coderef dereferencing
+    &{$anim."::speak"};
+}
 
+# In Perl isn't a difference between a subroutine and a method
