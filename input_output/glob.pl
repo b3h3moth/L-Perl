97ef999a36f798e878f643b7d3964ce1c567022b
B3h3m0th <behemoth@autistici.org>
2015-05-22 19:40:53 +0200

    globbing, syntax definition

--- input_output/glob.pl
+++ input_output/glob.pl
@@ -2,3 +2,9 @@
 use warnings;
 use strict;
 use v5.14;
+
+# glob EXPR
+# In scalar context, glob iterates through such filename expansions, returning
+# undef when the list is exhausted. If EXPR is omitted, $_ is used. 
+#
+# Globbing: patterns to match files and directories.
