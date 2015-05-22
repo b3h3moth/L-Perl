ea6490c076aa1b93a812d42b9db357209d94515c
B3h3m0th <behemoth@autistici.org>
2015-05-22 19:46:21 +0200

    glob() syntax, list context

--- input_output/glob.pl
+++ input_output/glob.pl
@@ -4,7 +4,9 @@ use strict;
 use v5.14;
 
 # glob EXPR
+#
+# In list context, returns a list of filename expansions on the value of EXPR.
 # In scalar context, glob iterates through such filename expansions, returning
 # undef when the list is exhausted. If EXPR is omitted, $_ is used. 
-#
+# 
 # Globbing: patterns to match files and directories.
