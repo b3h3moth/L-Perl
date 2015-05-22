eb3f6cfe9dc4244711b65f94a9aeac6082c52440
B3h3m0th <behemoth@autistici.org>
2015-05-22 22:50:14 +0200

    print filename into directory and check each one

--- input_output/globdir.pl
+++ input_output/globdir.pl
@@ -5,5 +5,18 @@ use v5.14;
 
 # Get a list of the current directory and test each file
 foreach (glob '*') {
-    say $_;
+    # remove current '.' and parent directory '..'
+    # next if ($_ eq '.' or $_ eq '..');
+
+    # trunc filename if greater than 30 characters
+    print $_, " " x (30 - length($_));
+
+    print "-d" if -d $_;
+    print "-r" if -r $_;
+    print "-w" if -w $_;
+    print "-x" if -x $_;
+    print "-o" if -o $_;
+    print "\t";
+    print -s $_ if -r $_ and -f $_;
+    print "\n";
 }
