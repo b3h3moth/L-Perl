e8e8b27c6f5bf61fe9adf452f5fe1af4a040220c
B3h3m0th <behemoth@autistici.org>
2015-05-23 23:34:17 +0200

    print out address book date and added a comment

--- references/address_book.pl
+++ references/address_book.pl
@@ -7,8 +7,11 @@ use v5.14;
 
 my %address_book;
 
-
+# Basic structure is an hash referenced by address_book, it's an anonymous
+# hash.
 $address_book{"luca"} = {
     address     => "san lorenzo",
     cellphone   => "1234556",
 };
+
+say $address_book{luca}{address};
