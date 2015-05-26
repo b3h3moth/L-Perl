9c2c72343dedd5ba32e8eb9f096ff3b6269ba482
B3h3m0th <behemoth@autistici.org>
2015-05-26 19:51:02 +0200

    removed v5.14 pragma

--- OOP/lib/Person/main1.pl
+++ OOP/lib/Person/main1.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use v5.14;
 
 use Person1; # Now I can start using Person.pm package
--- OOP/lib/Person/main2.pl
+++ OOP/lib/Person/main2.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use v5.14;
 use Person2;
 
 my $person = Person2->new();
