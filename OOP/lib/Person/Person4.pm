9e53d4636b30d8910fd2895b92888a37d6a6ff7d
B3h3m0th <behemoth@autistici.org>
2015-05-26 19:38:36 +0200

    provied new class, person4

---
+++ OOP/lib/Person/Person4.pm
@@ -0,0 +1,18 @@
+package Person3;
+
+use strict;
+
+# How to provide object methods
+
+# The constructor
+sub new {
+    my $class = shift;      # The class name, passed as the first argument
+    my $self = {@_};        # Get arguments passed to new()
+    bless $self, $class;
+
+    return $self;
+}
+
+1;
+__END__
+See main3.pl, how to send attributes to this class
