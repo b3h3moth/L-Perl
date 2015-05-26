24e3b593f42e68e3d90a30362ac411061c92a878
B3h3m0th <behemoth@autistici.org>
2015-05-26 22:03:29 +0200

    fixed source code indentation

--- OOP/lib/Person/Person4.pm
+++ OOP/lib/Person/Person4.pm
@@ -8,43 +8,38 @@ use strict;
 sub new {
     my $class = shift;      # The class name, passed as the first argument
     my $self = {@_};        # Get arguments passed to new()
-    bless $self, $class;
 
+    bless $self, $class;
     return $self;
 }
 
 # Get firstname 
 sub first_name {
     my $self = shift;
-    
     return $self->{firstname};
 }
 
 # Get nickname
 sub nickname {
     my $self = shift;
-    
     return $self->{nickname};
 }
 
 # Get address
 sub address {
     my $self = shift;
-    
     return $self->{address};
 }
 
 # Get country
 sub country {
     my $self = shift;
-    
     return $self->{country};
 }
 
 # Get occupation
 sub occupation {
     my $self = shift;
-    
     return $self->{occupation};
 }
 
