2cb8e58635d6a2b31117bf60cc8141bac41146b7
B3h3m0th <behemoth@autistici.org>
2015-05-26 17:51:10 +0200

    OOP Perl in depth

--- OOP/lib/Person/Person2.pm
+++ OOP/lib/Person/Person2.pm
@@ -5,7 +5,7 @@ use strict;
 # Constructor
 sub new { 
     my $self = {};              # hash reference
-    bless $self, "Person2";     # $self became a Person2 reference
+    bless $self, "Person2";     # $self became an object of Person2 class
     return $self;
 }
 
