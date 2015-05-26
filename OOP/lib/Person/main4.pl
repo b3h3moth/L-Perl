e8fc3c46b65a0dc1afa64880f9196212a8f73c7d
B3h3m0th <behemoth@autistici.org>
2015-05-26 21:57:52 +0200

    invoke occupation()

--- OOP/lib/Person/main4.pl
+++ OOP/lib/Person/main4.pl
@@ -8,9 +8,11 @@ my $person = Person4->new(
     firstname   => "mister",
     lastname    => "behemoth",
     address     => "5th avenue",
-    country     => "italy"
+    country     => "italy",
+    occupation  => "Developer"
 );
 
 say 'Firstname is: '.$person->first_name();
 say 'Lastname is: '.$person->last_name();
 say 'Country is: '.$person->country();
+say 'Occupation is: '.$person->occupation();
