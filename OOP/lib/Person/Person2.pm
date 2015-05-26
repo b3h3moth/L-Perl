4f713059075112c51e7a49a5affade0bb1946136
B3h3m0th <behemoth@autistici.org>
2015-05-26 18:16:46 +0200

    added class name, stored in

--- OOP/lib/Person/Person2.pm
+++ OOP/lib/Person/Person2.pm
@@ -3,9 +3,10 @@ package Person2;
 use strict;
 
 # Constructor
-sub new { 
-    my $self = {};              # hash reference
-    bless $self, "Person2";     # $self became an object of Person2 class
+sub new {
+    my $class = shift;       # The first thing passed is the class name
+    my $self = {};           # Hash reference
+    bless $self, $class;     # $self became an object of the class $class
     return $self;
 }
 
