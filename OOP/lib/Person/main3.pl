f849c0cbd581503c41aa070bf02d9f8f667b4a59
B3h3m0th <behemoth@autistici.org>
2015-05-26 19:24:24 +0200

    testing Class->new(list of attributes)

--- OOP/lib/Person/main3.pl
+++ OOP/lib/Person/main3.pl
@@ -5,5 +5,16 @@ use v5.14;
 use Data::Dumper;
 use Person3;
 
-my $person = Person3->new("behemoth","italy","Computer Scientis","Perl");
-print Dumper \$person;
+my $person_a = Person3->new("mister","behemoth","47th avenue","Italy");
+
+my $person_b = Person3->new(
+    firstname   => "mister",
+    lastname    => "behemoth",
+    address     => "5th avenue",
+    country     => "Italy",
+    city        => "campobasso",
+    occupation  => "Computer Scientist"
+);
+
+print Dumper \$person_a;
+print Dumper \$person_b;
