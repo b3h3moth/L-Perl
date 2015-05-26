6c9b19bb0d3be2c3ac5b92669bab03614cc94815
B3h3m0th <behemoth@autistici.org>
2015-05-26 16:48:58 +0200

    OOP rules

--- OOP/lib/Person/Person1.pm
+++ OOP/lib/Person/Person1.pm
@@ -3,5 +3,9 @@ package Person1;
 use strict;
 
 1;
+
 __END__
+
 - The name of the package is the same as the name of class/package
+- Perl expects a true value as the last thing in the package
+- How to start using this class? With "use Person1" in a separate file
