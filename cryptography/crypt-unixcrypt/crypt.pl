50e61af7a6109e9c2f62afa9432808cc5b28f856
B3h3m0th <behemoth@autistici.org>
2015-05-26 10:25:01 +0200

    added Override builtin

--- cryptography/crypt-unixcrypt/crypt.pl
+++ cryptography/crypt-unixcrypt/crypt.pl
@@ -2,8 +2,11 @@
 use warnings;
 use strict;
 use v5.14;
+BEGIN { $Crypt::UnixCrypt::OVERRIDE_BUILTIN = 1 }
 use Crypt::UnixCrypt;
 
 my $text = "My name is Behemoth";
 my $hashed = crypt($text, "A9");
 say $hashed;
+
+
