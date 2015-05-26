08625582bee57d1a2aece00184abe382fc2344a5
B3h3m0th <behemoth@autistici.org>
2015-05-26 22:01:07 +0200

    replaced lastnme with nickname

--- OOP/lib/Person/main4.pl
+++ OOP/lib/Person/main4.pl
@@ -6,13 +6,14 @@ use Person4;
 
 my $person = Person4->new(
     firstname   => "mister",
-    lastname    => "behemoth",
+    nickname    => "behemoth",
     address     => "5th avenue",
     country     => "italy",
     occupation  => "Developer"
 );
 
 say 'Firstname is: '.$person->first_name();
-say 'Lastname is: '.$person->last_name();
+say 'Nickname is: '.$person->nickname();
+say 'Address is: '.$person->address();
 say 'Country is: '.$person->country();
 say 'Occupation is: '.$person->occupation();
