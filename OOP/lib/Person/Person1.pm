ed057b1e219fafa9aa82fb6b518f0bbfc5e39fd3
B3h3m0th <behemoth@autistici.org>
2015-05-26 19:54:28 +0200

    fixed source code elements

--- OOP/lib/Person/Person1.pm
+++ OOP/lib/Person/Person1.pm
@@ -5,9 +5,7 @@ use strict;
 # Basic (empty) class
 
 1;
-
 __END__
-
 - The name of the package is the same as the name of class/package
 - Perl expects a true value as the last thing in the package
 - How to start using this class? With "use Person1" in a separate file
