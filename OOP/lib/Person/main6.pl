964e1c7b392c2befb351c7dba3c44601ed6294b7
B3h3m0th <behemoth@autistici.org>
2015-05-27 18:12:43 +0200

    testing Person6 class

---
+++ OOP/lib/Person/main6.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Person6;
+
+my $person = Person6->new(
+    firstname   => "ciccio",
+    nickname    => "franchi",
+    address     => "piazza dante",
+    country     => "jamaica",
+    occupation  => "chef"
+);
+
+say 'Firstname is: '.$person->first_name();
+say 'Nickname is: '.$person->nickname();
+say 'Address is: '.$person->address();
+say 'Country is: '.$person->country();
+say 'Occupation is: '.$person->occupation();
+
+$person->first_name("mister");
+$person->nickname("behemoth");
+$person->address("47th avenue");
+$person->country("usa");
+$person->occupation("admin");
+
+say "defined Firstname to: ".$person->first_name();
+say 'defined Nickname to: '.$person->nickname();
+say 'defined Address to: '.$person->address();
+say 'defined Country to: '.$person->country();
+say 'defined Occupation to: '.$person->occupation();
