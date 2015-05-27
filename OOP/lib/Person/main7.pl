056b0eb5c878121dc75ec3a43ed1f88ae5eeb09d
B3h3m0th <behemoth@autistici.org>
2015-05-27 18:49:31 +0200

    how to invoke class attributes

---
+++ OOP/lib/Person/main7.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Person7;
+
+
+my $person1 = Person7->new();
+say 'Record counter now is: '.Person7::head_count();
+my $person2 = Person7->new();
+say 'Record counter now is: '.Person7::head_count();
+my $person3 = Person7->new();
+say 'Record counter now is: '.Person7::head_count();
+my $person4 = Person7->new();
+say 'Record counter now is: '.Person7::head_count();
+my $person5 = Person7->new();
+say 'Record counter now is: '.Person7::head_count();
