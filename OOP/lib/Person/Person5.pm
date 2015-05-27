9ffba52f6901c911c162aada0c1085e31aa9cfe6
B3h3m0th <behemoth@autistici.org>
2015-05-27 16:40:28 +0200

    new class Person5, get/set object methods

---
+++ OOP/lib/Person/Person5.pm
@@ -0,0 +1,48 @@
+package Person5;
+
+use strict;
+
+# How to provide Get/Set object methods
+
+# The constructor
+sub new {
+    my $class = shift;      # The class name, passed as the first argument
+    my $self = {@_};        # Get arguments passed to new()
+
+    bless $self, $class;
+    return $self;
+}
+
+# Get firstname 
+sub first_name {
+    my $self = shift;
+    return $self->{firstname};
+}
+
+# Get nickname
+sub nickname {
+    my $self = shift;
+    return $self->{nickname};
+}
+
+# Get address
+sub address {
+    my $self = shift;
+    return $self->{address};
+}
+
+# Get country
+sub country {
+    my $self = shift;
+    return $self->{country};
+}
+
+# Get occupation
+sub occupation {
+    my $self = shift;
+    return $self->{occupation};
+}
+
+1;
+__END__
+See main5.pl, how to call object methods
