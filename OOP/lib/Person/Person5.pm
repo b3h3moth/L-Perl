eb8e01d407e79eb250eaf6aef0a0d6835e9c5e5b
B3h3m0th <behemoth@autistici.org>
2015-05-27 16:46:55 +0200

    fixed every methos for getting/setting data

--- OOP/lib/Person/Person5.pm
+++ OOP/lib/Person/Person5.pm
@@ -16,30 +16,45 @@ sub new {
 # Get/Set firstname 
 sub first_name {
     my $self = shift;
+    my $data = shift;
+
+    $self->{firstname} = $data if defined $data;
     return $self->{firstname};
 }
 
 # Get/Set nickname
 sub nickname {
     my $self = shift;
+    my $data = shift;
+
+    $self->{nickname} = $data if defined $data;
     return $self->{nickname};
 }
 
 # Get/Set address
 sub address {
     my $self = shift;
+    my $data = shift;
+
+    $self->{address} = $data if defined $data;
     return $self->{address};
 }
 
 # Get/Set country
 sub country {
     my $self = shift;
+    my $data = shift;
+
+    $self->{country} = $data if defined $data;
     return $self->{country};
 }
 
 # Get/Set occupation
 sub occupation {
     my $self = shift;
+    my $data = shift;
+
+    $self->{occupation} = $data if defined $data;
     return $self->{occupation};
 }
 
