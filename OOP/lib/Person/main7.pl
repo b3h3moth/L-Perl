860fdccf052705b9f12f3761b9643c669625a571
B3h3m0th <behemoth@autistici.org>
2015-05-28 10:58:02 +0200

    replaced Record with Object

--- OOP/lib/Person/main7.pl
+++ OOP/lib/Person/main7.pl
@@ -4,14 +4,13 @@ use strict;
 use v5.14;
 use Person7;
 
-
 my $person1 = Person7->new();
-say 'Record counter now is: '.Person7::head_count();
+say 'Object counter now is: '.Person7::head_count();
 my $person2 = Person7->new();
-say 'Record counter now is: '.Person7::head_count();
+say 'Object counter now is: '.Person7::head_count();
 my $person3 = Person7->new();
-say 'Record counter now is: '.Person7::head_count();
+say 'Object counter now is: '.Person7::head_count();
 my $person4 = Person7->new();
-say 'Record counter now is: '.Person7::head_count();
+say 'Object counter now is: '.Person7::head_count();
 my $person5 = Person7->new();
-say 'Record counter now is: '.Person7::head_count();
+say 'Object counter now is: '.Person7::head_count();
