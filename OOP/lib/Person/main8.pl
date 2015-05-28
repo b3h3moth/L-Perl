0ccfad5b6873b5c4c4a88004080778b8035d5fa9
B3h3m0th <behemoth@autistici.org>
2015-05-28 11:02:06 +0200

    testing Person8 class, object counter

--- OOP/lib/Person/main8.pl
+++ OOP/lib/Person/main8.pl
@@ -3,3 +3,15 @@ use warnings;
 use strict;
 use v5.14;
 use Person8;
+
+say 'Object Counter now is: '.Person8->head_count();
+
+my $person = Person8->new(
+    firstname   => "mister",
+    nickname    => "behemoth",
+    address     => "77th avenue",
+    country     => "Cuba",
+    occupation  => "killerApp"
+);
+
+say 'Object Counter now is: '.Person8->head_count();
