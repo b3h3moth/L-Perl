1530181fbbd02c4f6a071635204e8d492d778fc5
B3h3m0th <behemoth@autistici.org>
2015-05-28 11:14:46 +0200

    looping into record with object reference

--- OOP/lib/Person/main8.pl
+++ OOP/lib/Person/main8.pl
@@ -6,7 +6,7 @@ use Person8;
 
 say 'Object Counter now is: '.Person8->head_count();
 
-my $person = Person8->new(
+my $object_a = Person8->new(
     firstname   => "mister",
     nickname    => "behemoth",
     address     => "77th avenue",
@@ -15,3 +15,18 @@ my $person = Person8->new(
 );
 
 say 'Object Counter now is: '.Person8->head_count();
+
+my $object_b = Person8->new(
+    firstname   => "leonardo",
+    nickname    => "leo",
+    address     => "Vinci",
+    country     => "Italy",
+    occupation  => "genius"
+);
+
+say 'Object Counter now is: '.Person8->head_count();
+
+say 'Full record:';
+foreach my $person( Person8->every_record() ) {
+    say $person->first_name().' '.$person->nickname().' '.$person->country();
+}
