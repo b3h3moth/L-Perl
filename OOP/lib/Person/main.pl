43edcc556b744579d68b6c119efd5c8732201f9e
B3h3m0th <behemoth@autistici.org>
2015-05-29 19:01:05 +0200

    added three record and print them into output

--- OOP/lib/Person/main.pl
+++ OOP/lib/Person/main.pl
@@ -4,11 +4,29 @@ use strict;
 use v5.14;
 use Person;
 
-my $person = Person->new(
+my $person1 = Person->new(
     firstname   => "luca",
-    lastname    => "suriano",
-    nickname    => "behemoth",
-    address     => "26th San Lorenzo avenue",
-    country     => "Italy",
+    lastname    => "pompelmo",
+    nickname    => "red",
+    address     => "26th avenue",
+    country     => "USA",
     occupation  => "*nix system programmer"
 );
+
+my $person2 = Person->new(
+    firstname   => "daniela",
+    lastname    => "pompinius",
+    nickname    => "topa",
+    address     => "tangenziale",
+    country     => "Italy",
+    occupation  => "bitch"
+);
+my $person3 = Person->new(
+    firstname   => "francesca",
+    lastname    => "banana",
+    nickname    => "stanga",
+    address     => "18th avenue",
+    country     => "France",
+    occupation  => "model"
+);
+$person3->print_every_record();
