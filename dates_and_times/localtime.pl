4dc057e793bf2d1772a8d6858e3b6576c3515d00
B3h3m0th <behemoth@autistici.org>
2015-05-29 19:25:01 +0200

    testing localtime()

--- dates_and_times/localtime.pl
+++ dates_and_times/localtime.pl
@@ -17,3 +17,12 @@ use v5.14;
 my ($day, $month, $year) = (localtime)[3,4,5];
 say "just localtime(): $year $month $day";
 printf("The current date is: %04d/%02d/%02d\n", $year+1900, $month+1, $day);
+
+my @date = (localtime)[3,4,5];
+say "date[0]: ", $date[0];
+say "date[1]: ", $date[1];
+say "date[2]: ", $date[2];
+$date[1]++;
+$date[2] += 1900;
+my $date = join "/", @date;
+say $date;
