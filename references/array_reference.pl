fcc8d4c83f2efcc94dbf0f1013b0e26102bcbdf0
B3h3m0th <behemoth@autistici.org>
2015-05-31 00:28:38 +0200

    reference to the array

--- references/array_reference.pl
+++ references/array_reference.pl
@@ -4,8 +4,9 @@ use strict;
 use v5.14;
 
 my @colors = qw(blue red white black);
-my $refcolor = \@colors;
+# reference to the array
+my $aref = \@colors;
 
-say $refcolor;
-my @copy_of_colors = @$refcolor;
-say @copy_of_colors;
+my $first_color = $aref->[0];
+say $first_color;
+
