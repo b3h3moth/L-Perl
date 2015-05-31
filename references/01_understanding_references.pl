b6121e2dc155543327c55dadae858ba569f763ca
B3h3m0th <behemoth@autistici.org>
2015-05-31 18:09:41 +0200

    subroutine reference, CODE reference

--- references/01_understanding_references.pl
+++ references/01_understanding_references.pl
@@ -13,7 +13,7 @@ sub numbers {
 }
 
 # The simplest way to create a reference is with the backslash operator '\'. It
-# works on any kind of variable name and hash/array elements
+# works on any kind of variable name and hash/array elements.
 my $scalar_ref     = \$year;
 my $array_ref      = \@colors;
 my $hash_ref       = \%country;
@@ -21,15 +21,18 @@ my $glob_ref       = \*STDOUT;
 my $array_elem_ref = \$colors[0];
 my $hash_elem_ref  = \$country{'print_year'};
 
-# References to literal values are read-only
+# References to literal values are read-only:
 my $decimal_ref  = \10;
 my $hex_ref      = \hex '0xF';
 
-# References to subroutines
-my $sub_ref        = \&print_year;
+# References to subroutines.
+# With '&' we get back a CODE reference - CODE(0x102bbbd8) - to the print_year
+# subroutine:
+my $sub_ref        = \(&print_year);
+say $sub_ref;
 
 
 
 # A reference on a list of values returns a list of references rather than a 
-# reference to a list
+# reference to a list:
 my $ref_list = \(1 .. 4);
