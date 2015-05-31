7066ab8ed86e58bf99da58f46780aa17342b6dd0
B3h3m0th <behemoth@autistici.org>
2015-05-31 17:23:19 +0200

    added a lot of perl references

--- references/01_understanding_references.pl
+++ references/01_understanding_references.pl
@@ -2,3 +2,20 @@
 use warnings;
 use strict;
 use v5.14;
+
+# A reference is always a scalar value.
+
+my $year = 2015;
+my @colors = qw(red white blue);
+my %country = (Italy => "Rome", USA => "Washington", France => "Paris");
+sub hello { say 'hello' }
+
+# The simplest way to create a reference is with the backslash operator '\'. It
+# works on any kind of variable name and hash/array elements
+my $scalar_ref     = \$year;
+my $array_ref      = \@colors;
+my $hash_ref       = \%country;
+my $sub_ref        = \&hello;
+my $glob_ref       = \*STDOUT;
+my $array_elem_ref = \$colors[0];
+my $hash_elem_ref  = \$country{'hello'};
