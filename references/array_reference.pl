ddb1fe54f5948f4f7b905752ec774273ed6150ee
B3h3m0th <behemoth@autistici.org>
2015-05-31 13:10:03 +0200

    fixed comment about dereferencing

--- references/array_reference.pl
+++ references/array_reference.pl
@@ -8,12 +8,13 @@ my @colors = qw(blue red white black);
 # To assign a reference to an array to a scalar, prepend it with a backslash
 my $aref = \@colors;
 
-# dereferencing
+# dereferencing with '->'
 my $first_color = $aref->[0];
 say 'First value is: ', $first_color;
 
 # copy values to another array by prepending it with the @ sign
 my @copy_of_colors = @$aref;
+
 foreach (@copy_of_colors) {
     say $_;
 }
