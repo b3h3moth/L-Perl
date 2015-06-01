51de08cae31616f74847ce9d07e136a99ce3ce31
B3h3m0th <behemoth@autistici.org>
2015-06-01 18:01:49 +0200

    dereferencing array

--- references/02_using_reference.pl
+++ references/02_using_reference.pl
@@ -4,11 +4,17 @@ use strict;
 use v5.14;
 
 # Dereferencing uses the value that a reference points to, there are several 
-# different forms.
+# different forms. The basic form is to use a block.
 
-# The basic form is to use a block:
+# Dereferencing ordinary scalar:
 my $year = 2015;
 my $scalar_ref = \$year;
 say ${ $scalar_ref };
 my $next_year = ${ $scalar_ref } + 1;
 say $next_year;
+
+# Dereferencing array:
+my @colors = qw(red white blue);
+my $array_ref = \@colors;
+say @{ $array_ref };        # all list
+say ${ $array_ref }[0];     # single element
