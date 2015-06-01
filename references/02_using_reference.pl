95cc0541b798a32e25f80f8cbedf872c20d4a3cc
B3h3m0th <behemoth@autistici.org>
2015-06-01 18:58:42 +0200

    code inside block

--- references/02_using_reference.pl
+++ references/02_using_reference.pl
@@ -9,9 +9,9 @@ use v5.14;
 # Dereferencing ordinary scalar:
 my $year = 2015;
 my $scalar_ref = \$year;
-say ${ $scalar_ref };
+say 'A: reference to scalar: ', ${ $scalar_ref };
 my $next_year = ${ $scalar_ref } + 1;
-say $next_year;
+say 'B: next_year = ', $next_year;
 
 # Dereferencing array:
 my @colors = qw(red white blue);
@@ -19,3 +19,18 @@ my $array_ref = \@colors;
 say @{ $array_ref };        # all list values
 say ${ $array_ref }[0];     # single element
 push @{ $array_ref }, qw(black green gray); # add elements to the list
+
+my $ref_anon_array_1 = [ 1 .. 5 ];
+my $ref_anon_array_2 = [ 6 .. 10 ];
+
+# The code inside the block can be complex:
+my $val = ${
+    my $i = shift $ref_anon_array_1;
+    if ($i) {
+        $ref_anon_array_1
+    } else {
+        $ref_anon_array_2
+    }
+}[0];
+say $val;
+say join ',',@{$ref_anon_array_1};
