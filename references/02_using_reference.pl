00acd66f14dda8da8d9e9e3b17ba4745b8a8bd82
B3h3m0th <behemoth@autistici.org>
2015-06-01 19:46:28 +0200

    get single hash value, canonical form

--- references/02_using_reference.pl
+++ references/02_using_reference.pl
@@ -52,4 +52,5 @@ my $hash_ref = { italy => 'rome', france => 'paris', spain => 'madrid' };
 say 'C: hash keys: ', join ', ', keys %{$hash_ref};
 # As well as scalars we can omitted the curly braces:
 say 'C: hash values: ', join ', ', values %{$hash_ref};
-
+# How to get single value (Canonical form);
+say 'C: single hash value: ', ${$hash_ref}{'italy'};
