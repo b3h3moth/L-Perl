90908c5dee08132ba035a296a29e13d8a3590314
B3h3m0th <behemoth@autistici.org>
2015-06-02 19:44:40 +0200

    syntax and test

--- references/03_autovivification.pl
+++ references/03_autovivification.pl
@@ -2,3 +2,15 @@
 use warnings;
 use strict;
 use v5.14;
+
+# Autovivifation
+# If you use a scalar with an undefined value as if it were a reference to
+# another object, Perl automatically creates an object of the appropriate type
+# and makes that scalar a reference to that type. This is called 
+# autovivification. 
+#
+# The following code creates an array of four elements and makes $ref a 
+# reference to it:
+undef $ref;
+$ref->[3] = 'four';
+
