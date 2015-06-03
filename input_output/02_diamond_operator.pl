3d69710f4e3523f73640d47a1c0a443eff1dd97c
B3h3m0th <behemoth@autistici.org>
2015-06-03 22:34:59 +0200

    Info about feautures of <>

--- input_output/02_diamond_operator.pl
+++ input_output/02_diamond_operator.pl
@@ -4,4 +4,6 @@ use strict;
 use v5.14;
 
 # Another way to read input is with the diamond operator <>.
+# Larry Wall put this feature into Perl because he wanted to make it easy for 
+# you to write your own programs that work like standard Unix utilities.
 
