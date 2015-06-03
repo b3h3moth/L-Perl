5afd2b6cdca29ddcf458263672e9a3c75fa22f75
B3h3m0th <behemoth@autistici.org>
2015-06-03 22:46:51 +0200

    improved comment indentation

--- input_output/06_writing_file.pl
+++ input_output/06_writing_file.pl
@@ -2,20 +2,20 @@
 use warnings;
 use strict;
 
-#Read file, reverse each line and then print output in a new file.
+# Read file, reverse each line and then print output in a new file.
 
 die "Usage: ./prg.pl <input_filename>\n" if (@ARGV != 1);
 
 my ($input_file) = @ARGV;
 my $output_file = "reverse_$input_file";
 
-#reading
+# Read file
 open(my $in, "< $input_file") or die "Err. reading $input_file. $!";
-#writing
+
+# Write into file
 open(my $out, "> $output_file") or die "Err. reading $output_file. $!";
 
-#always check that files have been opened correctly
-
+# Always check if that files have been opened correctly
 while (<$in>) {
     chomp;
     my $reverse = reverse $_;
