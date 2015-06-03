197581f2431a43f7294aab13f47d34e3f6e702c7
B3h3m0th <behemoth@autistici.org>
2015-06-03 22:37:38 +0200

    <> returns ...

--- input_output/02_diamond_operator.pl
+++ input_output/02_diamond_operator.pl
@@ -6,4 +6,5 @@ use v5.14;
 # Another way to read input is with the diamond operator <>.
 # Larry Wall put this feature into Perl because he wanted to make it easy for 
 # you to write your own programs that work like standard Unix utilities.
-
+# The diamond operator <> will return undef only at the end of all of the 
+# input.
