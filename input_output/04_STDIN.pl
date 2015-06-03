2f6ed7879f038e5dcf49d0251d493db85b2e01f6
B3h3m0th <behemoth@autistici.org>
2015-06-03 22:24:57 +0200

    removed file

--- input_output/04_STDIN.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-use warnings;
-use strict;
-
-print "Which is the best language? ";
-
-#<STDIN> filehandle can be abbreviated to <>
-my $lang = <STDIN>;
-print "The best language is $lang\n";
