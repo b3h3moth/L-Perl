4b6b041e39d3e80ba61e236dcae88f48ff944f68
B3h3m0th <behemoth@autistici.org>
2015-06-04 20:24:04 +0200

    write into FH+

--- input_output/03_filehandle_2.pl
+++ input_output/03_filehandle_2.pl
@@ -2,3 +2,15 @@
 use warnings;
 use strict;
 use v5.14;
+
+die "Usage: $0 <filename>\n" if (@ARGV != 1);
+
+my ($filename) = @ARGV;
+
+if ( ! open(PASS, '>>', "$filename")) {
+    die "Cannot open $filename: $!\n";
+}
+
+print PASS "test\n";
+
+close(PASS);
