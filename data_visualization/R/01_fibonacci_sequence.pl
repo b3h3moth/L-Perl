2f97debe2d2234642d82fc7bbd12d1c1c59b3ba6
B3h3m0th <behemoth@autistici.org>
2015-06-04 18:03:43 +0200

    how to create a png from R

--- data_visualization/R/01_fibonacci_sequence.pl
+++ data_visualization/R/01_fibonacci_sequence.pl
@@ -6,8 +6,8 @@ use Statistics::R;
 
 my $object = Statistics::R->new();
 $object->startR;
-$object->send('data <- c(1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987)');
+$object->send('sequence <- c(1,1,2,3,5,8,13,21,34,55,89,144,233,377,610)');
 $object->send('png("image.png", width=500, height=500)');
-$object->send('barplot(data)');
+$object->send('barplot(sequence, main="Fibonacci Series")');
 $object->stopR;
 
