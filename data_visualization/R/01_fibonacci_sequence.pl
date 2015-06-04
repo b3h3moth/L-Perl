aaf75c94b3d5973ad871581f2bb4bde456f71a94
B3h3m0th <behemoth@autistici.org>
2015-06-04 18:09:44 +0200

    added final output

--- data_visualization/R/01_fibonacci_sequence.pl
+++ data_visualization/R/01_fibonacci_sequence.pl
@@ -7,7 +7,8 @@ use Statistics::R;
 my $object = Statistics::R->new();
 $object->startR;
 $object->send('sequence <- c(1,1,2,3,5,8,13,21,34,55,89,144,233,377,610)');
-$object->send('png("image.png", width=500, height=500)');
+$object->send('png("fibo.png", width=500, height=500)');
 $object->send('barplot(sequence, main="Fibonacci Series")');
 $object->stopR;
 
+sys 'fibo.png created';
