0694a637ec808aecf6efea65bebed91cb9c8cde7
B3h3m0th <behemoth@autistici.org>
2015-06-04 16:49:01 +0200

    start using R, fibonacci series

--- data_visualization/R/barplot.pl
+++ data_visualization/R/barplot.pl
@@ -3,3 +3,11 @@ use warnings;
 use strict;
 use v5.14;
 use Statistics::R;
+
+my $object = Statistics::R->new();
+$object->startR;
+$object->send('data <- c(1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987)');
+$object->send('png("image.png", width=500, height=500)');
+$object->send('barplot(data)');
+$object->stopR;
+
