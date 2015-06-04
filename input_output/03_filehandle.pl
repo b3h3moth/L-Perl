6f38755523a4b990e512b4240d91c93f12b77f60
B3h3m0th <behemoth@autistici.org>
2015-06-04 11:39:18 +0200

    improved open() function on filehanldes

--- input_output/03_filehandle.pl
+++ input_output/03_filehandle.pl
@@ -4,35 +4,35 @@ use strict;
 
 # Filehandle is the name for an Input/Output connection between perl process
 # and the outside world. Larry Wall (Perl's creator) recomends to use all
-# uppercase letters in the name of filehandle. 
+# uppercase letters in the name of filehandle.
+#
 # Note that there are six special filehandle names in Perl, they are:
 # STDIN, STDOUT, STDERR, DATA, ARGV, ARGVOUT.
-
+# 
 # STDIN, STOUT, STDERR are automatically opened.
-
+# 
 # To open a new filehanlde we can use open().
 # open a filehandle called INPUT to a file called 'file':
-# open(INPUT,  'file');
-# 
+#   open(INPUT,  'file');
 # open a filename called 'file' as input to a filehandle called CONFIG:
-# open(CONFIG, '<file');
-#
+#   open(CONFIG, '<file');
 # open a filehandle called 'MAIL' for output to the new file called 'file':
-# open(MAIL,   '>file);
-#
+#   open(MAIL,   '>file);
 # open a filehandle called 'LOG' for appending to the file called 'file':
-# open(LOG,    '>>file);
+#   open(LOG,    '>>file);
+#
+# From Perl v5.6 we can use three arguments:
+# open(CONFIG, '<',  'file');
+# open(MAIL,   '>',  'file');
+# open(LOG,    '>>', 'file');
+
 
 die "Usage: $0 <filename>\n" if (@ARGV != 1);
 
 #get file from command line
 my ($filename) = @ARGV;
 
-#open the file for reading.
-#INPUT is the name of filehandle
-#< is the mode, in this case reading from the file
-#$filename is the filename.
-open(INPUT, "< $filename");
+open(INPUT, '<', "$filename");
 
 #read the file from filehandle and store in $line
 while (my $line = <INPUT>) {
