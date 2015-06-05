7bd1119f6825301f50341efcf09535829b26c7d7
B3h3m0th <behemoth@autistici.org>
2015-06-05 17:00:57 +0200

    improved and fixed select() test, both into FH to into file

--- input_output/04_select.pl
+++ input_output/04_select.pl
@@ -4,18 +4,26 @@ use strict;
 use v5.14;
 
 # select
-# Returns the currently selected filehandle. By default, if you don't give a 
+# Returns the currently selected filehandle. By default, if you don't give a
 # filehandle to print the output will go to STDOUT.
 
 die "Usage: $0 <filename>\n" if (@ARGV != 1);
 
 my ($filename) = @ARGV;
 
-if ( ! open(PASS, '>', "$filename")) {
+if ( ! open(FH, '>', "$filename")) {
     die "Cannot open $filename: $!\n";
 }
 
-select PASS;
-print "Using select() to write with filehandle\n";
+# print into STDOUT
+select STDOUT;
+print "Using select() to write into STDOUT\n";
+say "but I prefer to write into filehandle, How can I do? hey hey ...";
 
-close(PASS);
+# now print into filehandle FH
+select FH;
+
+print "cool, now $filename is not empty\n";
+say "yes but it's not so interesting";
+
+close(FH);
