6016d5453b98c1a89773a57b0e3ab570c3466f75
B3h3m0th <behemoth@autistici.org>
2015-06-05 19:49:58 +0200

    improved regexp

--- input_output/05_filehandle_scalar.pl
+++ input_output/05_filehandle_scalar.pl
@@ -7,7 +7,8 @@ use strict;
 die "Usage: ./prg.pl <input_filename>\n" if (@ARGV != 1);
 
 my ($input_file) = @ARGV;
-my $output_file = "reverse_$input_file";
+my $output_file;
+($output_file = $input_file) =~ s/.+\/$/reverse_/;
 
 # Read file
 # set scalar filehandle (input):
@@ -15,7 +16,7 @@ open(my $in, "< $input_file") or die "Err. reading $input_file. $!";
 
 # Write into file
 # set scalar filehandle (output):
-open(my $out, "> $output_file") or die "Err. reading $output_file. $!";
+open(my $out, "> $output_file") or die "Err. writing $output_file. $!";
 
 # Always check if that files have been opened correctly:
 while (<$in>) {
