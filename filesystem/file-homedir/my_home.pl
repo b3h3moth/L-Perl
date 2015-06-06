8f4c506637a82fa810f172ee816c52654fa42d18
B3h3m0th <behemoth@autistici.org>
2015-06-06 18:34:25 +0200

    my_home() test

--- filesystem/file-homedir/my_home.pl
+++ filesystem/file-homedir/my_home.pl
@@ -2,6 +2,9 @@
 use warnings;
 use strict;
 use v5.14;
+use File::HomeDir;
 
 # The my_home() method takes no arguments and returns the main home-profile
 # directory for the current user.
+my $home = File::HomeDir->my_home();
+say 'My home is: ', $home;
