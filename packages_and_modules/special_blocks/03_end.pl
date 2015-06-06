2c721f41e5f94e8b025dd70ea68083bd60028b2d
B3h3m0th <behemoth@autistici.org>
2015-06-06 18:04:09 +0200

    END file

---
+++ packages_and_modules/special_blocks/03_end.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+# There are five special blocks executed at the beginning and at the end of a
+# running program; these named code blocks are: BEGIN; CHECK; END; INIT; 
+# UNITCHECK.
+
+# BEGIN
