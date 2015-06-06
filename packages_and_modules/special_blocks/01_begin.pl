635edc3d6a146d045c54460596fc9e53581b46be
B3h3m0th <behemoth@autistici.org>
2015-06-06 18:01:48 +0200

    comment about special code blocks

--- packages_and_modules/special_blocks/01_begin.pl
+++ packages_and_modules/special_blocks/01_begin.pl
@@ -2,3 +2,9 @@
 use warnings;
 use strict;
 use v5.14;
+
+# There are five special blocks executed at the beginning and at the end of a
+# running program; these named code blocks are: BEGIN; CHECK; END; INIT; 
+# UNITCHECK.
+
+# BEGIN
