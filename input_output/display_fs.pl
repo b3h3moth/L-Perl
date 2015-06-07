e9c777556be3ed8bea737e1150d08f9a03b09aec
B3h3m0th <behemoth@autistici.org>
2015-06-07 19:47:17 +0200

    display fs, full program

--- input_output/display_fs.pl
+++ input_output/display_fs.pl
@@ -3,28 +3,31 @@ use warnings;
 use strict;
 use v5.14;
 
-
-$varpath="/export/home/users/erikk/work/baselines/var";
+my $varpath='/home/behemoth';
 
 # get output of df -k
 open(MY_DF, " df -k |") or die "$0: couldn't get df: $!";
+
 while(<MY_DF>)
 {
-        chop;
-        if(/^\/dev\//)
-        {
-                @myrec = split(" ", $_);
-                $free = int($myrec[3]/1024);
-                #print "$myrec[5]: $myrec[4] $free MB free\n";
-                $fsname = $myrec[5];
-              $fsname =~ s/\//_/g;
-                            $logname = "df_".$fsname.".log";
-                                          open(MY_LOG, ">>$varpath/$logname")
-                                                                or die "$0: problem opening file: $!";
-                                                                          print MY_LOG "$free\n";
-                                                                                        close(MY_LOG);
-                                                                                                }
-                                                                                            }
-                                                                                            close(MY_DF);
-                                                                                            # EOF
-                                                                                            #
+    chomp;
+    # /dev/
+    if(/^\/dev\//) {
+        my @myrec = split(" ", $_);
+        my $free = int($myrec[3]/1024);
+        
+        #print "$myrec[5]: $myrec[4] $free MB free\n";
+        my $fsname = $myrec[5];
+        $fsname =~ s/\//_/g;
+        
+        my $logname = "df_".$fsname.".log";
+        open(MY_LOG, ">>$varpath/$logname") 
+            or die "$0: problem opening file: $!";
+        
+        print MY_LOG "$free\n";
+
+        close(MY_LOG);
+    }
+}
+
+close(MY_DF);
