aa018a3a827c44ac9f49e8946dabe046190e8ccc
B3h3m0th <behemoth@autistici.org>
2015-06-07 17:32:43 +0200

    CHECK syntax

--- packages_and_modules/special_blocks/02_check.pl
+++ packages_and_modules/special_blocks/02_check.pl
@@ -7,4 +7,10 @@ use v5.14;
 # running program; these named code blocks are: BEGIN; CHECK; END; INIT; 
 # UNITCHECK.
 
+# UNITCHECK, CHECK and INIT code blocks are useful to catch the transition 
+# between the compilation phase and the execution phase of the main program.
+
 # CHECK
+# CHECK code blocks are run just after the initial Perl compile phase ends and
+# before the run time begins, in LIFO order. CHECK code blocks are used in the
+# Perl compiler suite to save the compiled state of the program.
