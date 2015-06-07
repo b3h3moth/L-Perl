eb3bc0df7d16375498de7d8c93b69f1b9bc63066
B3h3m0th <behemoth@autistici.org>
2015-06-07 20:20:43 +0200

    few improvements to have just one logfile

--- input_output/display_fs.pl
+++ input_output/display_fs.pl
@@ -4,6 +4,7 @@ use strict;
 use v5.14;
 
 my $varpath='/home/behemoth';
+my $logname = 'df.log';
 
 # get output of df -k
 open(MY_DF, " df -k |") or die "$0: couldn't get df: $!";
@@ -18,16 +19,19 @@ while(<MY_DF>)
         # save directory fields
         my $fsname = $myrec[5];
         # replaced / with _
-        $fsname =~ s/\//_/g;
+        #$fsname =~ s/\//_/g;
 
         # Create logfile
-        my $logname = 'df'.$fsname.'.log';
-        open(MY_LOG, '>', "$varpath/$logname") or die "Err. open $logname:$0";
-        print MY_LOG "$free MB\n";
+        #my $logname = 'df'.$fsname.'.log';
+        open(MY_LOG, '>>', "$varpath/$logname") 
+            or die "Can't open open $logname: $!";
+        
+        say MY_LOG "$fsname $free MB";
         close(MY_LOG);
 
-        say "$logname saved: $varpath/$logname";
     }
 }
 
 close(MY_DF);
+        
+say "$logname saved: $varpath/$logname";
