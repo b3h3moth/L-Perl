fcab6f9cd5f5662a35aff2c340c9c477d50b221e
B3h3m0th <behemoth@autistici.org>
2015-06-07 17:36:01 +0200

    INIT syntax

--- packages_and_modules/special_blocks/04_init.pl
+++ packages_and_modules/special_blocks/04_init.pl
@@ -7,4 +7,9 @@ use v5.14;
 # running program; these named code blocks are: BEGIN; CHECK; END; INIT; 
 # UNITCHECK.
 
+# UNITCHECK, CHECK and INIT code blocks are useful to catch the transition
+# between the compilation phase and the execution phase of the main program.
+
 # INIT
+# INIT blocks are run just before the Perl runtime begins execution, in FIFO 
+# order.
