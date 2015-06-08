4f49a3dc90da8af29e1ba542e8f685ded614e23d
B3h3m0th <behemoth@autistici.org>
2015-06-08 12:15:48 +0200

    replaced filehandles, added string to send to output

--- input_output/07_filehandle_pipe.pl
+++ input_output/07_filehandle_pipe.pl
@@ -2,3 +2,18 @@
 use warnings;
 use strict;
 use v5.14;
+
+my $file = "$$.txt";
+
+# The filename is interpreted as a command that pipes output.
+open(LISTDIR, 'ls -l |') or die "Couldn't get pipes output: $!\n";
+open(FILE, '>', $file) or die "Can't open $file: $!\n";
+
+while (<LISTDIR>) {
+    print FILE $_;
+}
+
+close(LISTDIR);
+close(FILE);
+
+say "FILE: $file";
