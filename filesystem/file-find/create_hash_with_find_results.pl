23f85cce904a9e1d6cf8b30055369225b5891793
B3h3m0th <behemoth@autistici.org>
2015-06-10 13:37:45 +0200

    improved, the purpose of the program

--- filesystem/file-find/create_hash_with_find_results.pl
+++ filesystem/file-find/create_hash_with_find_results.pl
@@ -5,14 +5,14 @@ use v5.14;
 use Data::Dumper;
 use File::Find;
 
-# How to create an hash whose keys is the file size and whose values are 
-# their names.
+# How to create an hash whose key is the file size and whose value is an array
+# containing their filenames.
 
 my %hash;
 
 my $dir = '/var/www';
 
-# Find all hash inside $dir directory and push into hash
+# Find all hash inside $dir directory and push into hash, whose key 
 find( sub {
         -f &&
         push @{$hash{(stat(_))[7]}}, $File::Find::name
