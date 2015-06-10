8a09ace66ce8284a554c9a543ecb424b9490d7b3
B3h3m0th <behemoth@autistici.org>
2015-06-10 13:20:29 +0200

    How to create an hash with filesize and filenames(array)

---
+++ filesystem/file-find/create_hash_with_find_results.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Data::Dumper;
+use File::Find;
+
+# How to create an hash whose keys is the file size and whose values are 
+# their names.
+
+my %files;
+
+my @dir_list = @ARGV;
+
+find( sub {
+        push @{$files{(stat(_))[7]}}, $File::Find::name
+    }, @dir_list
+);
+
+say Dumper \%files;
