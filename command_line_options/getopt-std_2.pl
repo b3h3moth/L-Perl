9208d726ddbe3d081051d8a04da44bbd00c7bae4
B3h3m0th <behemoth@autistici.org>
2015-06-11 17:40:07 +0200

    fixed other things with @ARGV

--- command_line_options/getopt-std_2.pl
+++ command_line_options/getopt-std_2.pl
@@ -16,9 +16,8 @@ print "-l $options{l}\n" if defined $options{l};
 print "-m $options{m}\n" if defined $options{m};
 print "-n $options{n}\n" if defined $options{n};
  
-# other things found on the command line
 print "Other things found on the command line:\n" if $ARGV[0];
-foreach (@ARGV)
-{
-  print "$_\n";
+
+foreach (@ARGV) {
+    say $_;
 }
