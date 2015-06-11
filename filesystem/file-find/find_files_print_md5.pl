5796bb7d89e66c3991f7f9da189a35ffc7dee9d0
B3h3m0th <behemoth@autistici.org>
2015-06-11 19:07:52 +0200

    check md5 every file, find()

---
+++ filesystem/file-find/find_files_print_md5.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Data::Dumper;
+use File::Find;
+use Digest::MD5;
+
+# How to create an hash whose keys is the file size and whose values are 
+# their names.
+
+my %hash;
+
+my @dir_list = (@ARGV);
+
+find( sub {
+        push @{$hash{(stat(_))[7]}}, $File::Find::name if -f;
+    }, @dir_list
+);
+
+foreach my $size (keys %hash) {
+    if ( $#{$hash{$size}} < 1 ) {
+        next;
+    }
+
+    my %md5;
+
+    #print "$size: ";
+
+    foreach my $cur_file (@{$hash{$size}}) {
+        #print "$cur_file, ";
+
+        open(FILE, $cur_file) or next;
+        binmode(FILE);
+
+        # Add elements into md5 hash:
+        # key:  md5 checksum
+        # value: filename
+        push @{$md5{Digest::MD5->new->addfile(*FILE)->hexdigest()}}, $cur_file;
+        close(FILE);
+
+
+    }
+    #print "\n";
+   
+   foreach my $msize (keys %md5) {
+       print "$msize:";
+       
+       foreach my $mfile (@{$md5{$msize}}) {
+           print $mfile;
+       }
+       print "\n";
+   }
+
+}
