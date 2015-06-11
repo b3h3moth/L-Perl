b3e67b03ed3f0ab36dc9441c1a774f8b86e773e6
B3h3m0th <behemoth@autistici.org>
2015-06-11 17:12:40 +0200

    print each hash value of array reference

---
+++ hash/print_hash_anon_array.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+my %hash = (
+    12 => ['maria', 'franca', 'luigia'],
+    43 => ['pina', 'maria', 'claudia', 'francesca', 'rossella', 'mara'],
+    67 => ['giada'],
+    28 => ['gina'],
+    98 => ['marta'],
+    89 => ['rossana'],
+    10 => undef
+);
+
+my $len_arrayref = scalar( @{$hash{43}} );
+my $total_hash_keys = keys %hash;
+my $total_hash_values = values %hash;
+
+foreach my $size (keys %hash) {
+    if ( $#{$hash{$size}} < 1 ) {
+        next;
+    }
+
+    print "$size: ";
+
+    foreach my $cur_file (@{$hash{$size}}) {
+        print "$cur_file, ";
+    }
+    print "\n";
+}
