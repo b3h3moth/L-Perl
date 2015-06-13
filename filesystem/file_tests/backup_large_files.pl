d90d4dfad0fd6446a336c577206ad21adbc868ba
B3h3m0th <behemoth@autistici.org>
2015-06-13 20:25:24 +0200

    skip all directories

--- filesystem/file_tests/backup_large_files.pl
+++ filesystem/file_tests/backup_large_files.pl
@@ -14,13 +14,13 @@ opendir(DH, $dir) or die "Can't open $dir: $!\n";
 my @config_files = ();
 
 while(my $file = readdir DH) {
-    # skip current and parent directory
-    if ($file eq '.' or $file eq '..') {
+    # skip all directories
+    if ($file eq '.' or $file eq '..' or -d "$dir/$file") {
         next;
     }
 
     # skip if file is a directory
-    next if (-d "$dir/$file");
+    # next if (-d "$dir/$file");
 
     # Push each config file (beginning with a period) in the array
     if ("$file" =~ /^\./) {
