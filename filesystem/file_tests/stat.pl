1603d3c6cabea1892131dc3a99c165f75b4a26a4
B3h3m0th <behemoth@autistici.org>
2015-06-14 15:47:14 +0200

    print out each stat() value

--- filesystem/file_tests/stat.pl
+++ filesystem/file_tests/stat.pl
@@ -8,3 +8,17 @@ die "$filename doesn't exist\n" if (! -e $filename);
 
 my($dev, $ino, $mode, $nlink, $uid, $gid, $rdev,
    $size, $atime, $mtime, $ctime, $blksize, $blocks) = stat($filename);
+
+say "    dev: $dev";
+say "    ino: $ino";
+say "   mode: $mode";
+say "  nlink: $nlink";
+say "    uid: $uid";
+say "    gid: $gid";
+say "    dev: $rdev";
+say "   size: $size";
+say "  atime: $atime";
+say "  mtime: $mtime";
+say "  ctime: $ctime";
+say "blksize: $blksize";
+say " blocks: $blocks";
