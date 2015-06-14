0f35b61c4f37f3358b7b413edcfde214c588987f
B3h3m0th <behemoth@autistici.org>
2015-06-14 15:43:09 +0200

    stat() on filename

--- filesystem/file_tests/stat.pl
+++ filesystem/file_tests/stat.pl
@@ -5,3 +5,6 @@ use v5.14;
 
 my $filename = '/home/behemoth/.profile';
 die "$filename doesn't exist\n" if (! -e $filename);
+
+my($dev, $ino, $mode, $nlink, $uid, $gid, $rdev,
+   $size, $atime, $mtime, $ctime, $blksize, $blocks) = stat($filename);
