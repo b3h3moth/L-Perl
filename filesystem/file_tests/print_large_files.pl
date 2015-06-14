609984b88c11be222e8fd33b4662dce37f4f1fd6
B3h3m0th <behemoth@autistici.org>
2015-06-14 13:26:07 +0200

    print files larger than 100KB

--- filesystem/file_tests/print_large_files.pl
+++ filesystem/file_tests/print_large_files.pl
@@ -2,5 +2,24 @@
 use warnings;
 use strict;
 use v5.14;
+use File::HomeDir;
 
-# Find files in Home directory an print only files larger than 1 MB
+# Find files in Home directory an print only files larger than 100 KB
+
+my $dir = shift // File::HomeDir->my_home();
+
+opendir(DIR, $dir) or die "Can't open $dir: $!\n";
+
+my @result = ();
+
+while(my $file = readdir DIR) {
+    if ($file eq '.' or $file eq '..' or -d "$dir/$file") {
+        next;
+    }
+
+    if (-s "$dir/$file" > 102400) {
+        say $file;
+    }
+}
+
+closedir(DIR);
