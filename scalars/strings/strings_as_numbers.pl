28a291dec0860974cb25a6936a8a3facd5ac2e03
B3h3m0th <behemoth@autistici.org>
2015-06-14 19:17:15 +0200

    strings used as numbers

---
+++ scalars/strings/strings_as_numbers.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+
+# Strings used as numbers
+my $result = '12' / 3;
+say $result;
+# Compare string length
+my $str1 = 'string1';
+my $str2 = 'str2';
+if ($str1 gt $str2) {
+    say 'ok';
+}
