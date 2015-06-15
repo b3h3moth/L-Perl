d63ed434600e1cde8117b04012a6095210c799df
B3h3m0th <behemoth@autistici.org>
2015-06-15 20:02:34 +0200

    File::Path info module

--- filesystem/file-path/rmtree.pl
+++ filesystem/file-path/rmtree.pl
@@ -2,3 +2,7 @@
 use warnings;
 use strict;
 use v5.14;
+
+# File::Path
+# This module provide a convenient way to create directories of arbitrary depth
+# and to delete an entire directory subtree from the filesystem.
