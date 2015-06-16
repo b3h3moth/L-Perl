cca3487d7a5441ed46659833d3d161dee27a332a
B3h3m0th <behemoth@autistici.org>
2015-06-16 21:22:45 +0200

    use utf8 added

--- scalars/unicode/utf.pl
+++ scalars/unicode/utf.pl
@@ -7,3 +7,7 @@ my $string = 'ŧ¶';
 my $length = length($string);
 print "$string has $length characters\n";
 
+use utf8;
+my $ustring = 'ŧ¶';
+my $ulength = length($ustring);
+print "$ustring has $ulength characters\n";
