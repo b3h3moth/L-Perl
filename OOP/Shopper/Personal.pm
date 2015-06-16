d06616d6d2fbd3d5f25dd279262c64ab47c3b87f
B3h3m0th <behemoth@autistici.org>
2015-06-16 22:39:58 +0200

    new() constructor class Shopper::Personal added

--- OOP/Shopper/Personal.pm
+++ OOP/Shopper/Personal.pm
@@ -3,4 +3,13 @@ package Shopper::Personal;
 use strict;
 use warnings;
 
+sub new {
+    my ($class, $args) = @_;
+
+    return bless {
+        firstname => $args->{firstname},
+        lastname => $args->{lastname},
+    }, $class;
+}
+
 1;
