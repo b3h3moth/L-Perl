7364fae94a228a51d2bd9e39d07bf66b6cb1aa70
B3h3m0th <behemoth@autistici.org>
2015-06-16 22:43:05 +0200

    new() info

--- OOP/Shopper/Personal.pm
+++ OOP/Shopper/Personal.pm
@@ -3,6 +3,9 @@ package Shopper::Personal;
 use strict;
 use warnings;
 
+# For some other OO languages, new() is a keyword used to construct objects. In
+# Perl the new() method is just another method.
+
 sub new {
     my ($class, $args) = @_;
 
