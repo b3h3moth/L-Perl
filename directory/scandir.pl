1611cc8c1003df0cb8f3a477670561510fc435e0
B3h3m0th <behemoth@autistici.org>
2015-06-17 22:35:41 +0200

    the program's purpose

--- directory/scandir.pl
+++ directory/scandir.pl
@@ -2,3 +2,7 @@
 use warnings;
 use strict;
 use v5.14;
+
+# The program starts with the requested directory to scan. When the scan is
+# complete it returns to the directory from which it was called.
+
