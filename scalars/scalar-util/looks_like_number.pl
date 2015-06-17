8951874883854649a4923be3751298a83b8f1ba9
B3h3m0th <behemoth@autistici.org>
2015-06-17 18:47:13 +0200

    looks_like_number() test

--- scalars/scalar-util/looks_like_number.pl
+++ scalars/scalar-util/looks_like_number.pl
@@ -6,3 +6,13 @@ use Scalar::Util qw(looks_like_number);
 
 # the looks_like_number() function returns a boolean value indicating whether a
 # string looks like a number
+
+my @list_of_values = qw(23 7o8  2:1 v2 19_73 24-h 1 00O);
+
+foreach my $value (@list_of_values) {
+    if (looks_like_number($value)) {
+        say "Yes, \'$value\' looks like number";
+    } else {
+        say "No, \'$value\' doesn't look like number";
+    }
+}
