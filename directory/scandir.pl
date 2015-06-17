6ab5bc0bd6ff30183c6cf87af3a517d3ba1b7dec
B3h3m0th <behemoth@autistici.org>
2015-06-17 22:43:42 +0200

    enter into dir to scan and open it

--- directory/scandir.pl
+++ directory/scandir.pl
@@ -2,7 +2,18 @@
 use warnings;
 use strict;
 use v5.14;
+use Cwd;
 
 # The program starts with the requested directory to scan. When the scan is
 # complete it returns to the directory from which it was called.
+
+sub scan_directory {
+    my $dir_to_scan = shift // '.';
+
+    # Save the starting point:
+    my $current_dir = cwd();
+
+    # Enter into the directory to scan and open it:
+    chdir $dir_to_scan or die "Cannot enter into $dir_to_scan: $!\n";
+    open(DIR, '.') or die "Cannot open $dir_to_scan: $!\n";
 
