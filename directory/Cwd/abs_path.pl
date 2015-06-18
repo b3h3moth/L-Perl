93dfbcc0f81231821e0b8e2694f96f0450e3cdf2
B3h3m0th <behemoth@autistici.org>
2015-06-18 19:19:08 +0200

    abs_path() output

--- directory/Cwd/abs_path.pl
+++ directory/Cwd/abs_path.pl
@@ -2,11 +2,15 @@
 use warnings;
 use strict;
 use v5.14;
-use Cwd 'abs_path';
+use Cwd;
 
 die "Usage: $0 <filename>\n", if (@ARGV != 1);
 chomp(my $file = $ARGV[0]);
 
-my $cwd = getcwd();
+my $cur_working_dir = cwd();
+
+use Cwd 'abs_path';
 my $cwd_file = abs_path($file);
 
+say "$cur_working_dir";
+say "$cwd_file";
