090bbc50e9ad7fac19b81943b3fc2fd88459fcbe
B3h3m0th <behemoth@autistici.org>
2015-06-18 19:56:26 +0200

    testing to push files into hash checking file with abs_path()

---
+++ directory/scandir_hash.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.14;
+use Cwd;
+use Data::Dumper;
+use Cwd 'abs_path';
+
+# The program starts with the requested directory to scan. When the scan is
+# complete it returns to the directory from which it was called.
+
+my @results = ();
+my %hash = ();
+
+sub scan_directory {
+    my $dir_to_scan = shift // '.';
+
+    # Save the starting point:
+    my $current_dir = cwd();
+
+    # Enter into the directory to scan and open it:
+    chdir $dir_to_scan or die "Cannot enter into $dir_to_scan: $!\n";
+    opendir(my $DIR, '.') or die "Cannot open $dir_to_scan: $!\n";
+
+    # Save each filename of the directory, than close dirhandle:
+    my @filenames = readdir($DIR) or die "Cannot read $dir_to_scan: $!\n";
+    closedir($DIR);
+
+    foreach my $file (@filenames) {
+        # Skip current '.' and parent '..' directories:
+        next if ($file eq '.' or $file eq '..');
+
+        next if ($file =~ /^\./);
+
+        if (-d $file) {
+            push @{$hash{abs_path($file)}}, undef;
+            scan_directory($file);
+            next;
+        }
+
+        foreach my $path (keys %hash) {
+            say $path;
+            if ($path eq abs_path($file)) {
+                push @{$hash{$path}}, $file;
+            }
+        }
+    }
+    chdir $current_dir or die "Cannot change to $current_dir: $!\n";
+}
+
+scan_directory('..');
+
+#print Dumper \%hash;
