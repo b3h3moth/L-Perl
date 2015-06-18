ddccf8e284346624c3fbc82142d3a33122db1ec2
B3h3m0th <behemoth@autistici.org>
2015-06-18 17:12:47 +0200

    new %hash for testing data manipulation

--- directory/scandir.pl
+++ directory/scandir.pl
@@ -3,11 +3,13 @@ use warnings;
 use strict;
 use v5.14;
 use Cwd;
+use Data::Dumper;
 
 # The program starts with the requested directory to scan. When the scan is
 # complete it returns to the directory from which it was called.
 
 my @results = ();
+my %hash = ();
 
 sub scan_directory {
     my $dir_to_scan = shift // '.';
@@ -31,9 +33,11 @@ sub scan_directory {
 
         if (-d $file) {
             push @results, "DIR: $file";
+            push @{$hash{$file}}, undef;
             scan_directory($file);
             next;
         }
+
         push @results, $file;
     }
     chdir $current_dir or die "Cannot change to $current_dir: $!\n";
@@ -41,6 +45,8 @@ sub scan_directory {
 
 scan_directory('..');
 
-foreach my $line (@results) {
-    say $line;
-}
+#foreach my $line (@results) {
+#    say $line;
+#}
+
+print Dumper \%hash;
