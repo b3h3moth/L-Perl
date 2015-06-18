573d453bd76599528b1e00e9c636b901a916e592
B3h3m0th <behemoth@autistici.org>
2015-06-18 18:58:03 +0200

    fixed comment, getcwd()

--- directory/Cwd/getcwd.pl
+++ directory/Cwd/getcwd.pl
@@ -4,4 +4,8 @@ use strict;
 use v5.14;
 use Cwd;
 
+# This module provides functions for determining the pathname of the current
+# working directory.
+
 # Get pathname of current working directory
+my $current_woring_directory = getcwd();
