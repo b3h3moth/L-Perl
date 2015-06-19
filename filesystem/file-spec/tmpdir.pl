28510aa227a7c50611859bca3acfcd1556e30091
B3h3m0th <behemoth@autistici.org>
2015-06-19 10:55:15 +0200

    tmpdir()

--- filesystem/file-spec/tmpdir.pl
+++ filesystem/file-spec/tmpdir.pl
@@ -2,3 +2,10 @@
 use warnings;
 use strict;
 use v5.14;
+use File::Spec;
+
+# Returns a string representation of the first writable directory from a list 
+# of possible temporary directory; in Unix System checks $ENV{TMPDIR} and /tmp.
+
+my $tmp_dir = File::Spec->tmpdir();
+say $tmp_dir;
