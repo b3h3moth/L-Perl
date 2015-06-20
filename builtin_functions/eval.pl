bcee3e205856d21208b1232d2e35854e762bdc7f
B3h3m0th <behemoth@autistici.org>
2015-06-20 18:54:15 +0200

    eval() syntax

--- builtin_functions/eval.pl
+++ builtin_functions/eval.pl
@@ -2,3 +2,17 @@
 use strict;
 use warnings;
 use v5.20.2;
+
+# eval EXPR
+# eval BLOCK
+# eval
+#
+# In the first form, often referred to as a "string eval", the return value of
+# EXPR is parsed and executed as if it were a little Perl program. The value of
+# the expression is first parsed, and if there were no errors, executed as a
+# block within the lexical context of the current Perl program.
+#
+# In the second form, the code within the BLOCK is parsed only once and
+# executed within the context of the current Perl program. This form is
+# typically used to trap exceptions more efficiently than the first, while also
+# providing the benefit of checking the code within BLOCK at compile time.
