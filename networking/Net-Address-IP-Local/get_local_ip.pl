25921c7bc3382baa8932b13b368e45f04e8bcc11
B3h3m0th <behemoth@autistici.org>
2015-06-20 18:26:02 +0200

    print out local IP address

--- networking/Net-Address-IP-Local/get_local_ip.pl
+++ networking/Net-Address-IP-Local/get_local_ip.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 use Net::Address::IP::Local;
+use v5.20.2;
 
 # Get the local system's IP address
-
+my $local_address = Net::Address::IP::Local->public;
+say $local_address;
