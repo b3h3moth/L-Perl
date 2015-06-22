00d13dd87dbb1eb3f8594a7d83502ccaeef297ee
B3h3m0th <behemoth@autistici.org>
2015-06-22 23:21:08 +0200

    print out results

--- command_line_options/argv_arguments_control.pl
+++ command_line_options/argv_arguments_control.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use v5.20.2;
 
 die "Usage: $0 <firstname> <lastname>" if (@ARGV != 2);
 
 chomp(my $firstname = $ARGV[0]);
 chomp(my $lastname = $ARGV[1]);
+
+say "Hi my name is $firstname $lastname";
