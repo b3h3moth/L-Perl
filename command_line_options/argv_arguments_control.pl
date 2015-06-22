66970ed04e795e56f6c79c71661393910cb1910e
B3h3m0th <behemoth@autistici.org>
2015-06-22 23:19:52 +0200

    replaced 1 with 2

--- command_line_options/argv_arguments_control.pl
+++ command_line_options/argv_arguments_control.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-die "Usage: $0 <firstname> <lastname>" if (@ARGV != 1);
+die "Usage: $0 <firstname> <lastname>" if (@ARGV != 2);
 
 chomp(my $firstname = $ARGV[0]);
 chomp(my $lastname = $ARGV[1]);
