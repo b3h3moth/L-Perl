fbeeb6b4f1fc2735c1fd5b18f2f89d8fff889cd5
B3h3m0th <behemoth@autistici.org>
2015-06-23 19:44:31 +0000

    user & pass moved at the top of source code

--- DBI/SQLite/connect_to_db.pl
+++ DBI/SQLite/connect_to_db.pl
@@ -6,8 +6,9 @@ use v5.14;
 
 my $driver = 'SQLite';
 my $db = 'addressbook';
+my $user_id = 'behemoth';
+my $user_pass = '9c6k3tbnf2';
 
 # Connection to DB
 my $db_connection = "DBI::$driver:dbname=$db";
-my $user_id = 'behemoth';
-my $user_pass = '9c6k3tbnf2';
+
