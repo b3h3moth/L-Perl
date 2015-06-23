f6a285bd0d3a43d37f29017ce277c148f603605a
B3h3m0th <behemoth@autistici.org>
2015-06-23 20:14:27 +0000

    fixed Database handle

--- DBI/SQLite/connect_to_db.pl
+++ DBI/SQLite/connect_to_db.pl
@@ -6,18 +6,18 @@ use v5.14;
 
 my $driver = 'SQLite';
 my $db = '';
-my $user_id = '';
-my $user_pass = '';
+my $username = '';
+my $password = '';
 
-# Connection to Database
-my $data_source = 'dbi:SQLite:dbname=test.db';
+# Data source
+my $dsn = "dbi:SQLite:dbname=test.db";
 
 # Database handle
 my $dbh = DBI->connect(
     $dsn, 
-    $user_id, 
-    $user_pass, 
-    { RaiseError => 1 },
+    $username,
+    $password,
+    { RaiseError => 1, PrintError => 0, AutoCommit => 1 }
 ) or die "DBI:errstr";
 
 say "Database Opened with success";
