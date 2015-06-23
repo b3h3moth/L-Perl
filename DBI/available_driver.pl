e27915318d457b2b17b58a4f9a647670a21a1f0b
B3h3m0th <behemoth@autistici.org>
2015-06-23 19:34:56 +0000

    added version to invoke say()

--- DBI/available_driver.pl
+++ DBI/available_driver.pl
@@ -2,7 +2,9 @@
 use warnings;
 use strict;
 use DBI;
+use v5.14;
 
 # The DBI Database Interface is a database access for the Perl programming
 # language.
 my @available_drivers = DBI->available_drivers();
+
