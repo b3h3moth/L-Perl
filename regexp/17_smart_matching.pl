e9210e1905ec7fa64069483b11ab4f3b2ed062ef
B3h3m0th <behemoth@autistici.org>
2015-06-24 14:12:52 +0000

    moved data at the top of source code, fixed a lot of comments

--- regexp/17_smart_matching.pl
+++ regexp/17_smart_matching.pl
@@ -14,44 +14,41 @@ use List::Util qw(max);
 
 my $text = "Perl is one the best languages to manage UNIX systems";
 my $perl_version = 5.20;
-
-say "A: strings" if ($text ~~ /perl/i);
-say "B: numbers" if ($perl_version ~~ 5.20);
-
-my @names_a = qw(mario luca gabriele michele maria lucia giovanni);
-my @names_b = qw(mario luca gabriele michele maria lucia giovanni);
-
-# Compare two arrays and array element wirth a string
-say "C: The arrays have the same elements" if (@names_a ~~ @names_b);
-say "C: The array has $& element" if (@names_a ~~ /lucia/);
+my @numbers = qw(1 2 3 5 8 13 21 34 55 89);
+my $result = max(@numbers);
 
 my %club_a = (
     torino => 'juventus',
     milano => ' milan',
-    torino => 'torino',
-    milano => 'inter',
     roma => 'roma',
     genova => 'sampdoria',
-    roma => 'lazio',
-    genova => 'genoa',
 );
 
 my %club_b = (
     torino => 'juventus',
     milano => ' milan',
-    torino => 'torino',
-    milano => 'inter',
     roma => 'roma',
     genova => 'sampdoria',
-    roma => 'lazio',
-    genova => 'genoa',
 );
 
-# Compare two hash and hash key
+# Compare strings
+say "A: Find 'perl'" if ($text ~~ /perl/i);
+
+# Compare numbers
+say "B: The numbers are equals" if ($perl_version ~~ 5.20);
+say "B: (@numbers) = max is $result" unless @numbers ~~ $result;
+
+my @names_a = qw(mario luca gabriele michele maria lucia giovanni);
+my @names_b = qw(mario luca gabriele michele maria lucia giovanni);
+
+# Compare two arrays 
+say "C: The arrays have the same elements" if (@names_a ~~ @names_b);
+# Compare the array elements with a string
+say "C: The array has $& element" if (@names_a ~~ /lucia/);
+
+
+# Compare two hash
 say "D: The hashes have the same pair of key\/values" if (%club_a ~~ %club_b);
+# Compare hash key with a string
 say "D: The hash has $& key" if (%club_a ~~ /torino/);
 
-my @numbers = qw(1 2 3 5 8 13 21 34 55 89);
-my $result = max(@numbers);
-
-say "(@numbers) = $result" unless @numbers ~~ $result;
