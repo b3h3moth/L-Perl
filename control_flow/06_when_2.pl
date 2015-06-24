4d36c09599f9bfbd74b2d1cd2c204a0003d048f0
B3h3m0th <behemoth@autistici.org>
2015-06-24 18:00:59 +0200

    testing when statements

--- control_flow/06_when_2.pl
+++ control_flow/06_when_2.pl
@@ -2,3 +2,35 @@
 use warnings;
 use strict;
 use v5.14;
+
+my @names = qw(vanessa rossana maria claudia francesca maria);
+
+foreach (@names) {
+    say "I'm working...";
+
+    when (/maria/i) {
+        say "maria is in it";
+        continue;
+    }
+
+    when (/\Amari/i) {
+        say "name starts with 'mari'";
+        continue;
+    }
+
+    when (/giusy/) {
+        say "giusy there is";
+        continue;
+    }
+
+    when (/rossana/) {
+        say "rossana there is";
+        continue;
+    }
+
+    say "Move to default ...";
+
+    default {
+        say "I don't see anything";
+    }
+}
