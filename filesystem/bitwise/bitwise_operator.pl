ff499c89bcce15a086f825d1a66491d107780847
B3h3m0th <behemoth@autistici.org>
2015-06-24 19:41:45 +0200

    bitwise op. or |

--- filesystem/bitwise/bitwise_operator.pl
+++ filesystem/bitwise/bitwise_operator.pl
@@ -4,8 +4,14 @@ use strict;
 use v5.14;
 
 # Bitwise and which bits are true in both operands.
-say 10 & 6;
+say "Bitwise operator: and, &";
 say 10 & 8;
 say 10 & 10;
 say 10 & 12;
+
+# Bitwise or which bits are true in one operand or the other.
+say "Bitwise operator: or, |";
+say 10 | 8;
+say 10 | 10;
+say 10 | 12;
 
