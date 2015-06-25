6d92199d14e18906051e054df905200ecf8dbef7
B3h3m0th <behemoth@autistici.org>
2015-06-25 22:15:46 +0200

    create tempfile

--- filesystem/file-temp/tempfile.pl
+++ filesystem/file-temp/tempfile.pl
@@ -3,10 +3,13 @@ use warnings;
 use strict;
 use v5.14;
 use File::Temp 'tempfile';
+use File::Spec;
 
-# tempfile()
-# The function make temporary files.
+# my $tmpdir = File::Spec->tmpdir();
 
-my $filename = 'temp';
-my $fh = tempfile();
-($fh, $filename) = tempfile();
+# tempfile() make a temporary files, and save it into /tmp.
+my $filename;
+die "I Cannot make filehandle: $!" unless( my $fh = tempfile() );
+die "I Cannot open $filename" unless ( ($fh, $filename) = tempfile() );
+
+say $filename;
