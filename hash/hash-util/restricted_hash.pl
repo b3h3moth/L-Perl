487a424cb85abe80ad6d1b5783f15df8550a2a4b
B3h3m0th <behemoth@autistici.org>
2015-06-25 19:10:37 +0200

    server hash, it is a restricted hash

--- hash/hash-util/restricted_hash.pl
+++ hash/hash-util/restricted_hash.pl
@@ -5,3 +5,10 @@ use v5.14;
 use Hash::Util;
 
 # An hash whose keys or values can't be altered
+my %server = (
+     user     => '/etc/passwd',
+     group    => '/etc/group',
+     homedir  => '/home/myname',
+     max_user => 100
+ );
+     
