c4f6f7e934c383f460b24269263f06d64d3648b9
B3h3m0th <behemoth@autistici.org>
2015-06-26 19:21:39 +0200

    print out every 'good' element

--- networking/HTTP-SimpleLinkChecker/check_link.pl
+++ networking/HTTP-SimpleLinkChecker/check_link.pl
@@ -4,10 +4,11 @@ use strict;
 use v5.14;
 use HTTP::SimpleLinkChecker 'check_link';
 
-my @links = qw(perl.org perl.it perl.com perl.co.uk perl.fr perl.net perl.eu);
+my @links = qw(perl.org perl.it perl.com perl.ne perl.fr perl.net perl.arbour);
 
 my @good_links = grep {
   check_link( $_ );
     ! $HTTP::SimpleLinkChecker::ERROR;
     } @links;
 
+say join ', ', @good_links;
