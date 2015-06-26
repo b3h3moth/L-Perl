c74c380caa13cfb91150c70ab61909750c7d9919
B3h3m0th <behemoth@autistici.org>
2015-06-26 19:16:05 +0200

    added @links list

--- networking/HTTP-SimpleLinkChecker/check_link.pl
+++ networking/HTTP-SimpleLinkChecker/check_link.pl
@@ -4,6 +4,8 @@ use strict;
 use v5.14;
 use HTTP::SimpleLinkChecker;
 
+my @links = qw(perl.org perl.it perl.com perl.co.uk perl.fr perl.net perl.eu);
+
 my @good_links = grep {
   check_link( $_ );
     ! $HTTP::SimpleLinkChecker::ERROR;
