6a0cb4aed2e954aecdf45c246f497ad500ecc844
B3h3m0th <behemoth@autistici.org>
2015-06-27 20:30:36 +0200

    dynamic code with with eval()

--- builtin_functions/eval/dynamic_code.pl
+++ builtin_functions/eval/dynamic_code.pl
@@ -2,3 +2,11 @@
 use warnings;
 use strict;
 use v5.14;
+
+my $weight = 12;
+my $height = 10;
+
+foreach my $op ( qw(* + -) ) {
+    my $result = eval "$weight $op $height";
+    say "$weight $op $height = $result";
+}
