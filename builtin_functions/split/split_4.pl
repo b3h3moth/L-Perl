9c595c50e0833fa9fe4ff1828cbea02d61d07374
B3h3m0th <behemoth@autistici.org>
2015-06-27 13:01:20 +0200

    the program's purpose

--- builtin_functions/split/split_4.pl
+++ builtin_functions/split/split_4.pl
@@ -5,6 +5,7 @@ use v5.14;
 
 my @numbers = qw(1 2 3 4 54 56  76 89 465 3452 234 435 0 7 42 54 56 890 21 32);
 
+# splits each number into its individual digits
 my @result = map { split // } @numbers;
 
 say join ', ', @result;
