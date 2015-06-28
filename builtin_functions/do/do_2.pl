885d10e64b779b4a4cc8279287edaec59b79d495
B3h3m0th <behemoth@autistici.org>
2015-06-28 18:23:04 +0200

    How I can use do{} with file

--- builtin_functions/do/do_2.pl
+++ builtin_functions/do/do_2.pl
@@ -1,7 +1,18 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use v5.14;
 
 # slurp all of a file's contents into a variable
 
+die "Usage: $0 <filename>\n" if (@ARGV != 1);
+chomp( my $filename = $ARGV[0] );
+
+my $file = do {
+    # localize input record separator
+    local $/;
+    # localize ARGV
+    local @ARGV = ( $filename );
+    <>;
+};
+
+print $file;
