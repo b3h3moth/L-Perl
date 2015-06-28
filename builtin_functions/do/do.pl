5e819b8dc31e274601ab6f27d4e2297dac66984a
B3h3m0th <behemoth@autistici.org>
2015-06-28 16:16:33 +0200

    do BLOCK syntax improved

--- builtin_functions/do/do.pl
+++ builtin_functions/do/do.pl
@@ -4,9 +4,12 @@ use strict;
 use v5.14;
 
 # do BLOCK
+# The do BLOCK lets you group several statements as a single expression.
+
 # Returns the value of the last command in the sequence of commands indicated 
 # by BLOCK.  When modified by the "while" or "until" loop modifier, executes 
 # the BLOCK once before testing the loop condition.
+
 my $string = do {
     my $size = eval ( 3 * 3 );
 };
