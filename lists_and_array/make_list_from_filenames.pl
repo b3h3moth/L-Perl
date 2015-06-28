dc95b7722f12549a0ff16929a1610b68a8ce80e4
B3h3m0th <behemoth@autistici.org>
2015-06-28 18:37:53 +0200

    method 2 added

--- lists_and_array/make_list_from_filenames.pl
+++ lists_and_array/make_list_from_filenames.pl
@@ -2,5 +2,9 @@
 use warnings;
 use strict;
 
+# method 1
 my @smaller_than_1000 = grep { (-s) < 1000 } @ARGV;
 print map { "    $_\n" } @smaller_than_1000;
+
+# method 2
+# print map { " $_\n" } grep { (-s) < 1000 } @ARGV;
