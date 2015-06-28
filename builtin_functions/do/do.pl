804813478b840e468819a7d9e72a3435dd592e9e
B3h3m0th <behemoth@autistici.org>
2015-06-28 16:15:24 +0200

    do BLOOCK syntax

--- builtin_functions/do/do.pl
+++ builtin_functions/do/do.pl
@@ -3,6 +3,10 @@ use warnings;
 use strict;
 use v5.14;
 
+# do BLOCK
+# Returns the value of the last command in the sequence of commands indicated 
+# by BLOCK.  When modified by the "while" or "until" loop modifier, executes 
+# the BLOCK once before testing the loop condition.
 my $string = do {
     my $size = eval ( 3 * 3 );
 };
