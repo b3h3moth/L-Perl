34272b7bb691345f293634bf22956e67c3d74546
B3h3m0th <behemoth@autistici.org>
2015-06-28 18:44:01 +0200

    the program's purpose

--- lists_and_array/make_list_from_filenames.pl
+++ lists_and_array/make_list_from_filenames.pl
@@ -2,6 +2,11 @@
 use warnings;
 use strict;
 
+# Take a list of filenames on the command line;
+# Use grep to select the file size less than 1.000 bytes;
+# Use map to transform the strings in this list (4 spaces + filename + \n);
+# Print the resulting list.
+
 # method 1
 my @smaller_than_1000 = grep { (-s) < 1000 } @ARGV;
 print map { "    $_\n" } @smaller_than_1000;
