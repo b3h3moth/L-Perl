1a840ab1ea89a1f816c20e7eb7eef930d73e60a7
B3h3m0th <behemoth@autistici.org>
2015-06-29 20:22:17 +0200

    no_upwards() purpose

--- filesystem/File-Spec/no_upwards.pl
+++ filesystem/File-Spec/no_upwards.pl
@@ -5,6 +5,7 @@ use diagnostics;
 use v5.14;
 use File::Spec;
 
+# Given a list of file names, strip out those that refer to a parent directory.
 opendir(DH, '.') or die "opendir() failed: $!";
 my @files = File::Spec->no_upwards( readdir(DH) );
 say join "\n", @files;
