3879c9064d0e18ad9167f46402aabcd05be99aad
B3h3m0th <behemoth@autistici.org>
2015-06-29 20:12:37 +0200

    no_upwards() test

--- filesystem/File-Spec/no_upwards.pl
+++ filesystem/File-Spec/no_upwards.pl
@@ -1,4 +1,10 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use diagnostics;
 use v5.14;
+use File::Spec;
+
+my @paths = qw(../../../etc.././home/devel../etc/config);
+File::Spec->no_upwards( @paths );
+say join ', ', @paths;
