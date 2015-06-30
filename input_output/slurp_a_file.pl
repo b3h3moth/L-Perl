29bdb4802ba3509d5c3d0a9f0efa1983522e3f54
B3h3m0th <behemoth@autistici.org>
2015-06-30 17:31:42 +0200

    slurp a file, method 1

--- input_output/slurp_a_file.pl
+++ input_output/slurp_a_file.pl
@@ -3,6 +3,8 @@ use warnings;
 use strict;
 use v5.14;
 
+# slurp all of a file's content into a variable
+
 my $content = do {
     local $/;
     open (FH, '<', '/proc/cpuinfo') or die "Cannot open file: $!";
