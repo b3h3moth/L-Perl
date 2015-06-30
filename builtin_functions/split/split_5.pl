34748182dbc51d1f9d0ff69b97cae654b09b875f
B3h3m0th <behemoth@autistici.org>
2015-06-30 18:46:15 +0200

    split each digit > 50

--- builtin_functions/split/split_5.pl
+++ builtin_functions/split/split_5.pl
@@ -2,3 +2,10 @@
 use warnings;
 use strict;
 use v5.14;
+
+# split each digit greater than 50
+
+my @numbers = qw(11 22 33 44 55 66 77 88 99);
+my @result = map { split // } grep { $_ > 50 } @numbers;
+
+say join "\n", @result;
