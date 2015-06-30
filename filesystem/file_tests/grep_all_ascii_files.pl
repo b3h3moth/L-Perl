e7903ef0f2a968b9afc65788588e59081daf2f36
B3h3m0th <behemoth@autistici.org>
2015-06-30 22:04:48 +0200

    print out the result

--- filesystem/file_tests/grep_all_ascii_files.pl
+++ filesystem/file_tests/grep_all_ascii_files.pl
@@ -3,6 +3,7 @@ use warnings;
 use strict;
 use v5.14;
 
-# Glob all files in the current directory and save them into an array
+# Glob all ASCII files in the current directory and save them into an array
 my $dir_name = '.';
 my @textfiles = grep { -T } glob "$dir_name/*";
+say join "\n", @textfiles;
