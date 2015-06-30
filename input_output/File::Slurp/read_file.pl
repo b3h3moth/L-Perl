5e519f250a3cd85ecd1d9e3df66f760d46eaf512
B3h3m0th <behemoth@autistici.org>
2015-06-30 18:28:42 +0200

    slurp file with read_file() File::Slurp

--- input_output/File::Slurp/read_file.pl
+++ input_output/File::Slurp/read_file.pl
@@ -2,3 +2,18 @@
 use warnings;
 use strict;
 use v5.14;
+use File::Slurp;
+
+my $cpu_info = '/proc/cpuinfo';
+my $partitions = '/proc/partitions';
+
+my $text_file = read_file($cpu_info);
+my @lines_file = read_file($partitions);
+
+say 'Reading (text) ', $cpu_info;
+print $text_file;
+
+say "\nReading (list) $partitions";
+foreach (@lines_file) {
+    print $_;
+}
