c06ecef6b69512ac9d309eab813c76e2450863f0
B3h3m0th <behemoth@autistici.org>
2015-06-30 17:21:50 +0200

    slurping a file

--- input_output/slurp_a_file.pl
+++ input_output/slurp_a_file.pl
@@ -2,3 +2,11 @@
 use warnings;
 use strict;
 use v5.14;
+
+my $content = do {
+    local $/;
+    open (FH, '<', '/proc/cpuinfo') or die "Cannot open file: $!";
+    <FH>;
+}; # I'm closing an istrcution, ';' is necessary.
+
+say $content;
