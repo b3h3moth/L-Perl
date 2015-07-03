f6e5cd19e654e0cfcf4db349219af04d0bdb2e4e
B3h3m0th <behemoth@autistici.org>
2015-07-03 11:24:14 +0200

    fixed source code

--- hash/create_hash_from_map.pl
+++ hash/create_hash_from_map.pl
@@ -4,18 +4,19 @@ use strict;
 use v5.14;
 use Data::Dumper;
 
+# How to create an hash from an array
 my @skills = qw(Perl C C++ InfoSec Unix SysAdmin);
 my %liuc = map { $_, 1 } @skills;
 
 print Dumper \%liuc;
 
 # The same thing, but with reference
+my @array = qw(1 2 3 4 5 6 7 8 9 10);
+
 sub set_hash_by_reference {
     my $items = shift;
     my %hash = map {$_, 1} @{$items};
     say Dumper \%hash;
 }
 
-my @array = qw(1 2 3 4 5 6 7 8 9 10);
 set_hash_by_reference(\@array);
-
