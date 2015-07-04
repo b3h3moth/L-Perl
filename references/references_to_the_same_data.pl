1c87e9dbbc9ff4bbf8da94d0026f707844c61f18
B3h3m0th <behemoth@autistici.org>
2015-07-04 17:57:34 +0200

    print out some elements with ref

--- references/references_to_the_same_data.pl
+++ references/references_to_the_same_data.pl
@@ -3,10 +3,14 @@ use warnings;
 use strict;
 use v5.14;
 
-my @array = qw(white blue reed green black);
+my @array = qw(white blue red green black);
 
 my $first_ref = \@array;
 my $second_ref = $first_ref;
 my $third_ref = $second_ref;
 
 # They are identical
+say $first_ref->[0];
+say $second_ref->[1];
+say $third_ref->[2];
+
