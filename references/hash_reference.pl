9248396cb8da09558beb00ba38f7d90c757c2a7d
B3h3m0th <behemoth@autistici.org>
2015-07-07 22:45:47 +0200

    dereferencing an entire hash (keys and values)

--- references/hash_reference.pl
+++ references/hash_reference.pl
@@ -16,3 +16,15 @@ my $hash_ref = \%juventus;
 # Dereferencing an hash reference to get a value
 my $favorite_footbal_club = ${$hash_ref}{'name'};
 say 'My favorite football club is: ', $favorite_footbal_club;
+
+# On entire hash (keys)
+my @keys = keys %{$hash_ref};
+foreach (@keys) {
+    say 'key: ', $_;
+}
+
+# On entire hash (values)
+my @values = values %{$hash_ref};
+foreach (@values) {
+    say 'values: ', $_;
+}
