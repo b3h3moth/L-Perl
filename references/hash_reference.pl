db2ef00b004ed8e270680c7a4ca7a2220f7c5326
B3h3m0th <behemoth@autistici.org>
2015-07-07 22:39:26 +0200

    dereferencing an hash value

--- references/hash_reference.pl
+++ references/hash_reference.pl
@@ -4,9 +4,14 @@ use strict;
 use v5.14;
 
 my %juventus = (
-    name => 'Juventus FC',
-    year => 1987,
-    lega => 'Serie A',
-    country => 'italy'
+    name        => 'Juventus FC',
+    foundation  => 1987,
+    league      => 'Serie A',
+    country     => 'italy',
+    city        => 'Torino'
 );
 
+my $hash_ref = \%juventus;
+
+my $favorite_footbal_club = ${$hash_ref}{'name'};
+say 'My favorite football club is: ', $favorite_footbal_club;
