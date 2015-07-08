7dc4b26ef336ae36cadc1edf965424a4997842f6
B3h3m0th <behemoth@autistici.org>
2015-07-08 19:52:55 +0200

    two hashes created, moto and auto

--- references/references_to_hashes.pl
+++ references/references_to_hashes.pl
@@ -2,3 +2,19 @@
 use warnings;
 use strict;
 use v5.14;
+
+my %auto = (
+    name  => 'ferrari',
+    model => 'testa rossa',
+    made => 'italy',
+    color => 'red'
+);
+
+my %moto = (
+    name => 'ducati',
+    model => 'monster',
+    made => 'italy',
+    color => ['red', 'white']
+);
+
+
