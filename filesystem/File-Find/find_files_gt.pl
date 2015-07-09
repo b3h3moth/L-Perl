1be097a017ec2233aa2e0316b114da26c487745c
B3h3m0th <behemoth@autistici.org>
2015-07-09 20:46:35 +0200

    fixed output and comment

--- filesystem/File-Find/find_files_gt.pl
+++ filesystem/File-Find/find_files_gt.pl
@@ -4,12 +4,13 @@ use strict;
 use v5.14;
 use File::Find 'find';
 
-# Print out the names of all the files with names longer than 20 characters,
+# Print out the names of all the files with names longer than 25 characters,
 # followed by the name of the largest file found. Job done.
 
 die "Usage: $0 <directory>\n" if (@ARGV != 1);
 
 my $max_str;
+my $max_len = 0;
 my $dir = $ARGV[0];
 
 find(\&wanted, $dir);
@@ -19,13 +20,12 @@ say "Max: $max_str";
 sub wanted {
     return unless -f $_;
 
-    if (length($_) > 0) {
+    if (length($_) > $max_len) {
         $max_str = $File::Find::name;
-        my $max_len = length($_);
-        say $max_len;
+        $max_len = length($_);
     }
 
-    if (length($File::Find::name) > 20) {
+    if (length($File::Find::name) > 25) {
         say $File::Find::name; 
     }
 }
