400a9c243fc5a8135bf94d8607492a5275c8e965
B3h3m0th <behemoth@autistici.org>
2015-07-09 20:32:05 +0200

    find file gt than N and characters name gt N

--- filesystem/File-Find/find_files_gt.pl
+++ filesystem/File-Find/find_files_gt.pl
@@ -4,4 +4,26 @@ use strict;
 use v5.14;
 use File::Find 'find';
 
+die "Usage: $0 <directory>\n" if (@ARGV != 1);
 
+my $max_str;
+my $max_len = 1;
+my $dir = $ARGV[0];
+
+find(\&wanted, $dir);
+
+say "max: $max_str";
+
+
+sub wanted {
+    return unless -f $_;
+
+    if (length($_) gt $max_len) {
+        $max_str = $File::Find::name;
+        $max_len = length($_);
+    }
+
+    if (length($File::Find::name) > 10) {
+        say $File::Find::name; 
+    }
+}
