9c1928b93d9c38c405ef50df285d32e904cff3a6
B3h3m0th <behemoth@autistici.org>
2015-07-09 20:42:11 +0200

    fixed code

--- filesystem/File-Find/find_files_gt.pl
+++ filesystem/File-Find/find_files_gt.pl
@@ -4,26 +4,26 @@ use strict;
 use v5.14;
 use File::Find 'find';
 
+
 die "Usage: $0 <directory>\n" if (@ARGV != 1);
 
 my $max_str;
-my $max_len = 1;
 my $dir = $ARGV[0];
 
 find(\&wanted, $dir);
 
-say "max: $max_str";
-
+say "Max: $max_str";
 
 sub wanted {
     return unless -f $_;
 
-    if (length($_) gt $max_len) {
+    if (length($_) > 0) {
         $max_str = $File::Find::name;
-        $max_len = length($_);
+        my $max_len = length($_);
+        say $max_len;
     }
 
-    if (length($File::Find::name) > 10) {
+    if (length($File::Find::name) > 20) {
         say $File::Find::name; 
     }
 }
