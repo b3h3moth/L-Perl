b18590c130facc69c03077dbd67371d7556d0219
B3h3m0th <behemoth@autistici.org>
2015-07-10 20:47:53 +0200

    removed few useless lines

--- hash/hash_autovivication.pl
+++ hash/hash_autovivication.pl
@@ -1,10 +1,9 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use v5.14;
 use Data::Dumper;
 
+
 my %kernel;
-my ($source, $destination) = qw(3.2.0-4-powerpc /usr/src);
-$kernel{$source}{$destination} = 'i386';
+$kernel{source}{destination} = 'i386';
 print Dumper \%kernel;
