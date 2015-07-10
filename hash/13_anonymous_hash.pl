d2cab472039a2e1afb82312d2b2a646c604f36a3
B3h3m0th <behemoth@autistici.org>
2015-07-10 19:22:54 +0200

    fixed comment

--- hash/13_anonymous_hash.pl
+++ hash/13_anonymous_hash.pl
@@ -3,10 +3,14 @@ use warnings;
 use strict;
 use v5.14;
 
-# Make anonymous hash simply substitute curly bracket for the parentheses; this
-# produces a hash which has no name, but which is referenced by the variable
-# $ref_hash.
-my $ref_hash = {nero => 'black', bianco => 'white', => arancione => 'orange'};
+# Make anonymous hash simply substitute curly brackets for the parentheses; 
+# this produces a hash which has no name, but which is referenced by the 
+# variable $ref_hash.
+my $ref_hash = {
+    nero => 'black', 
+    bianco => 'white',
+    arancione => 'orange'
+};
 
 # dereference with '->' operator:
 say 'nero in English is: '.$ref_hash->{nero};
