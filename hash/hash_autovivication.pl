4821c9b3d5b5f4b073fb27005f02790f476422f6
B3h3m0th <behemoth@autistici.org>
2015-07-10 19:36:44 +0200

    hash autovivification - it's not ready at the moment

--- hash/hash_autovivication.pl
+++ hash/hash_autovivication.pl
@@ -2,3 +2,9 @@
 use warnings;
 use strict;
 use v5.14;
+use Data::Dumper;
+
+my %kernel;
+my ($source, $destination) = qw(3.2.0-4-powerpc /usr/src);
+$kernel{$source}{$destination} = 'i386';
+print Dumper \%kernel;
