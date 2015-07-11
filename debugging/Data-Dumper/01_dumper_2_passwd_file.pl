f888ddb3140f5185a7c3d24b970bf405d8d16922
B3h3m0th <behemoth@autistici.org>
2015-07-11 14:02:57 +0200

    split /etc/passwd and print out dumper results

--- debugging/Data-Dumper/01_dumper_2_passwd_file.pl
+++ debugging/Data-Dumper/01_dumper_2_passwd_file.pl
@@ -3,14 +3,21 @@ use warnings;
 use strict;
 use Data::Dumper;
 
+# Open /etc/passwd file, split each row, save passwd data into %unix_user hash,
+# than Dumper results.
+
 my %unix_user;
 
 my $passwd_file = '/etc/passwd';
+
 open(PASSWD, '<', $passwd_file) or die "Unable to open $passwd_file: $!\n";
 
-while (PASSWD) {
+while (<PASSWD>) {
   my ($login, $pass, $ID_user, $ID_group, $username, $dir, $shell) = split ':';
-  %unix_user{login} = $login;
+  $unix_user{$login} = [$login, $pass, $ID_user, $ID_group, 
+                        $username, $dir, $shell];
 }
+
+close(PASSWD);
 
 print Dumper \%unix_user;
