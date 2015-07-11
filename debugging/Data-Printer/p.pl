1724051c4ef782616c15cb98214cb0109df58f50
B3h3m0th <behemoth@autistici.org>
2015-07-11 16:06:00 +0200

    Data::Printer::p subroutine, test

---
+++ debugging/Data-Printer/p.pl
@@ -0,0 +1,23 @@
+#!/Usrb/bin/env perl
+use warnings;
+use strict;
+use Data::Printer;
+
+# Open /etc/passwd file, split each row, save passwd data into %unix_user hash,
+# than dump results.
+
+my %unix_user;
+
+my $passwd_file = '/etc/passwd';
+
+open(PASSWD, '<', $passwd_file) or die "Unable to open $passwd_file: $!\n";
+
+while (<PASSWD>) {
+  my ($login, $pass, $ID_user, $ID_group, $username, $dir, $shell) = split ':';
+  $unix_user{$login} = [$login, $pass, $ID_user, $ID_group, 
+                        $username, $dir, $shell];
+}
+
+close(PASSWD);
+
+p(%unix_user);
