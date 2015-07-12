c54703516ad765b858ec5d2c1bc86b0615314cf7
B3h3m0th <behemoth@autistici.org>
2015-07-12 19:23:22 +0200

    return values comment added

---
+++ subroutines/04_return_values.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.22.0;
+
+# All Perl subroutines have a return value, there's no distinction between
+# those that return values and those that don't. Not all Perl subroutines have
+# a useful return value.
+
