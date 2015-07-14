3817d53b7718303907a717ce0174cfd122638a2e
B3h3m0th <behemoth@autistici.org>
2015-07-14 20:25:32 +0200

    replaced the subroutine invocation

--- subroutines/02_arguments.pl
+++ subroutines/02_arguments.pl
@@ -3,21 +3,21 @@ use warnings;
 use strict;
 use v5.14;
 
-#A function receives its parameters in a single array @_
+# A function receives its parameters in a single array @_
 sub language {
     my ($name) = @_;
     say "I'm writing $name program";
 }
 
-language('Perl');
+&language('Perl');
 
 sub languages {
     say "One of my favorite programming languages is: $_" for @_;
 }
 
-languages('C','C++','Perl','AWK','Lisp');
+&languages('C','C++','Perl','AWK','Lisp');
 
-#each invocation of shift will remove an item from the @_ array
+# Each invocation of shift will remove an item from the @_ array
 sub configuration1 {
     my $os = shift;
     my $mail_server = shift;
@@ -27,23 +27,23 @@ sub configuration1 {
     say "A: $os\nA: $mail_server\nA: $http_server\nA: $db_server";
 }
 
-configuration1('OpenBSD','OpenSMTPD','apache','SQLite');
+&configuration1('OpenBSD','OpenSMTPD','apache','SQLite');
 
-#list assignment (much more clear)
+# list assignment (much more clear)
 sub configuration2 {
     my ($os, $mail_server, $http_server, $db_server) = @_;
     say "B: $os\nB: $mail_server\nB: $http_server\nB: $db_server";
 }
 
-configuration2('Debian','exim4','nginx','MySQL');
+&configuration2('Debian','exim4','nginx','MySQL');
 
-#array assignment
+# array assignment
 sub configuration3 {
     my (@array) = @_;
     say "C: @array";
 }
 
-configuration3('FreeBSD','sendmail','http','MariaDB');
+&configuration3('FreeBSD','sendmail','http','MariaDB');
 
 my %italian_football_club = (
     torino => 'torino',
@@ -51,7 +51,7 @@ my %italian_football_club = (
     genova => 'genoa',
 );
 
-#hash assignment
+# hash assignment
 sub print_club {
     my %club = @_;
 
@@ -60,7 +60,7 @@ sub print_club {
     }
 }
 
-print_club(%italian_football_club);
+&print_club(%italian_football_club);
 
 sub print_club_city {
     my ($club_name, %club_city) = @_;
@@ -71,4 +71,4 @@ sub print_club_city {
     }
 }
 
-print_club_city('torino', %italian_football_club);
+&print_club_city('torino', %italian_football_club);
