f4228b407feba6e19c14e4f1b06e0bd26fa023f8
B3h3m0th <behemoth@autistici.org>
2015-07-15 19:38:01 +0200

    mult() subroutine

--- subroutines/02_parameters_length.pl
+++ subroutines/02_parameters_length.pl
@@ -2,3 +2,13 @@
 use warnings;
 use strict;
 use v5.22.0;
+
+sub mult {
+    if (@_ != 2) {
+        say "WARNING! &mult() subroutines wants two parameters!";
+    }
+
+    $_[0] * $_[1];
+}
+
+say ( my $total = &mult(10, 20) );
