2d563e4ed049c956d0928df7e1e2b05bbdda90ea
B3h3m0th <behemoth@autistici.org>
2015-07-16 17:44:53 +0200

    improved comment, adde a note about blocks

--- subroutines/variable_scoping/lexical_variables.pl
+++ subroutines/variable_scoping/lexical_variables.pl
@@ -5,8 +5,13 @@ use v5.14;
 
 # my VARIABLE;
 # my (LIST OF VARIABLES);
+#
 # It declares the listed variables to be local to the enclosing block, file, 
-# or 'eval'. If more than one value is listed, the list must be placed in
-# parentheses.
+# or 'eval'. 
+#
+# If more than one value is listed, the list must be placed in parentheses.
+#
+# Note that a file is a block.
 
+sub mult
 my $name = 'behemoth';
