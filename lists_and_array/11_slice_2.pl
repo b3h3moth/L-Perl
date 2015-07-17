20fb92c748e215274e06dd1b6837737d0f176e46
B3h3m0th <behemoth@autistici.org>
2015-07-17 23:24:54 +0200

    slice

--- lists_and_array/11_slice_2.pl
+++ lists_and_array/11_slice_2.pl
@@ -2,3 +2,16 @@
 use warnings;
 use strict;
 use v5.22.0;
+
+open (FH, "<", "/etc/passwd") or die "Cannot open file\n";
+
+while (<FH>) {
+    chomp;
+    my @item = split /:/;
+    # slice
+    my ($user, $shell) = ($item[0], $item[5]);
+    print "%-20 %-20\n", $user, $shell;
+}
+
+
+close(FH);
