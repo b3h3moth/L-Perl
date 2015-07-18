a7ea70752792896a020fe0e31c038e4aa2e889eb
B3h3m0th <behemoth@autistici.org>
2015-07-18 19:56:30 +0200

    added new password to list

--- cryptography/Crypt-PBKDF2/check_password.pl
+++ cryptography/Crypt-PBKDF2/check_password.pl
@@ -4,8 +4,8 @@ use strict;
 use v5.22.0;
 use Crypt::PBKDF2;
 
-my @password = qw(weak-pass-for-testing weak_pass_for_testing kljasdhf);
-my $encrypted_pass = "{X-PBKDF2}HMACSHA1:AAAD6A:qJZXJQ==:suQCLEns5f0Y2v+QUJ3gERgg/r4=";
+my @password = qw(alcnshdto djshutlso abcdefghi laosdktip spidotqwt ajsdtosop);
+my $encrypted_pass = "{X-PBKDF2}HMACSHA1:AAAD6A:fjrXNw==:ZkwbGNgk9ZsN6b/+fSm1vCRRZO4=";
 
 my $object = Crypt::PBKDF2->new;
 
