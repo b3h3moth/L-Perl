a4419d0a323d87277e403f5c76b086c4a250f5e6
B3h3m0th <behemoth@autistici.org>
2015-07-18 19:45:18 +0200

    new file, check pass

---
+++ cryptography/Crypt-PBKDF2/check_password.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.22.0;
+use Crypt::PBKDF2;
+
+my $object = Crypt::PBKDF2->new;
+my $pass = "weak_password";
+
+my $encrypted_pass = $object->generate($pass);
+
+say $encrypted_pass;
