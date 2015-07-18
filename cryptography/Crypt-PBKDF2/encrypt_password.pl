7666f31af6fb7912274f5bcc54d6cb5ac097f715
B3h3m0th <behemoth@autistici.org>
2015-07-18 19:56:06 +0200

    replaced password

--- cryptography/Crypt-PBKDF2/encrypt_password.pl
+++ cryptography/Crypt-PBKDF2/encrypt_password.pl
@@ -5,7 +5,7 @@ use v5.22.0;
 use Crypt::PBKDF2;
 
 my $object = Crypt::PBKDF2->new;
-my $clean_password = "weak_pass_for_testing";
+my $clean_password = "abcdefghi";
 
 my $encrypted_pass = $object->generate($clean_password);
 
