980eede34fef4b7c7150c366d2c6882c0a10f12a
B3h3m0th <behemoth@autistici.org>
2015-07-18 19:52:55 +0200

    check password validity

--- cryptography/Crypt-PBKDF2/check_password.pl
+++ cryptography/Crypt-PBKDF2/check_password.pl
@@ -4,9 +4,15 @@ use strict;
 use v5.22.0;
 use Crypt::PBKDF2;
 
+my @password = qw(weak-pass-for-testing weak_pass_for_testing kljasdhf);
+my $encrypted_pass = "{X-PBKDF2}HMACSHA1:AAAD6A:qJZXJQ==:suQCLEns5f0Y2v+QUJ3gERgg/r4=";
+
 my $object = Crypt::PBKDF2->new;
-my $pass = "weak_password";
 
-my $encrypted_pass = $object->generate($pass);
-
-say $encrypted_pass;
+foreach (@password) {
+    if ($object->validate($encrypted_pass, $_)) {
+        say "[$_] Test passed, your password is ok";
+    } else {
+        say "[$_] WARNING! password is wrong";
+    }
+}
