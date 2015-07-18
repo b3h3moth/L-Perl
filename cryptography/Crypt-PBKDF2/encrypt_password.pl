e79d0254f40e67a000208259f572f78501b63e11
B3h3m0th <behemoth@autistici.org>
2015-07-18 19:42:30 +0200

    fixed and improved

--- cryptography/Crypt-PBKDF2/encrypt_password.pl
+++ cryptography/Crypt-PBKDF2/encrypt_password.pl
@@ -5,8 +5,8 @@ use v5.22.0;
 use Crypt::PBKDF2;
 
 my $object = Crypt::PBKDF2->new;
-my $pass = "weak_password";
+my $clean_password = "weak_pass_for_testing";
 
-my $encrypted_pass = $object->generate($pass);
+my $encrypted_pass = $object->generate($clean_password);
 
 say $encrypted_pass;
