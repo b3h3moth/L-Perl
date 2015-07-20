cbfa7a20aec5aaa78f7c2555c3072ed5554b6420
B3h3m0th <behemoth@autistici.org>
2015-07-20 17:37:10 +0200

    salt_len and optput_len added

--- cryptography/Crypt-PBKDF2/encrypt_password_improved.pl
+++ cryptography/Crypt-PBKDF2/encrypt_password_improved.pl
@@ -6,7 +6,14 @@ use Crypt::PBKDF2;
 
 my $object = Crypt::PBKDF2->new(
     hash_class  => 'HMACSHA2',  # default is HMACSHA1
-    iterations  =>  '1500',     # default is 100# default is 1000
+    hash_args   => {
+        sha_size => 512,
+    },
+    iterations  =>  1500,       # default is 1000
+    salt_len    =>  5,          # default is 4
+    output_len  =>  20
+);
+
 my $clean_password = "abcdefghi";
 
 my $encrypted_pass = $object->generate($clean_password);
