8c356d68cd900dd0d859d4319693b374ad9f91a1
B3h3m0th <behemoth@autistici.org>
2015-07-20 19:43:17 +0200

    Marshalling data definition

--- debugging/Data-Dumper/marhalling_data.pl
+++ debugging/Data-Dumper/marhalling_data.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use v5.14;
+use v5.22.0;
+
+# Marhalling in computer science is the process of transforming the memory of
+# an object to a data format suitable for storage or transmission, and it is
+# typically used when data must be moved between different parts of a computer
+# program or from one program to another. It is similar to serialization.
