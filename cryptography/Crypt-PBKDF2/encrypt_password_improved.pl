499416e7e2b0c91b6d1ca55cb1c2e12378deb585
B3h3m0th <behemoth@autistici.org>
2015-07-20 17:23:22 +0200

    HMACSHA1 replaced with HMACSHA2

--- cryptography/Crypt-PBKDF2/encrypt_password_improved.pl
+++ cryptography/Crypt-PBKDF2/encrypt_password_improved.pl
@@ -4,7 +4,8 @@ use strict;
 use v5.22.0;
 use Crypt::PBKDF2;
 
-my $object = Crypt::PBKDF2->new;
+my $object = Crypt::PBKDF2->new(
+    hash_class => 'HMACSHA2', # default is HMACSHA1
 my $clean_password = "abcdefghi";
 
 my $encrypted_pass = $object->generate($clean_password);
