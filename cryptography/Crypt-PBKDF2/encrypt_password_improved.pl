8059b47fe1148d4d78dfa12f0389c29fcc980867
B3h3m0th <behemoth@autistici.org>
2015-07-20 17:24:43 +0200

    replaced iterations with 1500

--- cryptography/Crypt-PBKDF2/encrypt_password_improved.pl
+++ cryptography/Crypt-PBKDF2/encrypt_password_improved.pl
@@ -5,7 +5,8 @@ use v5.22.0;
 use Crypt::PBKDF2;
 
 my $object = Crypt::PBKDF2->new(
-    hash_class => 'HMACSHA2', # default is HMACSHA1
+    hash_class  => 'HMACSHA2',  # default is HMACSHA1
+    iterations  =>  '1500',     # default is 100# default is 1000
 my $clean_password = "abcdefghi";
 
 my $encrypted_pass = $object->generate($clean_password);
