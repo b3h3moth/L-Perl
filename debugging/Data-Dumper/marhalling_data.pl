82c7f558bfb0cb938c3209187f552987da77d0ac
B3h3m0th <behemoth@autistici.org>
2015-07-21 15:12:17 +0200

    Data::Dumper added

--- debugging/Data-Dumper/marhalling_data.pl
+++ debugging/Data-Dumper/marhalling_data.pl
@@ -2,8 +2,10 @@
 use warnings;
 use strict;
 use v5.22.0;
+use Data::Dumper
 
 # Marhalling in computer science is the process of transforming the memory of
 # an object to a data format suitable for storage or transmission, and it is
 # typically used when data must be moved between different parts of a computer
 # program or from one program to another. It is similar to serialization.
+
