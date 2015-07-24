e600b4f43c89c4d6ece35a5101eb44111f810f81
B3h3m0th <behemoth@autistici.org>
2015-07-24 18:38:42 +0200

    fixed output

--- data_serialization/marshalling_data/marhalling_data.pl
+++ data_serialization/marshalling_data/marhalling_data.pl
@@ -4,6 +4,8 @@ use strict;
 use v5.22.0;
 use Data::Dumper;
 
+$Data::Dumper::Indent = 0;
+
 # Marhalling in computer science is the process of transforming the memory of
 # an object to a data format suitable for storage or transmission, and it is
 # typically used when data must be moved between different parts of a computer
@@ -16,10 +18,11 @@ print '$dog_name = ', $dog_name, "\n";
 print '$district = [', join ', ',@$colors, ']';
 
 # new Data::Dumper object
-my $dumper = Data::Dumper->new( [$dog_name, $colors], [qw(dog_name colors)] );
+my $dumper_a = Data::Dumper->new( [$dog_name], [qw(dog_name)] );
+my $dumper_b = Data::Dumper->new( [$colors], [qw(colors)] );
 
 # Dump the Perl values out into a variable
-my $dumped_val = $dumper->Dump();
+my $dumped_val = $dumper_a->Dump()."\n".$dumper_b->Dump();
 
 # Show what Data::Dumper has made of the variables!
 say "\nPerl Data::Dumper:";
