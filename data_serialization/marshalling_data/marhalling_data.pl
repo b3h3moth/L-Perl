61721eed3dbd7a576817abf580fa2a9e6bc5ca94
B3h3m0th <behemoth@autistici.org>
2015-07-24 18:25:08 +0200

    anon. array added

--- data_serialization/marshalling_data/marhalling_data.pl
+++ data_serialization/marshalling_data/marhalling_data.pl
@@ -10,22 +10,10 @@ use Data::Dumper;
 # program or from one program to another. It is similar to serialization.
 
 my $dog_name = 'luna';
-my @colors = ['black' 'green' 'gray' 'blu' 'red' 'white'];
+my $colors = ['black', 'green', 'gray', 'blu', 'red', 'white'];
 
-print '$dog_name = ', $dog_name;
-
-push @eng_football_club, \@ita_football_club;
-push @ita_football_club, \@eng_football_club;
-
-my $string = Data::Dumper->Dump(
-    [\@ita_football_club, \@eng_football_club],
-    [ qw(*ita_football_club *eng_football_club) ]
-);
-
-
-my $data = eval $string;
-
-print $data;
+print '$dog_name = ', $dog_name, "\n";
+print '$district = [', join ', ',@$colors, ']';
 
 # Historical note: Data Marshalling is so named because it was first studied in
 # 1962 by Edwars Waire Marshalll.
