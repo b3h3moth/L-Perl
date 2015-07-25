9ff31c710c836f5f5a7c7d271c1a15c0d6075359
B3h3m0th <behemoth@autistici.org>
2015-07-25 16:50:55 +0200

    Read structure back in again

--- data_serialization/marshalling_data/working_with_complex_structure.pl
+++ data_serialization/marshalling_data/working_with_complex_structure.pl
@@ -4,6 +4,8 @@ use warnings;
 use v5.22.0;
 use Data::Dump qw(dump);
 
+my $file_dump = 'dumped_structure.txt';
+
 # Structure I want to dump
 my %hash = (
     number => 31,
@@ -17,18 +19,21 @@ print "Here is the structure before dumping to file:\n";
 dump \%hash;
 
 # Print structure to file
-open my $out, '>', 'dumped_structure.txt' or die $!;
-print {$out} dump \%hash;
-close $out;
+open (DUMP_OUT, '>', $file_dump) or die "Cannot write $file_dump: $!\n";
+print DUMP_OUT dump \%hash;
+close(DUMP_OUT);
 
 # Read structure back in again
-open my $in, '<', 'dumped_structure.txt' or die $!;
+open (DUMP_IN, '<', $file_dump) or die "Cannot read $file_dump: $!\n";
+
 my $data;
 {
-    local $/;    # slurp mode
-    $data = eval <$in>;
+    # slurp!
+    local $/;
+    $data = eval <DUMP_IN>;
 }
-close $in;
+
+close(DUMP_IN);
 
 # See what the structure read in looks like
 print "Here is the structure after reading from file:\n";
