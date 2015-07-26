087991ff7ff2d231ec20b68ea580fffee584b8d2
B3h3m0th <behemoth@autistici.org>
2015-07-26 21:06:00 +0200

    fixed and improved output of code

--- lists_and_array/11_slice_3.pl
+++ lists_and_array/11_slice_3.pl
@@ -1,36 +1,35 @@
 #!/usr/bin/env perl
-    use strict;
-    use warnings;
+use strict;
+use warnings;
+use 5.22.0;
      
-    use 5.22.0;
-     
-    my %employee = (
-    jack => 980144,
-    peter => 128756,
-    john => 903610
-    );
-     
-    #Assign a hash slice to @id1 array
-    my @id1 = ($employee{"jack"}, $employee{"peter"}, $employee{"john"});
-     
-    #Print all employee ids from array @id1
-    say join ',', @id1;
-     
-    #Assign a hash slice to @id2 array
-    my @id2 = @employee{ qw/jack peter john/ };
-     
-    #Print all employee ids from array @id2
-    say join ',', @id2;
-     
-    my %employee2 = (
+my %employee = (
+    luca => 980144,
+    maria => 128756,
+    fiorenza => 903610
+);
+
+# Assign a hash slice to @id1 array
+my @id1 = ($employee{"luca"}, $employee{"maria"}, $employee{"fiorenza"});
+
+# Print all employee ids from array @id1
+say join ',', @id1;
+
+# Assign a hash slice to @id2 array
+my @id2 = @employee{ qw/luca maria fiorenza/ };
+
+# Print all employee ids from array @id2
+say join ',', @id2;
+
+my %employee2 = (
     #Name, Employee Id, Department, Location
-    jack => [980144,'Marketing','London'],
-    peter => [128756,'Research', 'Detroit'],
-    john => [903610, 'Development', 'Sydney']
-    );
-     
-    #Retrive the location of all employees
-    my @location = ($employee2{"jack"}->[2], $employee2{"peter"}->[2], $employee2{"john"}->[2]);
-     
-    #Print all employee's location
-    say join ',', @location;
+    luca => [980144,'Hacking','Rome'],
+    maria => [128756,'Operating Systems', 'NewYork'],
+    fiorenza => [903610, 'Web Development', 'Campobasso']
+);
+
+#Retrive the location of all employees
+my @location = ($employee2{"luca"}->[2], $employee2{"maria"}->[2], $employee2{"fiorenza"}->[2]);
+
+#Print all employee's location
+say join ',', @location;
