301c93ffe5180be6b45d581bb04a28914bb9b08c
B3h3m0th <behemoth@autistici.org>
2015-07-27 19:17:18 +0200

    sqrt() added

---
+++ builtin_functions/sqrt/sqrt.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.22.0;
+
+# sqrt EXPR
+# Returns the positive square root of EXPR
+
+my $num = 9;
+
+my $integer = sqrt(9);
+say 'Square root of ', $num,' is ',$integer;
