4fe4e1077cb2a018538fe96740bd1d69f6dbcead
B3h3m0th <behemoth@autistici.org>
2015-07-27 16:16:20 +0200

    how to remove decimal numbers from a number

--- builtin_functions/int/int.pl
+++ builtin_functions/int/int.pl
@@ -5,3 +5,10 @@ use v5.22.0;
 
 # int EXPR
 # Returns the integer portion of EXPR, it truncates towards 0.
+
+my $num = 9.8347;
+say $num;
+
+# Remove decimal numbers
+my $integer = int($num);
+say $integer;
