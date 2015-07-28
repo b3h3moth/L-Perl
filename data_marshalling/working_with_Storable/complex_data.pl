0acdada089bfcde77bd4350ccd3cff74e6a92ec0
B3h3m0th <behemoth@autistici.org>
2015-07-28 18:19:49 +0200

    freeze data

--- data_marshalling/working_with_Storable/complex_data.pl
+++ data_marshalling/working_with_Storable/complex_data.pl
@@ -2,4 +2,12 @@
 use warnings;
 use strict;
 use v5.22.0;
-use Storable;
+use Storable 'freeze';
+
+my @it_colors = qw(verde bianco rosso);
+my @en_colors = qw(green white red);
+
+push @en_colors, \@it_colors;
+push @it_colors, \@en_colors;
+
+my $data = freeze [\@it_colors, \@en_colors];
