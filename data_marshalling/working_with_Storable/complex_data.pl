bed5abdd476ce2272183e855abb991f42d164b48
B3h3m0th <behemoth@autistici.org>
2015-07-28 18:32:31 +0200

    comment on 'say', new hash ans store, store_fd

--- data_marshalling/working_with_Storable/complex_data.pl
+++ data_marshalling/working_with_Storable/complex_data.pl
@@ -2,7 +2,7 @@
 use warnings;
 use strict;
 use v5.22.0;
-use Storable qw(freeze store_fd);
+use Storable qw(freeze store_fd store);
 
 my @it_colors = qw(verde bianco rosso);
 my @en_colors = qw(green white red);
@@ -12,11 +12,17 @@ push @it_colors, \@en_colors;
 
 # Serializing to memory
 my $data = freeze [\@it_colors, \@en_colors];
-say $data;
+# say $data;
 
 my %rgb_table_color = (
-    255 => 'white';
-    0   => 'black';
+    255 => 'white',
+    0   => 'black',
     125 => 'gray'
 );
 
+my $color_ref = 'rgb_table.txt';
+
+# save into file
+store(\%rgb_table_color, $color_ref);
+# print out on stdout
+store_fd(\%rgb_table_color, \*STDOUT);
