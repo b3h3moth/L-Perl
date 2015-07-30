cbc7000566eb8b7af20800385e4a76e59d6d5789
B3h3m0th <behemoth@autistici.org>
2015-07-30 19:41:47 +0200

    nstore() with hash

--- data_marshalling/Storable/hash.pl
+++ data_marshalling/Storable/hash.pl
@@ -2,25 +2,11 @@
 use warnings;
 use strict;
 use v5.22.0;
-use Storable qw(freeze store_fd store);
+use Storable qw(freeze store_fd store nstore);
 
 # File which will store data
-my $filename = 'rgb_table.txt';
-
-my @it_colors = qw(verde bianco rosso);
-my @en_colors = qw(green white red);
-
-push @en_colors, \@it_colors;
-push @it_colors, \@en_colors;
-
-# Serializing to memory
-my $data = freeze [\@it_colors, \@en_colors];
-
-# Print out the Data on the stdout
-store_fd(\$data, \*STDOUT);
-
-# Save the data into a file
-store(\$data, $filename);
+my $filename = 'hash.txt';
+my $filename_binary = 'bin_hash.txt';
 
 my %rgb_table_color = (
     255 => 'white',
@@ -28,9 +14,11 @@ my %rgb_table_color = (
     125 => 'gray'
 );
 
-# Save the data into a file
-store(\%rgb_table_color, $filename);
-
-# Print out the Data on the stdout
+# Print out data on the stdout
 store_fd(\%rgb_table_color, \*STDOUT);
 
+# Store data into a file
+store(\%rgb_table_color, $filename);
+
+# Store binary representation into a file
+nstore(\%rgb_table_color, $filename_binary);
