65f529649aa3bee6dd36d58e513a5e9e0116393c
B3h3m0th <behemoth@autistici.org>
2015-08-01 19:47:03 +0200

    sorted and no-sorted lists numerically

--- lists_and_array/sort/sorting_numerically.pl
+++ lists_and_array/sort/sorting_numerically.pl
@@ -2,3 +2,13 @@
 use warnings;
 use strict;
 use v5.22.0;
+
+# Simple list
+my @list = qw(120 4 93 8 5 28 4 6 39 93 09 4 82 37 -8  5 674 34 32 3 0);
+
+# Print out list
+say 'A: ', join ', ', @list;
+
+# Sort list
+my @sorted_list = sort @list;
+say 'B: ', join ', ', @sorted_list;
