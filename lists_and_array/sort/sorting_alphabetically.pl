54978683cac7816d40a321a76050f71b46dfcaa0
B3h3m0th <behemoth@autistici.org>
2015-08-01 19:43:48 +0200

    sorted and no-sorted lists

--- lists_and_array/sort/sorting_alphabetically.pl
+++ lists_and_array/sort/sorting_alphabetically.pl
@@ -2,3 +2,13 @@
 use warnings;
 use strict;
 use v5.22.0;
+
+# Simple list
+my @list = qw(get up stand up stand up for your rights);
+
+# Print out list
+say 'A: ', join ', ', @list;
+
+# Sort list
+my @sorted_list = sort @list;
+say 'B: ', join ', ', @sorted_list;
