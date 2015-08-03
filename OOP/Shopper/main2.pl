45ca8c8bfbbd20cd149695a4a06cf7cf945ca3db
B3h3m0th <behemoth@autistici.org>
2015-08-03 16:37:48 +0200

    test new Personal2 class

--- OOP/Shopper/main2.pl
+++ OOP/Shopper/main2.pl
@@ -1,7 +1,17 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use v5.22.0;
 use Personal2;
 
-# Pass a list
-my $luca = Shopper::Personal2->new('luca', 100);
+my $luca = Shopper::Personal2->new({
+        name    =>  'behemoth',
+        budget  =>  250,
+});
+
+say 'Nome: ', $luca->name;
+say 'Budget: ', $luca->budget;
+$luca->name('Giusy');
+$luca->budget(1080);
+say 'new Name: ', $luca->name;
+say 'new Budget: ', $luca->budget;
