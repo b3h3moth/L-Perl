9f9828388f91e777ccd73d6200531542514601fd
B3h3m0th <behemoth@autistici.org>
2015-08-03 15:18:12 +0200

    invoke Personal1 and Personal2 constructors

--- OOP/Shopper/main.pl
+++ OOP/Shopper/main.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use Personal;
+use Personal1;
+use Personal2;
 
+my $luca = Shopper::Personal2->new('luca', 100);
 
-my $luca = Shopper::Personal->new(
+my $marco = Shopper::Personal1->new(
     {
         mame    => 'luca',
         budget  => 100
