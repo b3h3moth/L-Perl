20e2ce20a9f40ea9083c90ba15613de5ab9787c0
B3h3m0th <behemoth@autistici.org>
2015-08-03 15:31:54 +0200

    comment on bless() and reference

--- OOP/Shopper/Personal1.pm
+++ OOP/Shopper/Personal1.pm
@@ -7,6 +7,8 @@ use strict;
 sub new {
     my ( $class, $args ) = @_;
 
+    # You can use bless() with any kind of reference, but is much easier to 
+    # work with a hash reference.
     return bless {
         name    => $args->{name},
         budget  => $args->{budget}
