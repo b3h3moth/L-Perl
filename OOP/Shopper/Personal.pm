e2ef76d5d2f2a34f45d4ea839087f3aa9fd9f6b1
B3h3m0th <behemoth@autistici.org>
2015-08-03 13:34:09 +0200

    create an object class, and bless it

--- OOP/Shopper/Personal.pm
+++ OOP/Shopper/Personal.pm
@@ -4,4 +4,10 @@ use warnings;
 use strict;
 
 sub new {
+    my ( $class, $args ) = @_;
+
+    return bless {
+        name    => $args->{name},
+        budget  => $args->{budget}
+    }, $class;
 }
