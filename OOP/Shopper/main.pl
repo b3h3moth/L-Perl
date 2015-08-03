c7bae5483238a9537486e34b9aec56166d3d5ab6
B3h3m0th <behemoth@autistici.org>
2015-08-03 15:22:03 +0200

    added comment to each new()

--- OOP/Shopper/main.pl
+++ OOP/Shopper/main.pl
@@ -4,11 +4,13 @@ use strict;
 use Personal1;
 use Personal2;
 
-my $luca = Shopper::Personal2->new('luca', 100);
-
+# Pass a hash reference
 my $marco = Shopper::Personal1->new(
     {
         mame    => 'luca',
         budget  => 100
     }
 );
+
+# Pass a list
+my $luca = Shopper::Personal2->new('luca', 100);
