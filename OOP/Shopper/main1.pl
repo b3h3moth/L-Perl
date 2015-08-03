91f447a72a4792d517d8357b7ccedf8c8f2edfce
B3h3m0th <behemoth@autistici.org>
2015-08-03 16:05:23 +0200

    invoke get method of the class Personal1

--- OOP/Shopper/main1.pl
+++ OOP/Shopper/main1.pl
@@ -2,15 +2,12 @@
 use warnings;
 use strict;
 use Personal1;
-use Personal2;
 
 # Pass a hash reference
-my $marco = Shopper::Personal1->new(
-    {
-        mame    => 'luca',
+my $marco = Shopper::Personal1->new({
+        name    => 'luca',
         budget  => 100
-    }
-);
+ });
 
-# Pass a list
-my $luca = Shopper::Personal2->new('luca', 100);
+print $marco->get_name;
+print $marco->get_budget;
