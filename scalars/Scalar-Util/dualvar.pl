bb07f5a4c226588e61e3c687b041fa35fd245c41
B3h3m0th <behemoth@autistici.org>
2015-08-05 21:50:18 +0200

    dualvar() syntax

--- scalars/Scalar-Util/dualvar.pl
+++ scalars/Scalar-Util/dualvar.pl
@@ -3,3 +3,7 @@ use warnings;
 use strict;
 use v5.22.0;
 use Scalar::Util;
+
+# dualvar NUM, STRING
+# Returns a scalar that has the value NUM in a numeric context and the value 
+# STRING in a string context.
