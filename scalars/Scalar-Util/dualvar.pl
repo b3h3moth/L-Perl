68cc59a37def31bfb027c8b6555d70631ea1a556
B3h3m0th <behemoth@autistici.org>
2015-08-06 20:55:15 +0200

    fixed output

--- scalars/Scalar-Util/dualvar.pl
+++ scalars/Scalar-Util/dualvar.pl
@@ -8,11 +8,10 @@ use Scalar::Util 'dualvar';
 # Returns a scalar that has the value NUM in a numeric context and the value 
 # STRING in a string context.
 
-my $str_and_num = dualvar 4, "The Perl";
+my $str_and_num = dualvar 4, "Perl";
 my $num = $str_and_num + 1;
 my $str = $str_and_num." Programming";
-say $num;
-say $str;
+say "I'm working on $str version $num";
 
 my @team = (10, 20, $str_and_num, 20, 10);
 say @team;
