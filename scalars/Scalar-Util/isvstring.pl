b460923247c4867c4dc48e50c134ace73a9bff32
B3h3m0th <behemoth@autistici.org>
2015-08-07 21:47:52 +0200

    isvstring test

--- scalars/Scalar-Util/isvstring.pl
+++ scalars/Scalar-Util/isvstring.pl
@@ -5,3 +5,7 @@ use v5.22.0;
 
 # isvstring EXPR
 # If EXPR is a scalar which was coded as a vstring the result is true.
+
+my $perl_version = v5.22.0;
+my $format = isvstring ($perl_version) ? "%vd" : "%s";
+printf($format, $perl_version);
