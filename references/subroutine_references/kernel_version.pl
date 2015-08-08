75a2f648f4cf2a3c5bc1adfaccf9d816c0eb4e56
B3h3m0th <behemoth@autistici.org>
2015-08-08 20:55:54 +0200

    Dereference a code reference

--- references/subroutine_references/kernel_version.pl
+++ references/subroutine_references/kernel_version.pl
@@ -11,4 +11,7 @@ sub kernel_version {
 # Basic subroutine call
 kernel_version('3.2.0-4-powerpc');
 
+# Take a reference to the subroutine (coderef)
 my $ref_to_kver = \&kernel_version;
+# Dereference a code reference
+&kernel_version('3.2.68-1+deb7u2');
