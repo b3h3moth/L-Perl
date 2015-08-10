1c39e02f674a72dbe475e2909546cebd48242c0e
B3h3m0th <behemoth@autistici.org>
2015-08-10 17:25:01 +0200

    Iterate over all subroutines

--- references/subroutine_references/01_subref.pl
+++ references/subroutine_references/01_subref.pl
@@ -29,15 +29,21 @@ say $ref_to_greeter;    # Output is CODE(0x106efe90)
 
 # To get back to the original subroutine, you must dereference a code 
 # reference (method 1)
-&luca_greets('Giusy 1');
+&luca_greets('Mara');
 
 # Replace the name of the subroutine with curly braces around the scalar
 # reference to subroutine (method 2)
-&{$ref_to_greeter}('Giusy 2');
+&{$ref_to_greeter}('Fiorenza');
 
 # If the value inside curly braces is a simple scalar variable, we can drop
 # the braces (method 3)
-&$ref_to_greeter('Giusy 3');
+&$ref_to_greeter('Lodovica');
 
 # We can use also the arrow notation (method 4)
-$ref_to_greeter->('Giusy 4');
+$ref_to_greeter->('Rossella');
+
+# Iterate over all subroutines, each of which is a coderef, then each coderef
+# is dereferenced.
+for my $greet(\&luca_greets, \&giusy_greets) {
+    $greet->('Claudia');
+}
