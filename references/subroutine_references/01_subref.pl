c6f0b83b961eff71bf7303cee0316d28437ce322
B3h3m0th <behemoth@autistici.org>
2015-08-10 17:16:52 +0200

    dereferencing methods to call a reference to subroutine

--- references/subroutine_references/01_subref.pl
+++ references/subroutine_references/01_subref.pl
@@ -28,6 +28,16 @@ my $ref_to_greeter = \&luca_greets;
 say $ref_to_greeter;    # Output is CODE(0x106efe90)
 
 # To get back to the original subroutine, you must dereference a code 
-# reference.
-&luca_greets('Giusy');
+# reference (method 1)
+&luca_greets('Giusy 1');
 
+# Replace the name of the subroutine with curly braces around the scalar
+# reference to subroutine (method 2)
+&{$ref_to_greeter}('Giusy 2');
+
+# If the value inside curly braces is a simple scalar variable, we can drop
+# the braces (method 3)
+&$ref_to_greeter('Giusy 3');
+
+# We can use also the arrow notation (method 4)
+$ref_to_greeter->('Giusy 4');
