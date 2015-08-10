45272d29e40b514005679a757b3da62fca498b68
B3h3m0th <behemoth@autistici.org>
2015-08-10 16:50:01 +0200

    fixed all subroutines

--- references/subroutine_references/kernel_version.pl
+++ references/subroutine_references/kernel_version.pl
@@ -3,24 +3,22 @@ use warnings;
 use strict;
 use v5.22.0;
 
-sub kernel_version {
-    my $version = shift;
-    say "My kernel version is: $version";
+sub luca_greets {
+    my $who = shift;
+    say "Luca: Hi, $who";
 }
 
-sub os_name {
-    my $name = shift;
-    say "My os is: $name";
+sub giusy_greets {
+    my $who = shift;
+
+    if ($who eq "Luca") {
+        say "Giusy: fuck you $who";
+    } else {
+        say "Giusy: hi $who";
+    }
 }
 
 # Basic subroutine call
-kernel_version('3.2.0-4-powerpc');
+luca_greets('Giusy');
+giusy_greets('Luca');
 
-# Take a reference to the subroutine (coderef)
-my $ref_to_kver = \&kernel_version;
-# Dereference a code reference
-&kernel_version('3.2.68-1+deb7u2');
-
-for my $version (\&kernel_version, \&os_name) {
-    $version->('reference');
-}
