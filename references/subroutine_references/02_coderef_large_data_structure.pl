96dcc7afe238e3c961855df1399aab0003142013
B3h3m0th <behemoth@autistici.org>
2015-08-10 17:32:36 +0200

    new file, coderefs into large data structure

---
+++ references/subroutine_references/02_coderef_large_data_structure.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.22.0;
+
+sub luca_greets {
+    my $who = shift;
+    say "Luca: Hey, $who";
+}
+
+sub giusy_greets {
+    my $who = shift;
+
+    if ($who eq "Luca") {
+        say "Giusy: $who?";
+    } else {
+        say "Giusy: Ciao $who";
+    }
+}
