8f7c6a014b39f0fe8b122bbf669a72642550cf3d
B3h3m0th <behemoth@autistici.org>
2015-08-11 16:47:46 +0200

    anon subroutine fixed

--- subroutines/08_anonymous_subroutines.pl
+++ subroutines/08_anonymous_subroutines.pl
@@ -3,15 +3,24 @@ use warnings;
 use strict;
 use v5.14;
 
-# An anonymous function is a subroutine without a name, it behaves like a named
-# subroutine but the only way to deal with it is by reference.
+# An anonymous subroutine is a subroutine without a name, it behaves like a 
+# named subroutine but the only way to deal with it is by reference.
 
-my $opinion = sub { say 'Information wants to be free' };
-$opinion->();
+my $login = sub { 
+    my $username = shift;
+    
+    if ($username eq "behemoth") {
+        say "Access granted as $username";
+    } else {
+        say "Access denied $username";
+    }
+}; # We need a semicolon to (en)close the statement.
+
 
 my $login_name = 'behemoth';
 my $access = sub { say $login_name };
 &$access();
-&{$access}();
-&{$access};
-&$access;
+# &{$access}();
+# &{$access};
+# &$access;
+
