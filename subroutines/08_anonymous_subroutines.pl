3a734cda7ef4fc798eaddc33534a2b05eb7483ac
B3h3m0th <behemoth@autistici.org>
2015-08-11 16:31:10 +0200

    methods to invoke anonymouse subroutines

--- subroutines/08_anonymous_subroutines.pl
+++ subroutines/08_anonymous_subroutines.pl
@@ -3,13 +3,15 @@ use warnings;
 use strict;
 use v5.14;
 
-#An anonymous function is a subroutine without a name, it behaves like a named
-#subroutine but the only way to deal with it is by reference.
+# An anonymous function is a subroutine without a name, it behaves like a named
+# subroutine but the only way to deal with it is by reference.
 
 my $opinion = sub { say 'Information wants to be free' };
 $opinion->();
 
-
 my $login_name = 'behemoth';
 my $access = sub { say $login_name };
 &$access();
+&{$access}();
+&{$access};
+&$access;
