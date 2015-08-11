5c6004d3921c82b72d835d924da9cf623eb65819
B3h3m0th <behemoth@autistici.org>
2015-08-11 16:17:30 +0200

    new file added

---
+++ references/subroutine_references/02_coderef_and_data_structure_2.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use v5.22.0;
+
+sub luca_greets {
+    my $who = shift;
+    say "Luca: Hey $who";
+}
+
+sub giusy_greets {
+    my $who = shift;
+
+    if ($who eq "Luca") {
+        say "Giusy: $who?";
+    } else {
+        say "Giusy: Ciao $who";
+    }
+}
+
+sub mara_greets {
+    my $who = shift;
+    say "Mara: Hi $who";
+}
+
+my %greets = (
+    Luca    => \&luca_greets,
+    Giusy   => \&giusy_greets,
+    Mara    => \&mara_greets,
+);
+
