accbe1d56c65e15ed31f3165e5eba880ecf3e6a9
B3h3m0th <behemoth@autistici.org>
2015-08-22 18:00:09 +0200

    chomped inpupt string

--- builtin_functions/chdir/update_git_repo.pl
+++ builtin_functions/chdir/update_git_repo.pl
@@ -7,6 +7,7 @@ use Cwd;
 die "Usage: $0 [pull | push]\n" if (@ARGV != 1);
 
 my $git_option = $ARGV[0];
+chomp($git_option);
 
 my @git_repositories = qw(/home/behemoth/devel/L-LP /home/behemoth/devel/L-C
                          /home/behemoth/devel/L-Perl /home/behemoth/devel/L-ASD
@@ -16,6 +17,4 @@ foreach (@git_repositories) {
     chdir $_;
     say getcwd();
     `git $git_option origin master`;
-    `git log -n 2`;
-    `git status`;
 }
