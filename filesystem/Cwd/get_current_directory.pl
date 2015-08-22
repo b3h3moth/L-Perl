7786aac32d7ad7b79e172baa44c4592fffbc69d4
B3h3m0th <behemoth@autistici.org>
2015-08-22 15:24:47 +0200

    abs_path(), get absolute path of the file

--- filesystem/Cwd/get_current_directory.pl
+++ filesystem/Cwd/get_current_directory.pl
@@ -2,8 +2,13 @@
 use warnings;
 use strict;
 use v5.22.0;
-use Cwd;
+use File::Slurp;
+use Cwd qw(getcwd abs_path);
 
 # Get current directory
 my $current_dir = getcwd();
 say $current_dir;
+
+# Get absolute path of the file
+my $absolute_path = abs_path('.');
+say $absolute_path;
