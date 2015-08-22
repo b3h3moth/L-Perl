0a0ccc505b640553c5af212ac5b479ed07fbfe55
B3h3m0th <behemoth@autistici.org>
2015-08-22 17:28:58 +0200

    change each directory with chdir()

--- builtin_functions/chdir/change_dir.pl
+++ builtin_functions/chdir/change_dir.pl
@@ -2,3 +2,12 @@
 use warnings;
 use strict;
 use v5.22.0;
+use Cwd;
+
+# chdir()  change the working directory
+
+my @directories = qw(. .. / /etc /boot /var /home ~);
+foreach (@directories) {
+    chdir $_;
+    say getcwd();
+}
