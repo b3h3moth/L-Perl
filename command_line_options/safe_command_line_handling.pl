e72877528fc7ea20bda83da999fec9404533b17f
B3h3m0th <behemoth@autistici.org>
2015-08-25 19:35:06 +0200

    <<>> double diamonds

--- command_line_options/safe_command_line_handling.pl
+++ command_line_options/safe_command_line_handling.pl
@@ -3,7 +3,11 @@ use warnings;
 use strict;
 use v5.22.0;
 
-# from Perl v5.22.0 we have safe command line handling, <<>>
+# Perl v5.22.0 new operator; safe command line handling with <<>>.
+
+# Double diamonds <<>> is like <> but uses three-argument open to open each 
+# file in @ARGV. This means that each element of @ARGV will be treated as an
+# actual file name, and "|foo" won't be treated as a pipe open.
 
 while (<<>>) {
     print $_;
