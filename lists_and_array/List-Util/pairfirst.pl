5a3c645728fe5058526d6aa18fff8935d733cc9e
B3h3m0th <behemoth@autistici.org>
2015-08-31 20:47:13 +0200

    testing pairfirst

--- lists_and_array/List-Util/pairfirst.pl
+++ lists_and_array/List-Util/pairfirst.pl
@@ -2,8 +2,8 @@
 use warnings;
 use strict;
 use v5.22.0;
-use List::Util;
+use List::Util 'pairfirst';
 
-my @kvlist = qw(1 123 23 34 45 56 67 78 98 98 78 6 45 34 35);
+my @kvlist = qw(1 123 23 34 45 56 67 78 98 98 78 6 45 34 35i 10);
 
-my ( $key, $value ) = pairfirst { my $a =~ m/^[[:upper:]]+$/ } @kvlist
+my ( $key, $value ) = pairfirst { $a = $_; $b = $_  } @kvlist;
