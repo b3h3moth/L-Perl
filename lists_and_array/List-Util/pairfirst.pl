8f0afb7a2088bf5ab3dc0906c684c41b08036a52
B3h3m0th <behemoth@autistici.org>
2015-08-31 20:48:10 +0200

    print the pairfirst() results

--- lists_and_array/List-Util/pairfirst.pl
+++ lists_and_array/List-Util/pairfirst.pl
@@ -7,3 +7,5 @@ use List::Util 'pairfirst';
 my @kvlist = qw(1 123 23 34 45 56 67 78 98 98 78 6 45 34 35i 10);
 
 my ( $key, $value ) = pairfirst { $a = $_; $b = $_  } @kvlist;
+
+say "$key, $value";
