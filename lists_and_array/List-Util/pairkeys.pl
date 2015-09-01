32513f6005497fae41e3b31e43537f11338f684b
B3h3m0th <behemoth@autistici.org>
2015-09-01 22:10:40 +0200

    pairkeys()

--- lists_and_array/List-Util/pairkeys.pl
+++ lists_and_array/List-Util/pairkeys.pl
@@ -2,4 +2,8 @@
 use warnings;
 use strict;
 use v5.22.0;
-use List::Util;
+use List::Util 'pairkeys';
+
+my @list = qw(get up stand up stand up for your right by Bob Marley);
+my @keys = pairkeys @list;
+say join ', ', @keys;
