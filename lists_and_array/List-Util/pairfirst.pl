458b17c1f7209cd320e50bb924582bceae5428d9
B3h3m0th <behemoth@autistici.org>
2015-09-01 20:09:18 +0200

    print out key/value from hash, pairfirst()

--- lists_and_array/List-Util/pairfirst.pl
+++ lists_and_array/List-Util/pairfirst.pl
@@ -10,4 +10,8 @@ my @list = qw( 123 23 34 45 56 67 78 98 98 78 6 45 34 35 );
 my @new_list = pairfirst { $a > 20, $b < 30  } @list;
 say join ', ', @new_list;
 
-my %hash = pairfirst { $a = $a * $b, $b = $a + $b } @list;
+my %hash = pairfirst { $a = $a * $a, $b = $b * $b } @list;
+
+while (my ($key, $value) = each %hash) {
+    say "$key: $value";
+}
