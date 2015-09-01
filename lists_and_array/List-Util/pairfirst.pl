7bc03a65636c098e0539e6a692c13dab2741818f
B3h3m0th <behemoth@autistici.org>
2015-09-01 20:06:31 +0200

    get key/value from pairfirst(), hash

--- lists_and_array/List-Util/pairfirst.pl
+++ lists_and_array/List-Util/pairfirst.pl
@@ -3,9 +3,11 @@ use warnings;
 use strict;
 use v5.22.0;
 use List::Util 'pairfirst';
+use Data::Dumper;
 
 my @list = qw( 123 23 34 45 56 67 78 98 98 78 6 45 34 35 );
 
 my @new_list = pairfirst { $a > 20, $b < 30  } @list;
 say join ', ', @new_list;
 
+my %hash = pairfirst { $a = $a * $b, $b = $a + $b } @list;
