17c250abcfc94a1f750368d199beb1cef5039f3f
B3h3m0th <behemoth@autistici.org>
2015-09-02 17:31:15 +0200

    unshift with array and list

--- lists_and_array/03_shift_unshift.pl
+++ lists_and_array/03_shift_unshift.pl
@@ -3,7 +3,9 @@ use warnings;
 use strict;
 use v5.22.0;
 
-# The shift() function perform actions on the start of the array (left side).
+# The shift function perform actions on the start of the array (left side).
+
+# shift ARRAY, LIST
 # It removes/shifts the first value of the array off and returns it.
 my @languages = qw(C C++ Perl AWK Python Lisp Prolog);
 say 'A: ', join ', ', @languages;
@@ -12,3 +14,8 @@ my $favorite_lang = shift @languages;
 say 'My favorite lang is: ', $favorite_lang;
 say 'B: ', join ', ', @languages;
 
+# unshift ARRAY, LIST
+# It does the opposite of a 'shift'. It Prepends list to the front of the array
+# and returns the new number of elements in the array.
+unshift @languages, qw(Zsh Bash Ksh);
+say 'C: ', join ', ', @languages;
