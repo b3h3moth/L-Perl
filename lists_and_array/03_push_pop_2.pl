b8720164b9358aad62721633e5222f91e216b4e5
B3h3m0th <behemoth@autistici.org>
2015-09-02 17:09:37 +0200

    added comments on every statement

--- lists_and_array/03_push_pop_2.pl
+++ lists_and_array/03_push_pop_2.pl
@@ -6,14 +6,19 @@ use v5.22.0;
 my @words = qw(Buffal Soldier Dreadlock Rasta 
                There was a buffalo soldier in
                the heart of America);
+
+# Save long words
 my @long_words;
 
+# Loop each element of the list
 foreach my $word (@words) {
-    print "$word\n";
+    print "$word ";
+
+    # Save long words greater than 6 into list
     if (length($word) >= 6) {
         push(@long_words, $word);
     }
 }
 
-print "Therewhere ", scalar(@long_words)," long_words\n";
-print "@long_words";
+say "\nLyrics has ", scalar(@long_words)," long words:";
+say "@long_words";
