31cfde24d58cd6d4a4c74889ca47ba786cc9d343
B3h3m0th <behemoth@autistici.org>
2015-09-02 16:56:16 +0200

    print each push/pop test

--- lists_and_array/03_push_pop_1.pl
+++ lists_and_array/03_push_pop_1.pl
@@ -6,16 +6,23 @@ use v5.22.0;
 # push ARRAY,LIST
 # It appends the value of LIST to the end of ARRAY.
 my @languages = qw(C C++ Perl AWK Python Lisp);
+say 'A: ', join ', ', @languages;
+
 my @shell_scripting = qw(bash zsh);
+say 'B: ', join ', ', @languages;
 
 
 push @languages, 'Prolog';
+say 'C: ', join ', ', @languages;
+
 push @languages, @shell_scripting;
-say join ', ', @languages;
+say 'D: ', join ', ', @languages;
 
 pop @languages;
+say 'E: ', join ', ', @languages;
+
 pop @languages;
-say join ', ', @languages;
+say 'F: ', join ', ', @languages;
 
 # unshift e shift aggiungono e rimuovono dalla testa
 unshift @languages, qw(droga sesso rockNroll);
