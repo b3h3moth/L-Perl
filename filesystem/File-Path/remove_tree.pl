10449ae646f5fbdc8ce5f53347fd58ac3aa5c82b
B3h3m0th <behemoth@autistici.org>
2015-09-05 19:28:25 +0200

    remove_tree(), remove directory File::Path

--- filesystem/File-Path/remove_tree.pl
+++ filesystem/File-Path/remove_tree.pl
@@ -1,8 +1,16 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use File::Path;
+use File::Path qw(mkpath remove_tree);
 
 # File::Path
 # This module provide a convenient way to create directories of arbitrary depth
 # and to delete an entire directory subtree from the filesystem.
+
+my $dir = "new_dir";
+mkpath($dir);
+
+# remove_tree()
+# The "remove_tree" function deletes the given directories and any files and 
+# subdirectories they might contain. It's like the unix commands "rm -r".
+die "Err.:$!\n" if !remove_tree($dir, 1, 1);
