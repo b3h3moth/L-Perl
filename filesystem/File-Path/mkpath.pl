9a3e324f56c35c5af871ca0d9e5240ddda633497
B3h3m0th <behemoth@autistici.org>
2015-09-05 19:21:38 +0200

    how to create directory with mkpath()

--- filesystem/File-Path/mkpath.pl
+++ filesystem/File-Path/mkpath.pl
@@ -7,3 +7,8 @@ use File::Path;
 # File::Path
 # This module provide a convenient way to create directories of arbitrary depth
 # and to delete an entire directory subtree from the filesystem.
+
+# mkpath()
+# The mkpath() function provide the legacy interface of make_path() with a
+# different interpretation of the arguments passed.
+die "Err.\n" if !mkpath("dir1", 1, 0644);
