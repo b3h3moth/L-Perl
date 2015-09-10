2f93e0540e8a3bcc403969de78cdbdbb38282226
B3h3m0th <behemoth@autistici.org>
2015-09-10 15:50:57 +0200

    true, false pairmap()

--- lists_and_array/List-Util/pairmap.pl
+++ lists_and_array/List-Util/pairmap.pl
@@ -5,5 +5,14 @@ use v5.22.0;
 use List::Util 'pairmap';
 
 my @numbers = qw(29 84 67 52 73 49 84 126 94 278 78 32 49 657 20 182);
-my @result = pairmap { "\$a = $a : \$b = $b" } @numbers;
-say join "\n", @result
+my @pairs = pairmap { "\$a = $a : \$b = $b" } @numbers;
+say join "\n", @pairs;
+
+my @results = pairmap { $a <=50 & $b >= 100 } @numbers;
+foreach my $num (@results) {
+    if ($num == 0) {
+        say 'false';
+    } elsif ($num > 0) {
+        say 'true';
+    }
+}
