e9dba53a40d56693cc45abda5bcaef23a87f9d13
B3h3m0th <behemoth@autistici.org>
2015-09-10 15:40:23 +0200

    pairmap() print each pair

--- lists_and_array/List-Util/pairmap.pl
+++ lists_and_array/List-Util/pairmap.pl
@@ -3,3 +3,7 @@ use warnings;
 use strict;
 use v5.22.0;
 use List::Util 'pairmap';
+
+my @numbers = qw(29 84 67 52 73 49 84 126 94 278 78 32 49 657 20 182);
+my @result = pairmap { "\$a = $a : \$b = $b" } @numbers;
+say join "\n", @result
