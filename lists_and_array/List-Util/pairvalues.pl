bae068acc0f40b078c84dbdd934cec8fada6c6db
B3h3m0th <behemoth@autistici.org>
2015-09-11 23:26:18 +0200

    pairvalues() returns the second value of pair

--- lists_and_array/List-Util/pairvalues.pl
+++ lists_and_array/List-Util/pairvalues.pl
@@ -3,3 +3,7 @@ use warnings;
 use strict;
 use v5.22.0;
 use List::Util 'pairvalues';
+
+my @numbers = qw(10 29 38 30 47 56 42 53 64 57 93 04 58 26 89 67 54 8 20 1);
+my @result = pairvalues @numbers;
+say join "\n", @result;
