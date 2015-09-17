ca6a9616cdfe5807a3296c54a685f31d5708f26b
B3h3m0th <behemoth@autistici.org>
2015-09-17 20:29:05 +0200

    perl v5.22.0

--- builtin_functions/split/split_1.pl
+++ builtin_functions/split/split_1.pl
@@ -1,13 +1,14 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use v5.22.0;
 
 # split()
 # It splits a string into a list of strings and returns that list.
 
 my $string = "take   a    walk  on    the    wild    side";
 
-#there are few whitespace between words.
+# there are few whitespace between words.
 my @new_string = split(/\s+/, $string);
 
 #\W+ is the same thing for this kind of $string
