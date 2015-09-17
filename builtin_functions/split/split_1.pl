0498d4d8a5dc1785012d5e0351bd818e83425b1f
B3h3m0th <behemoth@autistici.org>
2015-09-17 20:30:00 +0200

    replaced print with say

--- builtin_functions/split/split_1.pl
+++ builtin_functions/split/split_1.pl
@@ -11,9 +11,9 @@ my $string = "take   a    walk  on    the    wild    side";
 # there are few whitespace between words.
 my @new_string = split(/\s+/, $string);
 
-#\W+ is the same thing for this kind of $string
-#my @new_string = split(/\W+/, $string);
+# \W+ is the same thing for this kind of $string
+# my @new_string = split(/\W+/, $string);
 
 foreach my $i (@new_string) {
-    print "$i\n";
+    say $i;
 }
