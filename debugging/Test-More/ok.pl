67e6fdacb75068cc4f8e384605f045ef17af2624
B3h3m0th <behemoth@autistici.org>
2015-10-24 17:44:33 +0200

    fixed ok() tests, removed is() tests

--- debugging/Test-More/ok.pl
+++ debugging/Test-More/ok.pl
@@ -1,23 +1,22 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
-use Test::More tests => 8;
+use Test::More tests => 5;
 use v5.22.0;
 
+# Test::More is a Framework for writing test scripts
 my $number_of_tests_run;
 
-# Framework for writing test scripts
 my $x = 10;
 my $y = 20;
 
-ok($x <= $y, "$x <= $y it should be true");
-ok(1, 'it should be true');
-ok(0, 'it should be false');
-ok('a', 'it should be true');
-ok('', 'it should be false');
+my $true_result = "it should be true";
+my $false_result = "it should be false";
 
-is(30, $x + $y, 'it should be true');
-is(-10, $x - $y, 'it should be true');
-is(10, $x - $y, 'it should be false');
+ok ( $x <= $y, "$x <= $y $true_result");
+ok ( 1,        "$true_result");
+ok ( 0,         $false_result);
+ok ( 'a',      'it should be true');
+ok ( '',       'it should be false');
 
 done_testing($number_of_tests_run);
