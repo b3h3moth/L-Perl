ec6548fe41103ed661c393bba8bc380e2bf8bd0b
B3h3m0th <behemoth@autistici.org>
2016-01-21 11:26:26 +0100

    markov chain testing example

--- hash/markov.pl
+++ hash/markov.pl
@@ -2,3 +2,34 @@
 use warnings;
 use strict;
 use v5.22.0;
+
+my %statetab;
+my $MAXGEN = 10000;
+my $NONWORD = "\n";
+my $w1;
+my $w2;
+
+$w1 = $w2 = $NONWORD;
+
+while (<>) {
+    foreach (split) {
+        push(@{$statetab{$w1}{$w2}}, $_);
+        ($w1, $w2) = ($w2, $_);
+    }
+}
+
+# Aggiunge alla coda
+push(@{$statetab{$w1}{$w2}}, $NONWORD);
+
+$w1 = $w2 = $NONWORD;
+
+for (my $i = 0; $i < $MAXGEN; $i++) {
+    my $suf = $statetab{$w1}{$w2};
+    my $r = int(rand @$suf);
+    exit if ((my $t = $suf->[$r]) eq $NONWORD);
+    print "$t\n";
+
+    # Catena di markov
+    ($w1, $w2) = ($w2, $t);
+}
+
